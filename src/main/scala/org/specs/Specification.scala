84742d80f441d4f8e273891fe281d66534013ea2
etorreborre <etorreborre@yahoo.com>
2010-09-04 02:04:10 +1000

    trials for automated indentation

--- src/main/scala/org/specs/Specification.scala
+++ src/main/scala/org/specs/Specification.scala
@@ -1,51 +1,33 @@
 package org.specs
 
 trait Specification {
-  
-  case class Examples(s: String) {
-	def h1(ex: Example) = this
+  class Examples(val e: Example*) {
+	override def toString = e.mkString("\n")
   }
-  implicit def title(s: String) = new Examples(s)
-
-  case class Example(s: String)(nested: List[Example]) {
-	def ~[T](ex: =>T) = this
-	def ^[T](ex: =>T) = this
-	
-	def |(a: Example*) = Example(s)(a.toList)
-	def ^(a: Example*) = Example(s)(a.toList)
-	def &(a: Example*) = Example(s)(a.toList)
-	def \(a: Example*) = Example(s)(a.toList)
-	def /(a: Example*) = Example(s)(a.toList)
-	//def ->[T](a: =>T) = Example(s)(a.toList)
-	def ::(a: Example*) = Example(s)(a.toList)
-	def <(a: Example*) = Example(s)(a.toList)
-	def <<(a: Example*) = Example(s)(a.toList)
-	def >(a: Example*) = Example(s)(a.toList)
-	def >>(a: Example*) = Example(s)(a.toList)
-	def and(a: Example*) = Example(s)(a.toList)
-	def **(a: Example*) = Example(s)(a.toList)
-	def *(a: Example*) = Example(s)(a.toList)
-	def $(a: Example*) = Example(s)(a.toList)
+  case class Example(s: String = "", private val _nested: List[Example] = Nil, parent: Option[Example] = None, body: Option[Any] = None) { 
+    def level: Int = parent match { case None => 0; case Some(p) => p.level + 1}
+    def nested = _nested.map(_.copy(parent=Some(this)))
+    def add(a: Example) = copy(_nested = this._nested :+ a)
+    def getOrMakeParent = parent.getOrElse(Example(_nested=List(this)))
+	def ~[T](t: =>T) = this.copy(body = Some(t))
+	def ^(a: Example) = {
+	  this.body match {
+	 	case None => a.body match {
+	 	  case Some(ab) => add(a)
+	 	  case None => getOrMakeParent.add(a); a
+	 	}
+	 	case Some(b) => a.body match {
+	 	  case Some(ab) => getOrMakeParent.add(a); a
+	 	  case None => getOrMakeParent.getOrMakeParent.add(a); a
+	 	}
+	  }	
+	}
+    def indent = "  "*(level+1)
+	def show: String = "("+s + (if (nested.isEmpty) "" else nested.map(_.show).mkString("\n"+indent, "\n"+indent, "\n"))+")"
   }
-  implicit def forExample(s: String) = Example(s)(Nil)
-  implicit def toExamples(e: Example) = "" h1(e)
-	def report(e: Any*) = e
-	
-	  class Reporter {
-	//def |>(e: Examples) = e
+  implicit def forExample(s: String): Example = new Example(s)
+  class ExampleDesc(s: String) {
+	def ^(a: Example) = new Example(s).add(a)
+	def ~[T](t: =>T) = new Example(s, body = Some(t))
   }
-  object junit extends Reporter
-  object console extends Reporter
-  //object |> extends Reporter
-object end extends Example("end")(Nil)
-object | extends Example("sp")(Nil)
-  trait Context {
-	def before = {}
-	def after = {}
-  }
-  object context extends Context
-  implicit def toExamples(s: String): Any = new Object {
-	def ~(c: Context) = new Examples(s)
-  }
-
 }
--- src/test/scala/org/specs/specification/specificationSpec.scala
+++ src/test/scala/org/specs/specification/specificationSpec.scala
@@ -2,19 +2,32 @@ package org.specs
 
 class specificationSpec extends Specification { 
   val spec =
-  "it should"^
-    "and it should"^
+//  "it should"^
+    "in this context"^
       "do this" ~ e1^
       "do that" ~ e2^
-    "but also it should blah bli bal"^
-      "do this" ~ d().e3^
-      "do that" ~ d().e4^
-  "and eventually it should"^
-      "do this" ~ c.e1()
-  
+    "in there"^
+      "do also this" ~ e1^
+      "do also that" ~ e2
+
+    //    "in that context"^
+//      "do this other " ~ e1^
+//      "do that other" ~ e2|
+//  "it should also"^
+//    "do it again" ~ e1^
+//      "and again" ~ e1
   
   def e1 = List()
   def e2 = List()
+
+}
+object test extends specificationSpec with Application { 
+  override def main(args: Array[String]) = {
+	println(spec.getClass+":\n"+spec)  
+	println(spec.show)  
+  }	
+}
+
   class b() {
     val e0 = ""
     val before = "do before"
@@ -27,9 +40,16 @@ class specificationSpec extends Specification {
 	def e3 = List(e0)
 	def e4 = List(e0)
   }
-}
-object test extends specificationSpec with Application { 
-  override def main(args: Array[String]) = {
-	println(spec)  
-  }	
-}
+
+//  "it should" ^
+//    "do this" ~ e1^
+//    "do that" ~ e2|
+//  "it should also"^
+//    "do it again" ~ e1^
+//      "and again" ~ e1
+//      "do that" ~ e2^
+//    "but also it should blah bli bal"^
+//      "do this" ~ d().e3^
+//      "do that" ~ d().e4^
+//  "and eventually it should"^
+//      "do this" ~ c.e1()
