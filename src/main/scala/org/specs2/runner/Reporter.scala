ccd2d5fca7ffa586eec71d1ca4f110fe416edc82
etorreborre <etorreborre@yahoo.com>
2010-09-15 10:00:46 -0700

    added a linebreak

--- src/main/scala/org/specs2/runner/Reporter.scala
+++ src/main/scala/org/specs2/runner/Reporter.scala
@@ -6,9 +6,11 @@ import io._
 trait Reporter extends Output with Folder {
   def report(spec: Specification): Unit = 
 	report(SpecStart(name(spec)) +: spec.examples.fragments :+ SpecEnd(name(spec)))
+	
   def report(fragments: List[Fragment]): Unit = {
 	fragments.foldLeft(initial)(folder)
   } 
+  
   def name(spec: Specification) = ClassName.className(spec)
   val configuration = new Configuration
 }
