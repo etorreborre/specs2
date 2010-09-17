5ee93fcb120aae632ea2dacf05f944b847c876d5
etorreborre <etorreborre@yahoo.com>
2010-09-17 16:03:23 -0700

    made sure that the value for the expectable was only evaluated once

--- src/main/scala/org/specs2/matcher/Expectable.scala
+++ src/main/scala/org/specs2/matcher/Expectable.scala
@@ -3,15 +3,15 @@ package matcher
 import AnyMatchers._
 import execute._
 
-abstract class Expectable[T](t: =>T) {
+class Expectable[T](t: =>T) {
   protected val desc: Option[String] = None
+  private def value = t
   
   def applyMatcher(m: Matcher[T]): MatchResult[T] = {
-	lazy val value = t
-	m.apply(t)(this) 
+	m.apply(value)(this) 
   }
   
-  def description = d(t)
+  def description = d(value)
   /** @return the description of the matched value, quoted. */
   protected def d[T](value: =>T) = desc  match {
     case None => q(value)
