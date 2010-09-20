bf3ae2743e864fc9af6f85defddd7e48a73858af
etorreborre <etorreborre@yahoo.com>
2010-09-20 21:34:48 +1000

    started the documentation of the Matcher trait

--- src/main/scala/org/specs2/matcher/Matcher.scala
+++ src/main/scala/org/specs2/matcher/Matcher.scala
@@ -4,7 +4,26 @@ import specification._
 import execute._
 import AnyMatchers._
 
+/**
+ * <p>The <code>Matcher</code> trait is the base trait for Matchers.
+ * 
+ * This trait can be extended to provide an appropriate <code>apply</code> method that 
+ * will check an expectable value <code>a: Expectable[T]</code></p>.
+ * 
+ * Children of that class can use the optional description string to provide enhanced failure messages.
+ * 
+ * <i>Implementation notes</i>:<ul>
+ * <li>the parameter to the apply method must be a by-name parameter. This allows some values to be evaluated
+ * only when necessary. For example in <code>a must (m1(b) and m2(c))</code> m2(c) will not be evaluated if m1(b) is false</li>
+ * <li>However in the implementation of the apply function, it must be taken care of not evaluating the parameter twice. 
+ * Assigning it to a val is the solution to this issue.</li>
+ * <li>2 messages are included in the result of the apply function, to allow the easy creation of the negation of matchers
+ *  with the not method.</li>
+ * </ul>
+ * 
+ */
 trait Matcher[-T] { outer =>
+
   def apply[S <: T](t: =>Expectable[S]): MatchResult[S]
   
   protected def result[S <: T](test: =>Boolean, okMessage: =>String, koMessage: =>String, value: Expectable[S]): MatchResult[S] = {
@@ -18,6 +37,7 @@ trait Matcher[-T] { outer =>
     def apply[U <: S](a: =>Expectable[U]) = outer(a).or(m(a))
   }
 }
+
 object Matcher {
   def result[T](test: =>Boolean, okMessage: =>String, koMessage: =>String, value: =>Expectable[T]): MatchResult[T] = {
 	if (test) new MatchSuccess(okMessage, koMessage, value) 
