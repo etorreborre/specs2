a455c55b6d0c014e36f18ec39260ec79a65afa4d
etorreborre <etorreborre@yahoo.com>
2010-10-06 23:14:39 +1100

    fixed a bug

--- src/main/scala/org/specs2/specification/BaseSpecification.scala
+++ src/main/scala/org/specs2/specification/BaseSpecification.scala
@@ -3,7 +3,7 @@ package specification
 import matcher._
 
 trait BaseSpecification extends SpecificationStructure with FragmentsBuilder with Matchers {
-  def include(s: BaseSpecification) =  group(content.fragments)
+  def include(s: BaseSpecification) = group(s.content.fragments)
 }
 trait SpecificationStructure {
   val content: Fragments
