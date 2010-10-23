8a20a5a6b3e48a7ebc3ee639491bfce808f706ed
etorreborre <etorreborre@yahoo.com>
2010-10-23 12:01:08 +1100

    fixed the deletion of the file created in the FileWriterSpec

--- src/test/scala/org/specs2/io/FileWriterSpec.scala
+++ src/test/scala/org/specs2/io/FileWriterSpec.scala
@@ -4,31 +4,30 @@ import java.io.File
 import specification._
 import execute._
 
-class FileWriterSpec extends Specification {
-  def is = 
-"  A FileWriter should"                             ^
-"    write inside a file"                           ! c().e1^
-"    close the file if an exception occurs"         ! c().e2^
-"    rethrow the exception if an exception occurs"  ! c().e3
+class FileWriterSpec extends SpecificationWithJUnit {  def is = 
+"  A FileWriter should"                                              ^
+"    write inside a file"                                            ! c().e1^
+"    close the file if an exception occurs"                          ! c().e2^
+"    rethrow the exception if an exception occurs"                   ! c().e3
 
   case class c() extends After {
     val out = new MockWriter {}
     val fw = new  FileWriter {
       override def getWriter(path: String) = out
     }
-    def e1 = {
+    def e1 = this {
       fw.write("filePath")(_.write("hello world"))
       out.messages must_== List("hello world")
     }
-    def e2 = {
+    def e2 = this {
       try { fw.write("filePath")(_ => error("bad")) }
       catch { case e => () }
       out.closed must_== true
     }
-    def e3: Result = {
+    def e3 = this {
       try { fw.write("filePath")(_ => error("bad")); Failure("an exception must be thrown") }
-      catch { case e => { e.getMessage must_== "bad"} }
+      catch { case e => { e.getMessage must_== "bad" }.toResult }
     }
-    def after = new File("filePath").delete 
+    def after = { new File("filePath").delete} 
   }
 }
