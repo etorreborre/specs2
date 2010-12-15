f4035d49d4b4adffdc4667e20696b00c40ca7d39
etorreborre <etorreborre@yahoo.com>
2010-12-15 23:58:30 +1100

    more on specs2 philosophy

--- src/test/scala/org/specs2/guide/Philosophy.scala
+++ src/test/scala/org/specs2/guide/Philosophy.scala
@@ -6,8 +6,8 @@ class Philosophy extends Specification { def is = freetext                      
 ### The origins
 
 ***specs2*** has been created as an evolution of the [***specs***](http://code.google.com/p/specs) project.
-***specs*** started as learning project to explore Scala's DSL possibilities for doing Behaviour-Driven-Development (BDD).
 
+***specs*** started as learning project to explore Scala's DSL possibilities for doing Behaviour-Driven-Development (BDD).
 Among the very first objectives of specs were:
 
  * conciseness: it should be possible to express with the least amount of ceremony a software specification
@@ -22,20 +22,21 @@ Among the very first objectives of specs were:
 ### The score
 
 After a few years of use, let's have a look at what worked and what didn't.
-
 ##### Conciseness
 
 This objective was achieved thanks to the incredible power of implicits in Scala which provide lots of way to create
 an elegant syntax for specifying software. The following points must however be noted:
 
-  * ***specs*** provides implicits by inheriting methods from the `Specification` class. While this is very convenient
-    this is also considerably polluting the namespace of the user code _inside_ the specification
-  * some "reserved" words like `should`, `can`, `in` come from previous BDD libraries like rspec. But they are not always
-    convenient and sometimes one wants to write `"my example should provide"` just to avoid having every example under
-    `"my example" should` start with `provide`.
-    There is a way to do this in ***specs*** but this requires the creation of an ad-hoc method
-  * some people like to structure their specifications with other keywords like Given-When-Then which requires
-    additional methods in the library for no more added value than just displaying those words
+  *  ***specs*** provides implicits by inheriting methods from the `Specification` class. While this is very convenient
+     this is also considerably polluting the namespace of the user code _inside_ the specification
+
+  *  some "reserved" words like `should`, `can`, `in` come from previous BDD libraries like rspec. But they are not always
+     convenient and sometimes one wants to write `"my example should provide"` just to avoid having every example under
+    `"my example" should` start with `"provide..."`.
+     There is a way to do this in ***specs*** but this requires the creation of an ad-hoc method
+
+  *  some people like to structure their specifications with other keywords like Given-When-Then which requires
+     additional methods in the library for no more added value than just displaying those words
 
                                                                                                                         """^
                                                                                                                         end
