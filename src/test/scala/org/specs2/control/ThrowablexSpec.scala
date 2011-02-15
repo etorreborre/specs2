f26534945b0cf30d2c412b9b0e5517d4984bcb4f
etorreborre <etorreborre@yahoo.com>
2011-02-15 12:54:32 +1100

    fixed the failing examples

--- src/test/scala/org/specs2/control/ThrowablexSpec.scala
+++ src/test/scala/org/specs2/control/ThrowablexSpec.scala
@@ -26,26 +26,24 @@ class ThrowablexSpec extends SpecificationWithJUnit with Throwablex { def is =
     "headOption returns the first element as an option"                                                                 ! stack.e2^
                                                                                                                         end
 
-  import ThrowablexContext._
-
-  object location {
+  object chained extends ThrowablexContext {
+	  def e1 = e.chainedExceptions must_== List(e.getCause)
+	  def e2 = e.getFullStackTrace.size must_==
+	           e.getStackTrace.size + e.getCause.getStackTrace.size
+  }
+  object location extends ThrowablexContext {
     def e1 = e.location must_== "ThrowablexContext.scala:6"
     def e2 = e.classLocation must_== "org.specs2.control.ThrowablexContext:6"
     def e3 = e.fullLocation must_== "org.specs2.control.ThrowablexContext (ThrowablexContext.scala:6)"
   }
-  object filter {
+  object filter extends ThrowablexContext {
     def e1 = e.filter("org.specs2.control").getStackTrace.toList.map(_.toString) must
                containMatch("org.specs2.control")
-    def e2 = e.filterNot("org.specs2.control").getStackTrace.toList.map(_.toString) must
-               containMatch("org.specs2.control").not
+    def e2 = e.filterNot("org.specs2.control").getStackTrace.toList.map(_.toString) must not
+               containMatch("org.specs2.control")
   }
-  object stack {
-    def e1 = e(2).toString must beMatching(".*apply.*")
+  object stack extends ThrowablexContext {
+    def e1 = e(3).toString aka e.getStackTrace().mkString("\n") must beMatching(".*apply.*")
     def e2 = e.headOption.map(_.toString).toIterable must containMatch("ThrowablexContext")
-  }
-  object chained {
-	  def e1 = e.chainedExceptions must_== List(e.getCause)
-	  def e2 = e.getFullStackTrace.size must_== 
-	           e.getStackTrace.size + e.getCause.getStackTrace.size
   }
 }
