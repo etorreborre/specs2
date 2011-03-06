7ac3b9d9e9d69b7dfc8a19854e3a2724d2a966bb
etorreborre <etorreborre@yahoo.com>
2011-03-06 19:32:24 +1100

    fixed the latest failing spec

--- src/test/scala/org/specs2/reporter/SelectionSpec.scala
+++ src/test/scala/org/specs2/reporter/SelectionSpec.scala
@@ -7,8 +7,7 @@ import org.scalacheck._
 
 class SelectionSpec extends SpecificationWithJUnit with ScalaCheck with ArbitraryFragments { def is =
                                                                                                                         """
-Before executing and reporting a specification, the fragments must be selected and
-sorted:
+Before executing and reporting a specification, the fragments must be selected and sorted:
 
  * they must be selected to keep only the relevant ones
  * they must be sorted to respect their execution dependencies
@@ -45,39 +44,39 @@ sorted:
     implicit val params = set(maxSize -> 3)
 
     def e1 = check { (fs: Fragments) =>
-      val selected = select(fs ^ step("1"))
-      val selected2 = select(fs ^ step("1") ^ step("2"))
+      val selected = selectSequence(fs ^ step("1"))
+      val selected2 = selectSequence(fs ^ step("1") ^ step("2"))
       selected2 must have size(selected.size)
     }           
     def e2 = check { (fs: Fragments) =>
-      val selected = select(fs ^ ex1)
-      val selected2 = select(fs ^ ex1 ^ ex2)
+      val selected = selectSequence(fs ^ ex1)
+      val selected2 = selectSequence(fs ^ ex1 ^ ex2)
       selected2 must have size(selected.size)
     }
     def e3 = check { (fs: Fragments) =>
-      val selected = select(fs ^ ex1)
-      val selected2 = select(fs ^ ex1 ^ step("1"))
+      val selected = selectSequence(fs ^ ex1)
+      val selected2 = selectSequence(fs ^ ex1 ^ step("1"))
       selected2 must have size(selected.size + 1)
     }
     def e4 = check { (fs: Fragments) =>
-      val selected = select(fs ^ step("1"))
-      val selected2 = select(fs ^ step("1") ^ ex2)
+      val selected = selectSequence(fs ^ step("1"))
+      val selected2 = selectSequence(fs ^ step("1") ^ ex2)
       selected2 must have size(selected.size + 1)
     }
     def e5 = {
       val fragments: Fragments = "intro" ^ step("1") ^ ex1 ^ ex2 ^ step("2") ^ step("3") ^ ex1 ^ ex2
-      select(fragments).map(l => l.toList.map(_.toString).toString) must contain(
-      "List(SpecStart(anon), Text(intro), Step)",
+      selectSequence(fragments).map((s: FragmentSeq) => s.fragments.toString) must contain(
+      "List(SpecStart(), Text(intro), Step)",
       "List(Example(ex1), Example(ex2))",
       "List(Step, Step)",
-      "List(Example(ex1), Example(ex2), SpecEnd(anon))").inOrder
+      "List(Example(ex1), Example(ex2), SpecEnd())").inOrder
     }
   }
 
   case class seq() extends WithSelection {
     def e1 = {
       val fragments: Fragments = sequential ^ example("e1") ^ step("s1") ^ example("e2")
-      select(fragments).toList.flatten.toString must_== "List(SpecStart(anon), Example(e1), Step, Example(e2), SpecEnd(anon))"
+      select(fragments).toString must_== "List(SpecStart(Object), Example(e1), Step, Example(e2), SpecEnd(Object))"
     }
     def e2 = {
       val spec = new Specification { def is = sequential ^ example("e1") ^ step("s1") ^ example("e2") }
@@ -92,9 +91,12 @@ sorted:
   trait WithSelection {
     val selection = new DefaultSelection with DefaultSequence with MockOutput
 
+    def selectSequence(fs: Fragments): Seq[FragmentSeq] = {
+      selection.sequence(selection.select(fs.arguments)(fs))(fs.arguments).toList
+    }
     def select(f: Fragments) = {
       val fs = new Specification { def is = f }.content
-      selection.select(f.arguments)(fs).map(l => l.fragments.map(_.toString))
+      selection.select(f.arguments)(fs).toList.map(_.toString)
     }
     def step(message: String) = Step({selection.println(message); reporter.println(message)})
     def example(message: String) = message ! { selection.println(message); reporter.println(message); success }
