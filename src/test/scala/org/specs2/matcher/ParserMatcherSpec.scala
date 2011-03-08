81d89f5ff50e0f3debc9e79fd72ff567c5cd5670
Alexey Romanov <alexey.v.romanov@gmail.com>
2011-03-08 17:38:25 +0300

    More tests.

--- src/test/scala/org/specs2/matcher/ParserMatcherSpec.scala
+++ src/test/scala/org/specs2/matcher/ParserMatcherSpec.scala
@@ -5,18 +5,28 @@ import util.parsing.combinator.RegexParsers
 
 class ParserMatcherSpec extends SpecificationWithJUnit with ParserMatchers {
   import ParserMatcherImplicits._
-  import ParsersUnderTest.number
+  import ParsersUnderTest.{number, error}
+  val parsers = ParsersUnderTest
 
   def is =
   """
   The ParserMatchers trait provides matchers for Parser and ParseResult instances.
   """^
   "succeedOn tests if the parser succeeds on the given input"^
-  { number must succeedOn("12").withResult(equalTo(12)) }
+  { number must succeedOn("12").withResult(equalTo(12)) }^
+  { number must not(succeedOn("abc")) }^
+  { number must not(succeedOn("abc").withResult(equalTo(13))) }^
+  "failOn tests if the parser fails on the given input"^
+  { number must failOn("abc").withMsg(matching(".*")) }^
+  { number must not(failOn("12")) }^
+  "errorOn tests if the parser errors out completely"^
+  { error must errorOn("") }^
+  { number must not(errorOn("abc")) }
 
-  val parsers = ParsersUnderTest
 }
 
 object ParsersUnderTest extends RegexParsers {
   val number: Parser[Int] = """\d+""".r ^^ {_.toInt}
+
+  val error: Parser[String] = err("Error")
 }
