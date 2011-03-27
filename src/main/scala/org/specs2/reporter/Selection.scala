006faadae00c6f69fc713a43c8fb5113445abcf3
etorreborre <etorreborre@yahoo.com>
2011-03-28 09:07:40 +1100

    added AsSection examples

--- src/main/scala/org/specs2/reporter/Selection.scala
+++ src/main/scala/org/specs2/reporter/Selection.scala
@@ -6,7 +6,7 @@ import control.LazyParameters._
 import specification._
 import Fragments._
 import SpecsArguments._
-
+import TagsFragments._
 /**
  * The Selection trait implements the logic for filtering the fragments to execute
  */
@@ -23,8 +23,21 @@ trait DefaultSelection {
   def select(implicit arguments: Arguments): Fragments => Seq[Fragment] = (fragments: Fragments) => select(fragments.fragments)(arguments)
   /** select function returning a filtered seq of Fragments */
   def select(fragments: Seq[Fragment])(implicit arguments: Arguments = Arguments()): Seq[Fragment] = {
-    SpecsArguments.filter(fragments)(filterFragment)(arguments)
+    SpecsArguments.foldAll(fragments).filter(filterTags, filterFragment)(arguments)
   }
+
+  /**
+   * @return filter fragments according to tags
+   */
+  def filterTags(implicit commandLineArgs: Arguments=Arguments()) = (fragmentsAndArguments: Seq[(Fragment, Arguments)]) => {
+    val fragments = fragmentsAndArguments.map(_._2)
+    fragmentsAndArguments.zip(fragments.drop(1) :+ fragments.take(1)).filterNot {
+      case ((f, args), t @ TaggedAs(_)) if !t.keep(args.overrideWith(commandLineArgs)) => true
+      case ((f, args), TaggedAs(_))                      => true
+      case _                                             => false
+    }.collect { case (a, b) => a }
+  }
+
   /** 
    * the filter method filters examples based on their description,
    * keeping only the ones matching the ex attribute of the arguments object
--- src/test/scala/org/specs2/specification/TagsSpec.scala
+++ src/test/scala/org/specs2/specification/TagsSpec.scala
@@ -2,8 +2,9 @@ package org.specs2
 package specification
 
 import reporter.DefaultSelection
+import matcher.ThrownExpectations
 
-class TagsSpec extends SpecificationWithJUnit { def is =
+class TagsSpec extends SpecificationWithJUnit with ThrownExpectations { def is =
                                                                                                                         """
 A specification can be tagged with some meaningful names like "integration" or "accounts". Creating tags amounts to
 adding new fragments in the specification. Then those fragments are used to determine which other fragments must be executed
@@ -17,18 +18,60 @@ during the specification execution. There are 2 types of tags:
 
                                                                                                                         """^
                                                                                                                         p^
-  "A TaggedAs fragment can be created using the tag method in an Acceptance specification"                              ^
-    "then the fragment can be excluded from the selection"                                                              ! e1^
+  "A TaggedAs(t1) fragment can be created using the tag method in an Acceptance specification"                          ^
+    "then, when using exclude='t1'"                                                                                     ^
+      "the tagged fragment is excluded from the selection"                                                              ! tag1^
+       "and other fragments are kept"                                                                                   ! tag2^
+    "then, when using include='t1'"                                                                                     ^
+      "the tagged fragment is included in the selection"                                                                ! tag3^
+      "and other fragments are excluded"                                                                                ! tag4^
+                                                                                                                        endp^
+  "A AsSection(t1) fragment can be created using the section method in an Acceptance specification"                     ^
+    "then, when using exclude='t1'"                                                                                     ^
+      "the tagged fragments just before and after the section tag are excluded from the selection"                      ! section1^
+      "and the fragments before the section are kept"                                                                   ! section2^
+      "if the section is closed with another AsSection fragment containing the tag t1"                                  ^
+        "the tagged fragments between the section tags are excluded"                                                    ! section3^
+        "and the fragments outside the section are kept"                                                                ! section4^
+    "then, when using include='t1'"                                                                                     ^
+      "the tagged fragments just before and after the section tag are included in the selection"                        ! section5^
+      "and the fragments before the section are excluded"                                                               ! section6^
+      "if the section is closed with another AsSection fragment containing the tag t1"                                  ^
+        "the tagged fragments between the section tags are included"                                                    ! section7^
+        "and the fragments outside the section are excluded"                                                            ! section8^
                                                                                                                         end
 
   import DefaultSelection._
   import Tags._
 
-  def e1 = {
-    val fragments =
-      "text" ^
-        "e1" ! success ^ tag("t1")^
-        "e2" ! success ^ end
-    select(args(exclude="t1"))(fragments).map(_.toString) must not containMatch("e1")
-  }
+  val tagged =
+    "text" ^
+      "e1" ! success ^ tag("t1")^
+      "e2" ! success ^ end
+
+  val sectioned =
+    "text" ^
+      "e1" ! success ^
+      "e2" ! success ^ section("t1")^
+      "e3" ! success ^
+      "e4" ! success ^ section("t1")^
+      "e5" ! success ^ end
+
+  def includeTag(fs: Fragments) = select(args(include="t1"))(fs).map(_.toString)
+  def excludeTag(fs: Fragments) = select(args(exclude="t1"))(fs).map(_.toString)
+
+  def tag1 = excludeTag(tagged) must not containMatch("e1")
+  def tag2 = excludeTag(tagged) must containMatch("e2")
+  def tag3 = includeTag(tagged) must not containMatch("e2")
+  def tag4 = includeTag(tagged) must containMatch("e1")
+
+  def section1 = excludeTag(sectioned) must containMatch("e1")
+  def section2 = excludeTag(sectioned) must not containMatch("e2")
+  def section3 = excludeTag(sectioned) must not containMatch("e3")
+  def section4 = excludeTag(sectioned) must containMatch("e5")
+
+  def section5 = includeTag(sectioned) must not containMatch("e1")
+  def section6 = includeTag(sectioned) must containMatch("e2")
+  def section7 = includeTag(sectioned) must containMatch("e3")
+  def section8 = includeTag(sectioned) must not containMatch("e5")
 }
