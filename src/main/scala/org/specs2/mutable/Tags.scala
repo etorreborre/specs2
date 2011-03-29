0ac2edddb4fb116b333323ef1e9ebde1d8d05be9
etorreborre <etorreborre@yahoo.com>
2011-03-29 11:24:37 +1100

    fixed the addition of mutable tags

--- src/main/scala/org/specs2/mutable/Tags.scala
+++ src/main/scala/org/specs2/mutable/Tags.scala
@@ -7,10 +7,17 @@ import specification.TagsFragments._
  * The tags trait allows the creation of Tags fragments in a mutable specification where the tags are defined for the
  * _next_ fragment (or fragments for a section)
  */
-trait Tags extends org.specs2.specification.Tags {
+trait Tags extends org.specs2.specification.Tags { this: FragmentsBuilder =>
   /** create a Tag fragment */
-  override def tag(names: String*): TaggingFragment = Tag(names:_*)
+  override def tag(names: String*): TaggingFragment = {
+    val t = Tag(names:_*)
+    addFragments(t)
+    t
+  }
   /** create a Section fragment */
-  override def section(names: String*): TaggingFragment = Section(names:_*)
+  override def section(names: String*): TaggingFragment = {
+    val t = Section(names:_*)
+    addFragments(t)
+    t
+  }
 }
-object Tags extends Tags
