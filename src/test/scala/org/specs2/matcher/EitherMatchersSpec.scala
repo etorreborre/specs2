18e0296af6cd4ee7a0554dd81a51a091e857ca62
etorreborre <etorreborre@yahoo.com>
2011-04-11 21:48:34 +1000

    more either matchers

--- src/test/scala/org/specs2/matcher/EitherMatchersSpec.scala
+++ src/test/scala/org/specs2/matcher/EitherMatchersSpec.scala
@@ -9,10 +9,12 @@ class EitherMatchersSpec extends SpecificationWithJUnit { def is =
   "beRight checks if an element is Right(_)"                                                                            ^
   { Right(1) must beRight(1) }                                                                                          ^
   { Left(1) must not be right(1) }                                                                                      ^
+  { Right(1) must beRight.like { case i => i must be_>(0) } }                                                           ^
                                                                                                                         p^
   "beLeft checks if an element is Left(_)"                                                                              ^
   { Left(1) must beLeft(1) }                                                                                            ^
   { Right(1) must not be left(1) }                                                                                      ^
+  { Left(1) must beLeft.like { case i => i must be_>(0) } }                                                             ^
                                                                                                                         end
 
 }
