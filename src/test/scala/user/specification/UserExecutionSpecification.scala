4052cc82fd751b6b54f1b565f2df6431df88f914
etorreborre <etorreborre@yahoo.com>
2011-04-15 07:27:18 +1000

    fixed the line number for the location of failures

--- src/test/scala/user/specification/UserExecutionSpecification.scala
+++ src/test/scala/user/specification/UserExecutionSpecification.scala
@@ -2,6 +2,6 @@ package user
 package specification
 import org.specs2.execute._
 
-class UserExecutionSpecification extends org.specs2.Specification {
+class UserExecutionSpecification extends org.specs2.SpecificationWithJUnit {
   def is = "ex1" ! Failure("fail")
 }
