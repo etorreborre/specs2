e7d4b2ca4c845ca01c623333d95b450089a23697
etorreborre <etorreborre@yahoo.com>
2011-05-11 18:14:15 +1000

    more flexible spec finder with base path

--- src/main/scala/org/specs2/runner/SpecificationsFinder.scala
+++ src/main/scala/org/specs2/runner/SpecificationsFinder.scala
@@ -5,6 +5,7 @@ import java.util.regex._
 import scala.collection.mutable.Queue
 import io._
 import reflect.Classes
+import specification.SpecificationStructure
 
 /**
  * This trait loads specifications found on a given source directory based
@@ -17,8 +18,8 @@ trait SpecificationsFinder extends FileSystem with Classes with ConsoleOutput {
     * @param pattern a regular expression which is supposed to match an object name extending a Specification
     * @return specification names by scanning files and trying to find specifications declarations
     */
-   def specificationNames(path: String, pattern: String) : List[String] = {
-     filePaths(path) filter (_.endsWith(".scala")) flatMap { p =>
+   def specificationNames(basePath: String = ".", path: String = "*", pattern: String = ".*Spec") : Seq[String] = {
+     filePaths(basePath, path) filter (_.endsWith(".scala")) flatMap { p =>
        val fileContent = readFile(p)
        val packName = packageName(fileContent)
        classNames(packName, fileContent, pattern, "object", "$") ++ classNames(packName, fileContent, pattern, "class", "")	 
@@ -34,7 +35,7 @@ trait SpecificationsFinder extends FileSystem with Classes with ConsoleOutput {
    * @param content content of the file
    * @param pattern a regular expression which is supposed to match an object name extending a Specification
    */
-  def classNames(packageName: String, content: String, pattern: String, specType: String, suffix: String): List[String] = {
+  def classNames(packageName: String, content: String, pattern: String, specType: String, suffix: String): Seq[String] = {
     def result(m: Matcher): Stream[String] = 
       if (m.find) { 
     	  val fullName = List(packageName, m.group(1).trim).mkString(".") + suffix   
@@ -42,9 +43,14 @@ trait SpecificationsFinder extends FileSystem with Classes with ConsoleOutput {
       }
       else Stream.empty
       
-    val specPattern = "\\s*"+specType+"\\s*(" + pattern + ")\\s*extends\\s*.*"
-    result(Pattern.compile(specPattern).matcher(content)).toList
+    result(Pattern.compile(specPattern(specType, pattern)).matcher(content)).toList
   }
+
+  /**
+   * pattern to use to get specification names from file contents
+   */
+  def specPattern(specType: String, pattern: String) = "\\s*"+specType+"\\s*(" + pattern + ")\\s*extends\\s*.*"
+
   /** @return the package declaration at the beginning of a file */
   def packageName(content: String): String = {
     def result(m: Matcher): Stream[String] = 
@@ -55,15 +61,17 @@ trait SpecificationsFinder extends FileSystem with Classes with ConsoleOutput {
     result(Pattern.compile(pattern).matcher(content)).mkString(".")
   }
   /**
-   * @return a <code>Specification</code> object from a className if that class is a <code>Specification</code> class.<br>
+   * @return a <code>SpecificationStructure</code> object from a className if that class is a <code>SpecificationStructure</code> class.<br>
    * Tries to load the class name and cast it to a specification
    *         None in case of an exception.
    */
-  def createSpecification(className: String): Option[Specification] = tryToCreateObject[Specification](className)
+  def createSpecification(className: String): Option[SpecificationStructure] =
+    tryToCreateObject[SpecificationStructure](className)
   /**
-   * @return a <code>Specification</code> object from a className if that class is a <code>Specification</code> class.<br>
+   * @return a <code>SpecificationStructure</code> object from a className if that class is a <code>SpecificationStructure</code> class.<br>
    * Tries to load the class name and cast it to a specification
    *         None in case of an exception.
    */
-  def createSpecification(className: String, printMessage: Boolean, printStackTrace: Boolean): Option[Specification] = createObject[Specification](className, printMessage, printStackTrace)
+  def createSpecification(className: String, printMessage: Boolean, printStackTrace: Boolean): Option[SpecificationStructure] =
+    createObject[SpecificationStructure](className, printMessage, printStackTrace)
 }
