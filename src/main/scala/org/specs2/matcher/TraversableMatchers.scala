db2723cdd22af7e8ecb8471c11119bfbb52e17db
etorreborre <etorreborre@yahoo.com>
2011-05-17 00:33:56 +1000

    added an implicit to be able to match the size of an Array

--- src/main/scala/org/specs2/matcher/TraversableMatchers.scala
+++ src/main/scala/org/specs2/matcher/TraversableMatchers.scala
@@ -68,11 +68,15 @@ trait TraversableBaseMatchers extends LazyParameters { outer =>
   }
 
   /** any scala collection has a size */
-  implicit def scalaTraversableIsSized[I <: TraversableOnce[_]] = new Sized[I] {
+  implicit def scalaTraversableIsSized[I <: TraversableOnce[_]]: Sized[I] = new Sized[I] {
     def size(t: I) = t.size
   }
+  /** any scala array has a size */
+  implicit def scalaArrayIsSized[T]: Sized[Array[T]] = new Sized[Array[T]] {
+    def size(t: Array[T]) = t.length
+  }
   /** any java collection has a size */
-  implicit def javaCollectionIsSized[T <: java.util.Collection[_]]  = new Sized[T] {
+  implicit def javaCollectionIsSized[T <: java.util.Collection[_]]: Sized[T] = new Sized[T] {
     def size(t: T) = t.size()
   }
   /** a regular string has a size, without having to be converted to an Traversable */
