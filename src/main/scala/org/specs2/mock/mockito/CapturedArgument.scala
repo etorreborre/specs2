c8498e10a47acef2878db32a71b32a4d74d325c3
etorreborre <etorreborre@yahoo.com>
2011-06-05 18:02:18 +1000

    fixed issue 20

--- src/main/scala/org/specs2/mock/mockito/CapturedArgument.scala
+++ src/main/scala/org/specs2/mock/mockito/CapturedArgument.scala
@@ -12,12 +12,14 @@ trait CapturedArgument {
   /** capture an argument of type T */
   def capture[T : ClassManifest]: ArgumentCapture[T] = new ArgumentCapture[T]
 
-  /** This class encapsulates an ArgumentCaptor */
-  class ArgumentCapture[T](implicit m: ClassManifest[T]) {
-    lazy private val captor: ArgumentCaptor[T] = ArgumentCaptor.forClass(m.erasure).asInstanceOf[ArgumentCaptor[T]]
-    def value = captor.getValue
-    def capture = captor.capture()
-  }
   /** this conversion allows to capture the parameter is a mocked call */
   implicit def captured[T](c: ArgumentCapture[T]): T = c.capture
 }
+
+/** This class encapsulates an ArgumentCaptor */
+class ArgumentCapture[T](implicit m: ClassManifest[T]) {
+  lazy private val captor: ArgumentCaptor[T] = ArgumentCaptor.forClass(m.erasure).asInstanceOf[ArgumentCaptor[T]]
+  def value = captor.getValue
+  def capture = captor.capture()
+}
+
