47c70eda1f34ef287f3d1b057ca36cd4f7422855
etorreborre <etorreborre@yahoo.com>
2011-08-22 18:36:47 +1000

    renamed a trait to avoid runtime issues

--- src/test/scala/org/specs2/reporter/StoringSpec.scala
+++ src/test/scala/org/specs2/reporter/StoringSpec.scala
@@ -5,46 +5,45 @@ import main._
 import ExecutedFragments._
 import mock.Mockito
 
-class StoringSpec extends SpecificationWithJUnit { def is =
-  
+class StoringSpec extends SpecificationWithJUnit { def is =       sequential ^
    "The statistics of a specification must"                                                        ^
      "be computed after execution"                                                                 ^
        "each executed fragment must have a stats member"                                           ^
-         "a Text fragment must have stats = 0"                                                     ! stats.e1^
-         "a failed example must have a stats = 1 failure"                                          ! stats.e2^
-         "the end of a specification must sum up all the results"                                  ! stats.e3^
+         "a Text fragment must have stats = 0"                                                     ! stats().e1^
+         "a failed example must have a stats = 1 failure"                                          ! stats().e2^
+         "the end of a specification must sum up all the results"                                  ! stats().e3^
                                                                                                    p^bt^
      "be stored"                                                                                   ^
-       "stored per specification name"                                                             ! stored.e1^
-       "each result also"                                                                          ! stored.e2^
-       "and retrieved per specification name"                                                      ! stored.e3^
+       "stored per specification name"                                                             ! stored().e1^
+       "each result also"                                                                          ! stored().e2^
+       "and retrieved per specification name"                                                      ! stored().e3^
                                                                                                    endp^
    "It is possible to compute the trends of the statistics"                                        ^
-     "between 2 runs"                                                                              ! trends.e1^
-     "the trends can be resetted"                                                                  ! trends.e2^
+     "between 2 runs"                                                                              ! trends().e1^
+     "the trends can be resetted"                                                                  ! trends().e2^
                                                                                                    end
 
    
-  trait Stored extends FragmentExecution with Mockito { outer =>
-    val repository = mock[StatisticsRepository]
-    val storing = new DefaultStoring {
+  trait StoringContext extends FragmentExecution with Mockito { outer =>
+    lazy val repository = mock[StatisticsRepository]
+    lazy val storing = new DefaultStoring {
       override lazy val repository = outer.repository
     }
-    implicit val arguments = Arguments() 
+    implicit def arguments = Arguments()
     def store(fs: Fragments)(implicit args: Arguments) = storing.store(args <| fs.arguments)(fs.fragments.map(executeFragment)).toList
 
     repository.getStatistics(any[SpecName]) returns None
 
   }
 
-   object stats extends Stored {
+   case class stats() extends StoringContext {
      def e1 = store("t1").filter(isExecutedText)(0).stats must_== Stats() 
      def e2 = store("e1" ! failure).filter(isExecutedResult)(0).stats must_== Stats(examples = 1, expectations = 1, failures = 1)
      def e3 = store("e0" ! success ^ "e1" ! failure).filter(isExecutedSpecEnd)(0).stats.toString must startWith ( 
        "Stats(examples = 2, successes = 1, expectations = 2, failures = 1")
    }
   
-   object stored extends Stored {
+   case class stored() extends StoringContext {
      def e1 = {
        store("t1":Fragments)
        there was atLeastOne(repository).storeStatistics(any[SpecName], any[Stats])
@@ -59,7 +58,7 @@ class StoringSpec extends SpecificationWithJUnit { def is =
      }
    }
 
-   object trends extends Stored {
+   case class trends() extends StoringContext {
      def e1 = {
        repository.getStatistics(any[SpecName]) returns Some(Stats(failures = 1))
        store("t1":Fragments).head.stats.trend must_== Some(Stats(failures = -1))
