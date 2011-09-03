1e52e7db787630c205972a3f8f19a6d4216b7668
etorreborre <etorreborre@yahoo.com>
2011-09-03 12:25:20 +1000

    added an example for the use of the specs2 matchers with JUnit

---
+++ src/test/scala/org/specs2/examples/JUnitSpec.scala
@@ -0,0 +1,32 @@
+package examples
+import org.junit._
+import org.specs2.matcher._
+
+/**
+ * This class shows how to use spec matchers in a JUnit specification
+ */
+class JUnitSpec extends JUnitMustMatchers {
+
+  @Test
+  def test1 {
+    "Hello world" must have size(11)
+  }
+
+  @Test
+  def test2 {
+    "Hello world" must startWith("Hello")
+  }
+
+  @Test
+  def test3 {
+    "Hello world" must endWith("world")
+  }
+
+  @Test
+  def test4 {
+    "Hello world2" must endWithWorld
+  }
+
+  // custom matchers can be created by coercing a function T => (Boolean, String, String) to Matcher[T]
+  def endWithWorld: Matcher[String] = (s: String) => (s.endsWith("world"), s+" ends with world", s+" doesn't end with world")
+}
