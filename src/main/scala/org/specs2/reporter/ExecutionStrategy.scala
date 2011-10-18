5276b8cf0e678b81b54a2730dc81bf5c7e2f3823
etorreborre <etorreborre@yahoo.com>
2011-10-19 06:56:36 +1100

    first trials to compute inside the promise

--- src/main/scala/org/specs2/reporter/ExecutionStrategy.scala
+++ src/main/scala/org/specs2/reporter/ExecutionStrategy.scala
@@ -2,7 +2,6 @@ package org.specs2
 package reporter
 
 import java.util.concurrent.Executors
-import execute.{ Error, Failure }
 import org.specs2.internal.scalaz._
 import Scalaz._
 import concurrent._
@@ -10,8 +9,7 @@ import Strategy.Executor
 import specification._
 import control.NamedThreadFactory
 import main.{ArgumentsArgs, Arguments}
-import scala.collection.immutable.List._
-import main.Arguments._
+import specification.FragmentExecution._
 
 /**
  * Generic trait for executing Fragments, which are sorted according to their dependencies
@@ -37,40 +35,60 @@ trait DefaultExecutionStrategy extends ExecutionStrategy with FragmentExecution 
    * If the stopOnFail argument is true, we check that the execution is ok before executing the next sequence.
    */
   def execute(implicit arguments: Arguments) = (spec: ExecutableSpecification) => {
-    val executed = spec.fs.foldLeft((Nil:List[ExecutedFragment], true)) { (res, fs) =>
-      val (executedFragments, executionOk) = res
-      val fsArgs = arguments <| fs.arguments
-      val executed = executeSequence(fs)(executionArgs(fsArgs, executionOk))
-      (executedFragments ++ executed, !fsArgs.stopOnFail || (executionOk && executed.forall(isOk(_))))
-    }._1
+//    val executed = spec.fs.foldLeft((promise(Seq[ExecutedFragment]()), true)) { (res, fs) =>
+//      val (executedFragments, executionOk) = res
+//      val fsArgs = arguments <| fs.arguments
+//      val executed = executeSequence(fs)(executionArgs(fsArgs, executionOk))
+//      ((executedFragments <**> executed)(_ ++ _), !fsArgs.stopOnFail || (executionOk && executed.forall(isOk(_))))
+//    }._1
+    val executed = spec.fs.traverse(execution).fragments
     ExecutedSpecification(spec.name, executed)
   }
 
   private def executionArgs(arguments: Arguments, previousExecutionOk: Boolean) =
     if (!arguments.stopOnFail || previousExecutionOk) arguments
     else (arguments <| args(skipAll=true))
+}
 
-  /**
-   * @return true if the executed fragment is not a Failure or an Error
-   */
-  private def isOk(e: ExecutedFragment) = e match {
-    case ExecutedResult(_,r,_,_,_) if r.isFailure || r.isError => false
-    case other                                                => true
+object DefaultExecutionStrategy {
+
+  def executionState(arguments: Arguments): State[(Boolean, Seq[ExecutedFragment]), Boolean] = state { (executionOk, fs) =>
+    val continueExecution = !arguments.stopOnFail || executionOk && fs.forall(isOk)
+    ((continueExecution, fs), continueExecution)
   }
 
-  private def executeSequence(fs: FragmentSeq)(implicit args: Arguments): Seq[ExecutedFragment] = {
+  def fragmentsExecution = (fs: Seq[ExecutedFragment]) => {
+    executeSequence(fs)(fs.args)
+  }
+
+  def execution: Function1[Seq[ExecutedFragment], PromisedExecutedFragments]= (fs: Seq[ExecutedFragment]) => {
+    new PromisedExecutedFragments()
+  }
+
+  def executeSequence(fs: FragmentSeq)(implicit args: Arguments): Promise[Seq[ExecutedFragment]] = {
     if (fs.fragments.size > 1 && !args.sequential)
       executeConcurrently(fs, args)
     else
-      fs.fragments.map(f => executeFragment(args)(f))
+      promise(fs.fragments.map(f => executeFragment(args)(f)))
   }
 
-  private def executeConcurrently(fs: FragmentSeq, args: Arguments) = {
+  def executeConcurrently(fs: FragmentSeq, args: Arguments) = {
     implicit val executor = Executors.newFixedThreadPool(args.threadsNb, new NamedThreadFactory("specs2.DefaultExecutionStrategy"))
     try {
-      fs.fragments.map(f => promise(executeFragment(args)(f))).sequence.get
+      fs.fragments.map(f => promise(executeFragment(args)(f))).sequence
     } finally {
       executor.shutdown()
     }
+  }
+  /**
+   * @return true if the executed fragment is not a Failure or an Error
+   */
+  def isOk(e: ExecutedFragment) = e match {
+    case ExecutedResult(_,r,_,_,_) if r.isFailure || r.isError => false
+    case other                                                 => true
+  }
+
+  class PromisedExecutedFragments {
+    def fragments:
   }
 }
--- src/main/scala/org/specs2/specification/ExecutedSpecification.scala
+++ src/main/scala/org/specs2/specification/ExecutedSpecification.scala
@@ -2,15 +2,19 @@ package org.specs2
 package specification
 import ExecutedFragment._
 import collection.Iterablex._
+import internal.scalaz._
+import concurrent._
+import Scalaz._
 
 /**
  * an executed specification with a name and a sequence of executed fragments
  */
-case class ExecutedSpecification(name: SpecName, fragments: Seq[ExecutedFragment]) {
-  def includedLinkedSpecifications: Seq[ExecutedSpecStart] = 
+case class ExecutedSpecification(name: SpecName, promised: Promise[Seq[ExecutedFragment]]) {
+  def fragments = promised.get
+  def includedLinkedSpecifications: Seq[ExecutedSpecStart] =
 	  fragments collect { case s @ ExecutedSpecStart(_,_,_) if s.isIncludeLink => s }
-		
-  def includedSeeOnlySpecifications: Seq[ExecutedSpecStart] = 
+
+  def includedSeeOnlySpecifications: Seq[ExecutedSpecStart] =
 	  fragments collect { case s @ ExecutedSpecStart(_,_,_) if s.isSeeOnlyLink => s }
 }
 
@@ -21,8 +25,8 @@ private[specs2]
 object ExecutedSpecification {
   def apply(fs: Seq[ExecutedFragment]): ExecutedSpecification = {
     fs match {
-      case (s @ ExecutedSpecStart(_,_,_)) +: rest => ExecutedSpecification(s.specName, fs)
-      case other                                  => ExecutedSpecification(SpecName(""), fs)
+      case (s @ ExecutedSpecStart(_,_,_)) +: rest => ExecutedSpecification(s.specName, promise(fs))
+      case other                                  => ExecutedSpecification(SpecName(""), promise(fs))
     }
   }
 }
