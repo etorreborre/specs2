b30edbd20970d5c597558071e54ac6a19009faf6
etorreborre <etorreborre@yahoo.com>
2011-11-12 10:51:10 +1100

    replacing futures with promises in the termination matchers and breaking them if the promised is not fulfilled when the time's up

--- src/main/scala/org/specs2/matcher/TerminationMatchers.scala
+++ src/main/scala/org/specs2/matcher/TerminationMatchers.scala
@@ -3,8 +3,9 @@ package matcher
 
 import time._
 import TimeConversions._
-import scala.actors._
-import Futures._
+import internal.scalaz._
+import Scalaz._
+import concurrent.Promise
 
 /**
  * This trait provides matchers to check if a block of code is terminating or not
@@ -24,15 +25,17 @@ trait TerminationBaseMatchers {
    */
   def terminate[T](retries: Int = 0, sleep: Duration = 100.millis): Matcher[T] = new Matcher[T] {
     def apply[S <: T](a: Expectable[S]) =
-      retry(retries, retries, sleep, a, future(a.value))
+      retry(retries, retries, sleep, a, promise(a.value))
 
-    def retry[S <: T](originalRetries: Int, retries: Int, sleep: Duration, a: Expectable[S], future: Future[S]): MatchResult[S] = {
+    def retry[S <: T](originalRetries: Int, retries: Int, sleep: Duration, a: Expectable[S], promise: Promise[S]): MatchResult[S] = {
       Thread.sleep(sleep.inMillis)
-      lazy val isSet = future.isSet
-      if (isSet || retries <= 0)
-        result(isSet, "the action terminates", "the action is blocking with retries="+originalRetries+" and sleep="+sleep.inMillis, a)
+      lazy val fulfilled = promise.fulfilled
+      if (fulfilled || retries <= 0) {
+        if (!fulfilled) promise.break
+        result(fulfilled, "the action terminates", "the action is blocking with retries="+originalRetries+" and sleep="+sleep.inMillis, a)
+      }
       else
-        retry(originalRetries, retries - 1, sleep, a, future)
+        retry(originalRetries, retries - 1, sleep, a, promise)
     }
   }
 }
--- src/test/scala/org/specs2/matcher/TerminationMatchersSpec.scala
+++ src/test/scala/org/specs2/matcher/TerminationMatchersSpec.scala
@@ -1,6 +1,8 @@
 package org.specs2
 package matcher
 
+import io.MockOutput
+
 class TerminationMatchersSpec extends Specification with TerminationMatchers { def is =
 
   "It is possible to check if a block of code terminates"                                          ^
@@ -12,12 +14,20 @@ class TerminationMatchersSpec extends Specification with TerminationMatchers { d
       "if it succeeds"                                                                             ! e3^
       "if it fails"                                                                                ! e4^
                                                                                                    p^
-    "We can write 'action must not terminate'"                                                     ! e5^
+    "if the termination fails, the computation is stopped"                                         ! e5^
+                                                                                                   p^
+    "We can write 'action must not terminate'"                                                     ! e6^
                                                                                                    end
 
   def e1 = { Thread.sleep(50) must terminate }
   def e2 = { (Thread.sleep(150) must terminate) returns "the action is blocking with retries=0 and sleep=100" }
   def e3 = { Thread.sleep(50) must terminate(retries=2, sleep=20.millis) }
   def e4 = { (Thread.sleep(100) must terminate(retries=3, sleep=20.millis)) returns "the action is blocking with retries=3 and sleep=20" }
-  def e5 = Thread.sleep(150) must not terminate
+  def e5 = {
+    val out = new MockOutput { }
+    val terminated = (1 to 5).foreach (i => {Thread.sleep(50); out.println(i) }) must not terminate(retries=5, sleep=20.millis)
+    Thread.sleep(300)
+    terminated and (out.messages must not contain("3"))
+  }
+  def e6 = { Thread.sleep(150) must not terminate }
 }
