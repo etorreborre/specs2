18acf08f0891e44cdce5b866f5d42fd498d4dc0a
etorreborre <etorreborre@yahoo.com>
2011-12-19 12:24:53 +1100

    added a comment for the index example

--- src/test/scala/org/specs2/examples/index.scala
+++ src/test/scala/org/specs2/examples/index.scala
@@ -7,6 +7,7 @@ class index extends Specification { def is =
 
   examplesLinks("Example specifications")
 
+  // see the SpecificationsFinder trait for the parameters of the 'specifications' method
   def examplesLinks(t: String) = specifications().foldLeft(t.title) { (res, cur) => res ^ see(cur) }
   
 }
