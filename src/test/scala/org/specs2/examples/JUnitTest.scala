532a9398d2026e8218bd6f80321cc0f827c7dd19
etorreborre <etorreborre@yahoo.com>
2011-12-20 09:54:32 +1100

    renamed the JUnitSpec class to JUnitTest because this is not a normal specification, it's just a JUnit TestCase

--- src/test/scala/org/specs2/examples/JUnitTest.scala
+++ src/test/scala/org/specs2/examples/JUnitTest.scala
@@ -5,7 +5,7 @@ import org.specs2.matcher._
 /**
  * This class shows how to use spec matchers in a JUnit specification
  */
-class JUnitSpec extends JUnitMustMatchers {
+class JUnitTest extends JUnitMustMatchers {
 
   @Test
   def test1 {
