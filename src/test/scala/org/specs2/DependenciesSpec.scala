72a5f56113a33c435a2ad13d0b1b02970cffbeed
etorreborre <etorreborre@yahoo.com>
2011-12-23 15:46:03 +1100

    commented specs2 design in layers

--- src/test/scala/org/specs2/DependenciesSpec.scala
+++ src/test/scala/org/specs2/DependenciesSpec.scala
@@ -7,15 +7,17 @@ class DependenciesSpec extends Specification with Analysis with Tags { def is = 
   The following dependencies must be enforced in specs2:
                                                                                           """ ^
   layers (
-    "runner",
-    "reporter",
-                                    // this class is excluded because it contains its own runner
+    "runner",                                  // specification runners, usually just taking the class name
+    "reporter",                                // execute and report the specification as html, console text, junit xml,....
+                                               // Specification structure: Fragments, Context, Statistics, Tags
+                            // this class is excluded because it contains its own runner
     "specification mutable".exclude("mutable.SpecificationWithJUnit"),
-    "mock      form",
-    "matcher",
-    "execute",
-    "reflect analysis time html",
-    "collection control io xml text main data").withPrefix("org.specs2")                  ^ section("slow") ^
+    "mock      form",                          // mocks definition/checks and forms definitions
+    "matcher",                                 // matchers and expectations
+    "execute",                                 // execution of anything that returns a Result
+    "reflect analysis time html",              // support classes for reflection, time measurement,...
+    "collection control io xml text main data" // utility classes for io, main arguments, control structures,...
+  ).withPrefix("org.specs2")                  ^ section("slow") ^
                                                                                           end
 
 }
