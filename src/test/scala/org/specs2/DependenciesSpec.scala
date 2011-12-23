d5a2719b4406fd2b7e5ae1e75140db14f88bcb3b
etorreborre <etorreborre@yahoo.com>
2011-12-23 16:26:13 +1100

    added the json package to the DependenciesSpec

--- src/test/scala/org/specs2/DependenciesSpec.scala
+++ src/test/scala/org/specs2/DependenciesSpec.scala
@@ -7,16 +7,16 @@ class DependenciesSpec extends Specification with Analysis with Tags { def is = 
   The following dependencies must be enforced in specs2:
                                                                                           """ ^
   layers (
-    "runner",                                  // specification runners, usually just taking the class name
-    "reporter",                                // execute and report the specification as html, console text, junit xml,....
-                                               // Specification structure: Fragments, Context, Statistics, Tags
+    "runner",                                       // specification runners, usually just taking the class name
+    "reporter",                                     // execute and report the specification as html, console text, junit xml,....
+                                                    // Specification structure: Fragments, Context, Statistics, Tags
                             // this class is excluded because it contains its own runner
     "specification mutable".exclude("mutable.SpecificationWithJUnit"),
-    "mock      form",                          // mocks definition/checks and forms definitions
-    "matcher",                                 // matchers and expectations
-    "execute",                                 // execution of anything that returns a Result
-    "reflect analysis time html",              // support classes for reflection, time measurement,...
-    "collection control io xml text main data" // utility classes for io, main arguments, control structures,...
+    "mock      form",                               // mocks definition/checks and forms definitions
+    "matcher",                                      // matchers and expectations
+    "execute",                                      // execution of anything that returns a Result
+    "reflect analysis time html",                   // support classes for reflection, time measurement,...
+    "collection control io xml text json main data" // utility classes for io, main arguments, control structures,...
   ).withPrefix("org.specs2")                  ^ section("slow") ^
                                                                                           end
 
