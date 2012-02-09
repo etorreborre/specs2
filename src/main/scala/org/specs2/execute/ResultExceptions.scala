b8e367697124133a24c92ce17f887def31698637
etorreborre <etorreborre@yahoo.com>
2012-02-10 07:07:44 +1100

    improved the FailureException so to ease the ScalaTest integration

--- src/main/scala/org/specs2/execute/ResultExceptions.scala
+++ src/main/scala/org/specs2/execute/ResultExceptions.scala
@@ -2,6 +2,10 @@ package org.specs2
 package execute
 
 /** this class allows to throw a failure in a Exception */
-case class FailureException(f: Failure) extends Exception
+case class FailureException(f: Failure) extends Exception {
+  override def getMessage = f.message
+  override def getCause = f.exception
+  override def getStackTrace = f.exception.getStackTrace
+}
 /** this class allows to throw a failure in a Exception */
 case class SkipException(f: Skipped) extends Exception
