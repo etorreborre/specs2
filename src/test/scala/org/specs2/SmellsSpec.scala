067f702fc2fbc26e957173fff38b8570b3aeb524
unknown <l034243@AU2104WP9002.infau.wbcau.westpac.com.au>
2012-02-15 17:10:11 +1100

    added the SmellsSpec back but it's still not passing

--- src/test/scala/org/specs2/SmellsSpec.scala
+++ src/test/scala/org/specs2/SmellsSpec.scala
@@ -3,17 +3,15 @@ import net.rosien.sniff._
 
 class SmellsSpec extends Specification {
 
-
   implicit val ignore = Ignores(
-    Ignore('NoLongLine, "src/main/scala/org/specs2/matcher/DataTable.scala",
-                        "src/main/scala/org/specs2/reporter/ConsoleNotifier.scala",
-                        "src/main/scala/org/specs2/matcher/ScalaCheckMatchers.scala")
+    Ignore('NoLongLines, "src/main/scala/org/specs2/matcher/DataTable.scala",
+                         "src/main/scala/org/specs2/reporter/ConsoleNotifier.scala",
+                         "src/main/scala/org/specs2/matcher/ScalaCheckMatchers.scala")
   )
 
   val IgnoredSmells = Set('NoJUnit, 'NoSpecs1, 'NoURL, 'NoThreadSleep)
 
   def is = "There should be no code smells" ^
-  ok
-    //CodeSnippets(Scala, Scala.snippets.smells.filterNot(s => IgnoredSmells(s.id)):_*).sniff("src/main/scala", "src/test/scala")
+           CodeSnippets(Scala, Scala.snippets.smells.filterNot(s => IgnoredSmells(s.id)):_*).sniff("src/main/scala", "src/test/scala")
 
 }
