e90e10a8ebf4db2dc10bce3faeebdd1804439aca
etorreborre <etorreborre@yahoo.com>
2012-03-20 22:42:20 +1100

    added ??? as a result

--- src/main/scala/org/specs2/execute/StandardResults.scala
+++ src/main/scala/org/specs2/execute/StandardResults.scala
@@ -13,5 +13,10 @@ trait StandardResults {
   def success = Success("success")
   def failure = Failure("failure")
   def skipped = Skipped("skipped")
+  // this will be removed in 2.10 and replaced by Predef.???
+  def ??? : Nothing = throw new NotImplementedError("not implemented")
 }
+// this will be switched with the real error class in scala 2.10
+case class NotImplementedError(msg: String) extends java.lang.Error(msg)
+
 object StandardResults extends StandardResults
