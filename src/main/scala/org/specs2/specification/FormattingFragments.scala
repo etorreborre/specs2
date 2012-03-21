619fdbf128a2a3ff00b21faf25dc793ce0ad13de
etorreborre <etorreborre@yahoo.com>
2012-03-22 08:35:12 +1100

    fixed the formatting with endp/endbr when using a mutable specification

--- src/main/scala/org/specs2/specification/FormattingFragments.scala
+++ src/main/scala/org/specs2/specification/FormattingFragments.scala
@@ -5,15 +5,15 @@ package specification
  * Set of fragments which can be used for formatting
  */
 trait FormattingFragments {
-  def p          = Fragments.createList(br, bt)
+  def p          = Fragments.createList(StandardFragments.Br(), StandardFragments.Backtab())
   def br         = StandardFragments.Br()
   def end        = StandardFragments.End()
   def t          = StandardFragments.Tab()
   def t(n: Int)  = StandardFragments.Tab(n)
   def bt         = StandardFragments.Backtab()
   def bt(n: Int) = StandardFragments.Backtab(n)
-  def endp       = Fragments.createList(end, br, br)
-  def endbr      = Fragments.createList(end, br)
+  def endp       = Fragments.createList(end, StandardFragments.Br(), StandardFragments.Br())
+  def endbr      = Fragments.createList(end, StandardFragments.Br())
 }
 private[specs2]
 object FormattingFragments extends FormattingFragments
