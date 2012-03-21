63b8ee9953d38908d2a33f2b0bc90f4b2ac3c554
etorreborre <etorreborre@yahoo.com>
2012-03-22 09:09:55 +1100

    added newp formatting fragment and using it with the MutableStack specs

--- src/main/scala/org/specs2/mutable/FormattingFragments.scala
+++ src/main/scala/org/specs2/mutable/FormattingFragments.scala
@@ -18,64 +18,68 @@ trait FormattingFragments extends specification.FormattingFragments { outer: Fra
   /**
    * This implicit allows to follow a fragment by a Formatting fragment
    */
-  implicit def fragmentAndFormattingFragment[T <: Fragment](f: T) = new FragmentAndFormattingFragment(f)
-  class FragmentAndFormattingFragment[T <: Fragment](private val f: T) {
-    def p          = outer.p
-    def br         = outer.br
-    def end        = outer.end
-    def t          = outer.t
-    def t(n: Int)  = outer.t(n)
-    def bt         = outer.bt
-    def bt(n: Int) = outer.bt(n)
-    def endp       = outer.endp
-    def endbr      = outer.endbr
+  implicit def fragmentAndFormattingFragment[T <: Fragment](f: T) = new FragmentAndFormattingFragment(() => f)
+  class FragmentAndFormattingFragment[T <: Fragment](private val f: () => T) {
+    def newp       = { outer.endp; f(); outer.p }
+    def p          = { f(); outer.p     }
+    def br         = { f(); outer.br    }
+    def end        = { f(); outer.end   }
+    def t          = { f(); outer.t     }
+    def t(n: Int)  = { f(); outer.t(n)  }
+    def bt         = { f(); outer.bt    }
+    def bt(n: Int) = { f(); outer.bt(n) }
+    def endp       = { f(); outer.endp  }
+    def endbr      = { f(); outer.endbr }
   }
   /**
    * This implicit allows to follow a Fragments object by a Formatting fragment
    */
-  implicit def fragmentsAndFormattingFragment(f: Fragments) = new FragmentsAndFormattingFragment(f)
-  class FragmentsAndFormattingFragment(private val f: Fragments) {
-    def p          = outer.p
-    def br         = outer.br
-    def end        = outer.end
-    def t          = outer.t
-    def t(n: Int)  = outer.t(n)
-    def bt         = outer.bt
-    def bt(n: Int) = outer.bt(n)
-    def endp       = outer.endp
-    def endbr      = outer.endbr
+  implicit def fragmentsAndFormattingFragment(f: Fragments) = new FragmentsAndFormattingFragment(() => f)
+  class FragmentsAndFormattingFragment(private val f: () => Fragments) {
+    def newp       = { outer.endp; f(); outer.p }
+    def p          = { f(); outer.p             }
+    def br         = { f(); outer.br            }
+    def end        = { f(); outer.end           }
+    def t          = { f(); outer.t             }
+    def t(n: Int)  = { f(); outer.t(n)          }
+    def bt         = { f(); outer.bt            }
+    def bt(n: Int) = { f(); outer.bt(n)         }
+    def endp       = { f(); outer.endp          }
+    def endbr      = { f(); outer.endbr         }
   }
 
   /**
    * This implicit allows to follow a FragmentsFragment object by a Formatting fragment
    */
-  implicit def fragmentsFragmentAndFormattingFragment(f: FragmentsFragment) = new FragmentsFragmentAndFormattingFragment(f)
-  class FragmentsFragmentAndFormattingFragment(private val f: FragmentsFragment) {
-    def p          = outer.p
-    def br         = outer.br
-    def end        = outer.end
-    def t          = outer.t
-    def t(n: Int)  = outer.t(n)
-    def bt         = outer.bt
-    def bt(n: Int) = outer.bt(n)
-    def endp       = outer.endp
-    def endbr      = outer.endbr
+  implicit def fragmentsFragmentAndFormattingFragment(f: FragmentsFragment) = new FragmentsFragmentAndFormattingFragment(() => f)
+  class FragmentsFragmentAndFormattingFragment(private val f: () => FragmentsFragment) {
+    def newp       = { outer.endp; f(); p }
+    def p          = { f(); outer.p       }
+    def br         = { f(); outer.br      }
+    def end        = { f(); outer.end     }
+    def t          = { f(); outer.t       }
+    def t(n: Int)  = { f(); outer.t(n)    }
+    def bt         = { f(); outer.bt      }
+    def bt(n: Int) = { f(); outer.bt(n)   }
+    def endp       = { f(); outer.endp    }
+    def endbr      = { f(); outer.endbr   }
   }
 
   /**
    * This implicit allows to follow a string object by a Formatting fragment
    */
-  implicit def textAndFormattingFragment(s: String) = new FragmentsFragmentAndFormattingFragment(s)
-  class TextAndFormattingFragment(private val s: String) {
-    def p          = { s.txt; outer.p     }
-    def br         = { s.txt; outer.br    }
-    def end        = { s.txt; outer.end   }
-    def t          = { s.txt; outer.t     }
-    def t(n: Int)  = { s.txt; outer.t(n)  }
-    def bt         = { s.txt; outer.bt    }
-    def bt(n: Int) = { s.txt; outer.bt(n) }
-    def endp       = { s.txt; outer.endp  }
-    def endbr      = { s.txt; outer.endbr }
+  implicit def textAndFormattingFragment(s: String) = new TextAndFormattingFragment(() => s)
+  class TextAndFormattingFragment(private val s: () => String) {
+    def newp       = { outer.endp; s().txt; outer.p }
+    def p          = { s().txt; outer.p     }
+    def br         = { s().txt; outer.br    }
+    def end        = { s().txt; outer.end   }
+    def t          = { s().txt; outer.t     }
+    def t(n: Int)  = { s().txt; outer.t(n)  }
+    def bt         = { s().txt; outer.bt    }
+    def bt(n: Int) = { s().txt; outer.bt(n) }
+    def endp       = { s().txt; outer.endp  }
+    def endbr      = { s().txt; outer.endbr }
   }
 
 }
--- src/test/scala/examples/MutableStackIsolatedSpec.scala
+++ src/test/scala/examples/MutableStackIsolatedSpec.scala
@@ -1,6 +1,7 @@
 package examples
 
 import org.specs2.mutable._
+import org.specs2.specification.AllExpectations
 
 /**
  * This is another way to write the StackSpec, using:
@@ -11,25 +12,25 @@ import org.specs2.mutable._
  * - the `<<` operator to create some text followed by a step (see the MutableGivenWhenThenSpec)
  * - formatting fragments operator to transform any string to a Text fragment followed by a formatting fragment
  */
-class MutableStackIsolatedSpec extends Specification { isolated; noindent
-  "A Stack with limited capacity can either be:".br
+class MutableStackIsolatedSpec extends Specification { isolated
+  "A Stack with limited capacity can either be:".txt
 
-  "1. Empty".br
+  "1. Empty".newp
   "when the stack is empty" <<
   { stack = emptyStack }
-  { stack must be empty }.eg;
+  { stack must beEmpty }.eg;
   { stack.top must throwA[NoSuchElementException] }.eg;
-  { stack.pop must throwA[NoSuchElementException] }.eg.br
+  { stack.pop must throwA[NoSuchElementException] }.eg
 
-  "2. Non-empty and not full".br
+  "2. Non-empty and not full".newp
   "when the stack is not empty and not full" <<
   { stack = normalStack }
   { stack must not be empty }.eg;
   { stack.top === normalStack.top; stack === normalStack }.eg;
   { stack.pop === normalStack.top; stack !== normalStack }.eg;
-  { stack push 1; stack.top === 1; stack !== normalStack }.eg.br
+  { stack push 1; stack.top === 1; stack !== normalStack }.eg
 
-  "3. Full".br
+  "3. Full".newp
   "when the stack is full" <<
   { stack = fullStack }
   { (stack push 1) must throwAn[Error] }.eg
@@ -44,28 +45,80 @@ class MutableStackIsolatedSpec extends Specification { isolated; noindent
 /**
  * The StackSpec with a mutable specification, isolated variables, autoexamples but no postfix operations
  */
-class MutableStackIsolated2Spec extends Specification { isolated; noindent
-  "A Stack with limited capacity can either be:".br
+class MutableStackIsolated2Spec extends Specification { isolated
+  "A Stack with limited capacity can either be:".p
 
   "1. Empty".br
-  "when the stack is empty"
+  "when the stack is empty".br
   step { stack = emptyStack }
   eg { stack must beEmpty }
   eg { stack.top must throwA[NoSuchElementException] }
-  eg { stack.pop must throwA[NoSuchElementException] }; br
+  eg { stack.pop must throwA[NoSuchElementException] }; endp
 
   "2. Non-empty and not full".br
-  "when the stack is not empty and not full"
+  "when the stack is not empty and not full".br
   step { stack = normalStack }
   eg { stack must not be empty }
   eg { stack.top === normalStack.top; stack === normalStack }
   eg { stack.pop === normalStack.top; stack !== normalStack }
-  eg { stack push 1; stack.top === 1; stack !== normalStack }; br
+  eg { stack push 1; stack.top === 1; stack !== normalStack }; endp
 
   "3. Full".br
-  "when the stack is full"
+  "when the stack is full".br
   step { stack = fullStack }
   eg { (stack push 1) must throwAn[Error] }
+
+  def emptyStack  = SizedStack(maxCapacity = 10, size = 0)
+  def normalStack = SizedStack(maxCapacity = 10, size = 2)
+  def fullStack   = SizedStack(maxCapacity = 10, size = 10)
+
+  var stack: SizedStack = emptyStack
+}
+
+/**
+ * The StackSpec with a mutable specification, isolated variables, no autoexamples and no postfix operations
+ * The AllExpectations trait is mixed in so that all expectations are verified in an Example
+ */
+class MutableStackIsolated3Spec extends Specification with AllExpectations { isolated
+  "A Stack with limited capacity can either be:".p
+
+  "1. Empty".br
+  "when the stack is empty" << {
+    stack = emptyStack
+  }
+  "it has no elements" >> {
+    stack must beEmpty
+  }
+  "it not possible to get the top element or to remove it" >> {
+    stack.top must throwA[NoSuchElementException]
+    stack.pop must throwA[NoSuchElementException]
+  }; endp
+
+  "2. Non-empty and not full".br
+  "when the stack is not empty and not full" << {
+    stack = normalStack
+  }
+  "it is possible to retrieve the top element, this doesn't modify the stack" >> {
+    stack.top === normalStack.top
+    stack === normalStack
+  }
+  "it is possible to remove the top element with 'pop', which modifies the stack" >> {
+    stack.pop === normalStack.top
+    stack !== normalStack
+  }
+  "it is possible to push an element on the top of the stack" >> {
+    stack push 1
+    stack.top === 1
+    stack !== normalStack
+  }; endp
+
+  "3. Full".br
+  "when the stack is full" << {
+    stack = fullStack
+  }
+  "it is not possible to push an element on top of the stack" >> {
+    (stack push 1) must throwAn[Error]
+  }
 
   def emptyStack  = SizedStack(maxCapacity = 10, size = 0)
   def normalStack = SizedStack(maxCapacity = 10, size = 2)
