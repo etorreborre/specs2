ac2777113aac588a205a5f81e30dc38445bc0d25
etorreborre <etorreborre@yahoo.com>
2012-03-28 00:20:28 +1100

    added new formatting fragments to indent examples locally

--- src/main/scala/org/specs2/mutable/FormattingFragments.scala
+++ src/main/scala/org/specs2/mutable/FormattingFragments.scala
@@ -18,72 +18,92 @@ trait FormattingFragments extends specification.FormattingFragments { outer: Fra
   /**
    * This implicit allows to follow a fragment by a Formatting fragment
    */
-  implicit def fragmentAndFormattingFragment[T <: Fragment](f: T) = new FragmentAndFormattingFragment(() => f)
+  implicit def fragmentAndFormattingFragment[T <: Fragment](f: =>T) = new FragmentAndFormattingFragment(() => f)
   class FragmentAndFormattingFragment[T <: Fragment](private val f: () => T) {
-    def newp       = { outer.endp; f(); outer.p }
-    def newbr      = { outer.endp; f(); outer.br }
-    def p          = { f(); outer.p     }
-    def br         = { f(); outer.br    }
-    def end        = { f(); outer.end   }
-    def t          = { f(); outer.t     }
-    def t(n: Int)  = { f(); outer.t(n)  }
-    def bt         = { f(); outer.bt    }
-    def bt(n: Int) = { f(); outer.bt(n) }
-    def endp       = { f(); outer.endp  }
-    def endbr      = { f(); outer.endbr }
+    def newp        = { outer.end; f(); outer.endp }
+    def newbr       = { outer.end; f(); outer.endbr }
+    def p           = { f(); outer.p     }
+    def br          = { f(); outer.br    }
+    def end         = { f(); outer.end   }
+    def t           = { f(); outer.t     }
+    def t(n: Int)   = { f(); outer.t(n)  }
+    def bt          = { f(); outer.bt    }
+    def bt(n: Int)  = { f(); outer.bt(n) }
+    def endp        = { f(); outer.endp  }
+    def endbr       = { f(); outer.endbr }
+    def lt          = { outer.t    ; f(); outer.bt    }
+    def lt(n: Int)  = { outer.t(n) ; f(); outer.bt(n) }
+    def blt         = { outer.bt   ; f(); outer.t     }
+    def blt(n: Int) = { outer.bt(n); f(); outer.t(n)  }
   }
   /**
    * This implicit allows to follow a Fragments object by a Formatting fragment
    */
-  implicit def fragmentsAndFormattingFragment(f: Fragments) = new FragmentsAndFormattingFragment(() => f)
+  implicit def fragmentsAndFormattingFragment(f: =>Fragments) =
+    new FragmentsAndFormattingFragment(() => f)
+
   class FragmentsAndFormattingFragment(private val f: () => Fragments) {
-    def newp       = { outer.endp; f(); outer.p }
-    def newbr      = { outer.endp; f(); outer.br }
-    def p          = { f(); outer.p             }
-    def br         = { f(); outer.br            }
-    def end        = { f(); outer.end           }
-    def t          = { f(); outer.t             }
-    def t(n: Int)  = { f(); outer.t(n)          }
-    def bt         = { f(); outer.bt            }
-    def bt(n: Int) = { f(); outer.bt(n)         }
-    def endp       = { f(); outer.endp          }
-    def endbr      = { f(); outer.endbr         }
+    def newp        = { outer.end; f(); outer.endp }
+    def newbr       = { outer.end; f(); outer.endbr }
+    def p           = { f(); outer.p             }
+    def br          = { f(); outer.br            }
+    def end         = { f(); outer.end           }
+    def t           = { f(); outer.t             }
+    def t(n: Int)   = { f(); outer.t(n)          }
+    def bt          = { f(); outer.bt            }
+    def bt(n: Int)  = { f(); outer.bt(n)         }
+    def endp        = { f(); outer.endp          }
+    def endbr       = { f(); outer.endbr         }
+    def lt          = { outer.t    ; f(); outer.bt    }
+    def lt(n: Int)  = { outer.t(n) ; f(); outer.bt(n) }
+    def blt         = { outer.bt   ; f(); outer.t     }
+    def blt(n: Int) = { outer.bt(n); f(); outer.t(n)  }
   }
 
   /**
    * This implicit allows to follow a FragmentsFragment object by a Formatting fragment
    */
-  implicit def fragmentsFragmentAndFormattingFragment(f: FragmentsFragment) = new FragmentsFragmentAndFormattingFragment(() => f)
+  implicit def fragmentsFragmentAndFormattingFragment(f: =>FragmentsFragment) =
+    new FragmentsFragmentAndFormattingFragment(() => f)
+
   class FragmentsFragmentAndFormattingFragment(private val f: () => FragmentsFragment) {
-    def newp       = { outer.endp; f(); p }
-    def newbr      = { outer.endp; f(); outer.br }
-    def p          = { f(); outer.p       }
-    def br         = { f(); outer.br      }
-    def end        = { f(); outer.end     }
-    def t          = { f(); outer.t       }
-    def t(n: Int)  = { f(); outer.t(n)    }
-    def bt         = { f(); outer.bt      }
-    def bt(n: Int) = { f(); outer.bt(n)   }
-    def endp       = { f(); outer.endp    }
-    def endbr      = { f(); outer.endbr   }
+    def newp        = { outer.end; f(); outer.endp }
+    def newbr       = { outer.end; f(); outer.endbr }
+    def p           = { f(); outer.p       }
+    def br          = { f(); outer.br      }
+    def end         = { f(); outer.end     }
+    def t           = { f(); outer.t       }
+    def t(n: Int)   = { f(); outer.t(n)    }
+    def bt          = { f(); outer.bt      }
+    def bt(n: Int)  = { f(); outer.bt(n)   }
+    def endp        = { f(); outer.endp    }
+    def endbr       = { f(); outer.endbr   }
+    def lt          = { outer.t    ; f(); outer.bt    }
+    def lt(n: Int)  = { outer.t(n) ; f(); outer.bt(n) }
+    def blt         = { outer.bt   ; f(); outer.t     }
+    def blt(n: Int) = { outer.bt(n); f(); outer.t(n)  }
   }
 
   /**
    * This implicit allows to follow a string object by a Formatting fragment
    */
-  implicit def textAndFormattingFragment(s: String) = new TextAndFormattingFragment(() => s)
+  implicit def textAndFormattingFragment(s: =>String) = new TextAndFormattingFragment(() => s)
   class TextAndFormattingFragment(private val s: () => String) {
-    def newp       = { outer.endp; s().txt; outer.p }
-    def newbr      = { outer.endp; s().txt; outer.br }
-    def p          = { s().txt; outer.p     }
-    def br         = { s().txt; outer.br    }
-    def end        = { s().txt; outer.end   }
-    def t          = { s().txt; outer.t     }
-    def t(n: Int)  = { s().txt; outer.t(n)  }
-    def bt         = { s().txt; outer.bt    }
-    def bt(n: Int) = { s().txt; outer.bt(n) }
-    def endp       = { s().txt; outer.endp  }
-    def endbr      = { s().txt; outer.endbr }
+    def newp        = { outer.end; s().txt; outer.endp }
+    def newbr       = { outer.end; s().txt; outer.endbr }
+    def p           = { s().txt; outer.p     }
+    def br          = { s().txt; outer.br    }
+    def end         = { s().txt; outer.end   }
+    def t           = { s().txt; outer.t     }
+    def t(n: Int)   = { s().txt; outer.t(n)  }
+    def bt          = { s().txt; outer.bt    }
+    def bt(n: Int)  = { s().txt; outer.bt(n) }
+    def endp        = { s().txt; outer.endp  }
+    def endbr       = { s().txt; outer.endbr }
+    def lt          = { outer.t    ; s(); outer.bt    }
+    def lt(n: Int)  = { outer.t(n) ; s(); outer.bt(n) }
+    def blt         = { outer.bt   ; s(); outer.t     }
+    def blt(n: Int) = { outer.bt(n); s(); outer.t(n)  }
   }
 
 }
