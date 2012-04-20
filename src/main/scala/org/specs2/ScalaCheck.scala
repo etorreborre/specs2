b3b8373cc883175db033f41425c4971ae10d546a
etorreborre <etorreborre@yahoo.com>
2012-04-20 16:02:01 +1000

    fixed the auto-examples when using eg with ScalaCheck

--- src/main/scala/org/specs2/ScalaCheck.scala
+++ src/main/scala/org/specs2/ScalaCheck.scala
@@ -1,8 +1,9 @@
 package org.specs2
 
-import matcher.Parameters
+import matcher.{MatchResult, Parameters, MatchersImplicits}
 import specification._
 import org.scalacheck.Prop
+import MatchersImplicits._
 /**
  * The ScalaCheck trait can be used to access ScalaCheck matchers
  */
@@ -13,6 +14,25 @@ trait ScalaCheck extends matcher.ScalaCheckMatchers with AutoExamples { this: Fr
   implicit def propFragments(expression: =>Prop)(implicit p: Parameters): Fragments = resultFragments(checkProperty(expression)(p))
 
   implicit def propExample(expression: =>Prop)(implicit p: Parameters) = resultExample(checkProperty(expression)(p))
+
+  override implicit def aMatchResultExample(expression: =>MatchResult[_]): ToMatchResultExample2 =
+    new ToMatchResultExample2(expression)
+  class ToMatchResultExample2(ex: =>MatchResult[_]) extends ToMatchResultExample(ex) {
+    override def eg = createExample(ex.toResult, 15)
+  }
+  override def eg(expression: =>MatchResult[_]): Example = createExample(expression.toResult)
+
+  override implicit def aBooleanExample(expression: =>Boolean): ToBooleanExample2 = new ToBooleanExample2(expression)
+  class ToBooleanExample2(ex: =>Boolean) extends ToBooleanExample(ex) {
+    override def eg = createExample(toResult(ex), 15)
+  }
+  override def eg(expression: =>Boolean): Fragments = createExample(toResult(expression))
+
+  override implicit def aResultExample(expression: =>execute.Result): ToResultExample2 = new ToResultExample2(expression)
+  class ToResultExample2(ex: =>execute.Result) extends ToResultExample(ex) {
+    override def eg = createExample(ex, 15)
+  }
+  override def eg(expression: =>execute.Result): Fragment = createExample(expression)
 
   private[specs2] override def createExample(expression: =>execute.Result, depth: Int = 16): Example =
     super.createExample(expression, depth)
