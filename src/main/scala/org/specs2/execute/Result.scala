e821bd1e4e43db2482167eb56102275547253c28
Eric Torreborre <etorreborre@yahoo.com>
2012-05-18 19:06:42 +1000

    improved the logical combinators for Skipped

--- src/main/scala/org/specs2/execute/Result.scala
+++ src/main/scala/org/specs2/execute/Result.scala
@@ -352,6 +352,27 @@ case class Pending(m: String = "")  extends Result(m) { outer =>
     override val expectationsNb = n
   }
 
+  override def and(res: =>Result): Result = {
+    val r = res
+    r match {
+      case s @ Success(m, exp)    => s
+      case f @ Failure(_,_,_,_)   => f
+      case e @ Error(_,_)         => e
+      case DecoratedResult(d, r1) => DecoratedResult(d, and(r1))
+      case _                      => super.and(r)
+    }
+  }
+
+  override def or(res: =>Result): Result = {
+    val r = res
+    r match {
+      case s @ Success(m, exp)      => s
+      case f @ Failure(m, e, st, d) => f
+      case DecoratedResult(d, r1)   => DecoratedResult(d, or(r1))
+      case _                        => super.or(r)
+    }
+  }
+
   override def isPending: Boolean = true
 }
 /** 
@@ -363,6 +384,27 @@ case class Skipped(m: String = "", e: String = "")  extends Result(m, e) { outer
   def mute = Skipped()
   def setExpectationsNb(n: Int): Result = new Skipped(m) {
     override val expectationsNb = n
+  }
+
+  override def and(res: =>Result): Result = {
+    val r = res
+    r match {
+      case s @ Success(m, exp)    => s
+      case f @ Failure(_,_,_,_)   => f
+      case e @ Error(_,_)         => e
+      case DecoratedResult(d, r1) => DecoratedResult(d, and(r1))
+      case _                      => super.and(r)
+    }
+  }
+
+  override def or(res: =>Result): Result = {
+    val r = res
+    r match {
+      case s @ Success(m, exp)    => s
+      case f @ Failure(_,_,_,_)   => f
+      case DecoratedResult(d, r1) => DecoratedResult(d, or(r1))
+      case _                      => super.or(r)
+    }
   }
 
   override def isSkipped: Boolean = true
