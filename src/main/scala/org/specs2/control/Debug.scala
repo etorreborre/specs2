e8a6915efda1998745ad06db23e0b6501cc3fdd1
Eric Torreborre <etorreborre@yahoo.com>
2012-06-21 11:55:21 +1000

    added a new pp method where it is possible to pass a show method

--- src/main/scala/org/specs2/control/Debug.scala
+++ src/main/scala/org/specs2/control/Debug.scala
@@ -9,17 +9,19 @@ package control
  *  will print 'this string' and pass it to the rest of the expectation
  *
  */
-trait Debug {
+trait Debug extends ImplicitParameters {
   
   implicit def debug[T](t: =>T): Debuggable[T] = new Debuggable(t)
   class Debuggable[T](t: =>T) {
     lazy val value = t
     /** print the object to the console and return it */
     def pp: T = { Console.println(value); value }
-    /** print the object to the console and return it, if the condition is satisfied*/
-    def pp(condition: Boolean): T = pp(v => condition)
-    /** print the object to the console and return it, if the condition is satisfied*/
+    /** print the object to the console and return it, if the condition is satisfied */
+    def pp(condition: Boolean): T = pp((t: T) => condition)
+    /** print the object to the console and return it, if the condition is satisfied */
     def pp(condition: T => Boolean): T = if (condition(value)) pp else value
+    /** print the object to the console with a specific function and return it */
+    def pp(show: T => String)(implicit p: ImplicitParam): T = { show(value).pp; value }
   }
 
 }
