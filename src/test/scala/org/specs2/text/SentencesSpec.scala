f29644169bc3d60238649be22c1ddd86fbed2cb1
etorreborre <etorreborre@yahoo.com>
2012-10-29 17:46:06 +1100

    added was and were for sentences negation

--- src/test/scala/org/specs2/text/SentencesSpec.scala
+++ src/test/scala/org/specs2/text/SentencesSpec.scala
@@ -13,6 +13,8 @@ class SentencesSpec extends Specification with Tables with Sentences {
     "it does a"            ! "it does not a"         ! true         |
     "it has changed"       ! "it has not changed"    ! true         |
     "it will do"           ! "it will not do"        ! true         |
+    "it was a"             ! "it was not a"          ! true         |
+    "they were a"          ! "they were not a"       ! true         |
     "it can do"            ! "it can not do"         ! true         |
     "it could do"          ! "it could not do"       ! true         |
     "it must do"           ! "it must not do"        ! true         |
@@ -21,8 +23,10 @@ class SentencesSpec extends Specification with Tables with Sentences {
     "it isn't a"           ! "it is a"               ! false        |
     "they aren't a"        ! "they are a"            ! false        |
     "it does a"            ! "it does not a"         ! true         |
-    "it does not have a"   ! "it has a"        ! false        |
+    "it does not have a"   ! "it has a"              ! false        |
     "it won't do"          ! "it will do"            ! false        |
+    "it wasn't a"          ! "it was a"              ! false        |
+    "they weren't a"       ! "they were a"           ! false        |
     "it can't do"          ! "it can do"             ! false        |
     "it couldn't do"       ! "it could do"           ! false        |
     "it mustn't do"        ! "it must do"            ! false        |
