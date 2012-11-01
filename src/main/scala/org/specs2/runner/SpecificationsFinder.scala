83fedce6dc1d89a6347d205d45f2ad6832ab2792
etorreborre <etorreborre@yahoo.com>
2012-11-01 11:27:45 +1100

    fixed #115. Better parsing of source files for package directives

--- src/main/scala/org/specs2/runner/SpecificationsFinder.scala
+++ src/main/scala/org/specs2/runner/SpecificationsFinder.scala
@@ -6,14 +6,13 @@ import io._
 import reflect.Classes
 import specification.SpecificationStructure
 import main.Arguments
-import control.Stacktraces
+import text.SourceFile
 
 /**
  * This trait loads specifications found on a given source directory based
  * on a regular expression representing the Specification name, usually .*Spec
  */
-trait SpecificationsFinder extends FileSystem with Classes with ConsoleOutput {
-  private lazy val CLASSNAME_REGEX = "([\\p{L}_$][\\p{L}\\p{N}_$]*\\.)*[\\p{L}_$][\\p{L}\\p{N}_$]*".r
+trait SpecificationsFinder extends FileSystem with Classes with ConsoleOutput with SourceFile {
   /**
    * @param path a path to a directory containing scala files (it can be a glob: i.e. "dir/**/*spec.scala")
    * @param pattern a regular expression which is supposed to match an object name extending a Specification
@@ -58,41 +57,10 @@ trait SpecificationsFinder extends FileSystem with Classes with ConsoleOutput {
    }
 
   /**
-   * adds possible specification class names found in the file <code>filePath</code><br>
-   * The specification pattern is: "\\s*object\\s*(" + pattern + ")\\s*extends\\s*.*Spec.*\\s*\\{"
-   * This may be later extended to support other arbitrary patterns
-   *
-   * @param packageName the base package for the class names
-   * @param content content of the file
-   * @param pattern a regular expression which is supposed to match an object name extending a Specification
-   */
-  def classNames(packageName: String, content: String, pattern: Pattern, suffix: String, verbose: Boolean = false): Seq[String] = {
-
-    def result(m: Matcher): Stream[String] = 
-      if (m.find) { 
-    	  val fullName = List(packageName, m.group(1).trim).mkString(".") + suffix   
-    	  Stream.cons(fullName, result(m))
-      } else Stream.empty
-
-    val found = result(pattern.matcher(content)).toList
-    if (verbose && found.nonEmpty) println("found the following specifications: "+found.mkString(","))
-    found.filter(c => CLASSNAME_REGEX.pattern.matcher(c).matches)
-  }
-
-  /**
    * pattern to use to get specification names from file contents
    */
   def specPattern(specType: String, pattern: String) = "\\s*"+specType+"\\s*(" + pattern + ")\\s*extends\\s*.*"
 
-  /** @return the package declaration at the beginning of a file */
-  def packageName(content: String): String = {
-    def result(m: Matcher): Stream[String] = 
-      if (m.find) Stream.cons(m.group(1).replace(";", "").trim, result(m))
-      else Stream.empty
-      
-	  val pattern = "\\s*package\\s*(.+)\\s*"
-    result(Pattern.compile(pattern).matcher(content)).mkString(".")
-  }
   /**
    * @return a <code>SpecificationStructure</code> object from a className if that class is a <code>SpecificationStructure</code> class.<br>
    * Tries to load the class name and cast it to a specification
