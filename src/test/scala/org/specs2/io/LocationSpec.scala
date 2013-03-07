9475cdb2d2e034864a346bef1cc83949b4bb3a99
etorreborre <etorreborre@yahoo.com>
2013-03-07 12:08:40 +1100

    fixed the LocationSpec

--- src/test/scala/org/specs2/io/LocationSpec.scala
+++ src/test/scala/org/specs2/io/LocationSpec.scala
@@ -63,10 +63,10 @@ class LocationSpec extends Specification {
     }
   }
 
-  "The FragmentsFragment::def ^(t: String) = fs add Text(t) method must be at line 156" >> {
+  "The FragmentsFragment::def ^(t: String) = fs add Text(t) method must be at line 157. Check the FragmentsBuilder file and fix the Location.scala code" >> {
     val lines = Source.fromFile("src/main/scala/org/specs2/specification/FragmentsBuilder.scala").getLines.toSeq
     // line 156 is index 155
-    lines.zipWithIndex.collect { case (line, 155) => line }.headOption must beSome.which { line: String =>
+    lines.zipWithIndex.collect { case (line, 156) => line }.headOption must beSome.which { line: String =>
       line must contain("def ^(t: String) = fragments add Text(t)")
     }
   }
