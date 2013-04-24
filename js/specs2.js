c933713e502ac6e272074c3ad55fcd49c1faad92
Clinton <clinton.freeman@nicta.com.au>
2013-04-24 16:41:18 +1000

    special javascript refactor.

--- js/specs2.js
+++ js/specs2.js
@@ -1,15 +1,16 @@
 function init() {  prettyPrint(); };
-      /* found on : http://www.tek-tips.com/faqs.cfm?fid=6620 */
-      String.prototype.endsWith = function(str) { return (this.match(str+'$') == str) };
-      function changeWidth(id,width) {  document.getElementById(id).style.width = width; };
-      function changeMarginLeft(id, margin) { document.getElementById(id).style.marginLeft = margin; };
-      function toggleImage(image) {
-        if (image.src.endsWith('./images/expanded.gif'))
-          image.src = './images/collapsed.gif';
-        else
-          image.src = './images/expanded.gif';
-      };
-      function showHide(id) {
-        element = document.getElementById(id);
-        element.style.display = (element.style.display == 'block')? 'none' : 'block';
-      };
+
+/* found on : http://www.tek-tips.com/faqs.cfm?fid=6620 */
+String.prototype.endsWith = function(str) { return (this.match(str+'$') == str) };
+function changeWidth(id,width) {  document.getElementById(id).style.width = width; };
+function changeMarginLeft(id, margin) { document.getElementById(id).style.marginLeft = margin; };
+function toggleImage(image) {
+  if (image.src.endsWith('./images/expanded.gif'))
+    image.src = './images/collapsed.gif';
+  else
+    image.src = './images/expanded.gif';
+};
+function showHide(id) {
+  element = document.getElementById(id);
+  element.style.display = (element.style.display == 'block')? 'none' : 'block';
+};
