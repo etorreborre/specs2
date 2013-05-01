b72a7bb8c7d414ecc1147bd0776d235ae2d20082
etorreborre <etorreborre@yahoo.com>
2013-05-02 00:31:16 +1000

    removed the snippets library scripts, the functionality is already covered

--- src/test/scala/examples/SnippetsScriptSpec.scala
+++ src/test/scala/examples/SnippetsScriptSpec.scala
@@ -1,33 +1,30 @@
 package examples
 
 import org.specs2._
-import specification.script._
+import specification._
 
-class SnippetsScriptSpec extends Specification with Snippets { def is = s2"""      ${library.start}
+class SnippetsScriptSpec extends Specification with Snippets { def is = s2"""
 
  This is some documentation for an API where pieces of code can be inserted
 
-  > snippet 1 <
+  $s1
 
  And also here
 
-  > snippet 2 <
+  $s2
 
- This is as simple as that!                                                     ${library.end}
+ This is as simple as that!
 """
+
+
 
   lazy val s1 = snippet {
     1 +
     1 == 2
-  }
+  }.offsetIs(-4)
 
   lazy val s2 = snippet {
     2 + 2
-  }.eval
-
-  lazy val library =
-    SnippetsLibrary("simple").
-      add("snippet 1")(s1).
-      add("snippet 2")(s2)
+  }.offsetIs(-4).eval
 
 }
