12b6d4c0af8d2561fac72abd3dc4ab76156161f4
etorreborre <etorreborre@yahoo.com>
2013-07-25 14:45:42 +1000

    fixed the presentation of the TextResultOutput by reusing the LineLoggerResultOutput

--- src/main/scala/org/specs2/reporter/LineLoggerOutput.scala
+++ src/main/scala/org/specs2/reporter/LineLoggerOutput.scala
@@ -10,11 +10,10 @@ import org.specs2.execute.Result
  * This outputs the textual results of an executed specification to a LineLogger
  */
 trait LineLoggerOutput extends ResultOutput with LineLogger {
-  private val output = new TextResultOutput
   private val buffer = new StringBuilder
 
   private def info(msg: String)(implicit args: Arguments) {
-    buffer.append(output.offset(msg))
+    buffer.append(offset(msg))
   }
 
   def flushText(force: Boolean = false) = {
@@ -36,17 +35,21 @@ trait LineLoggerOutput extends ResultOutput with LineLogger {
   def printSuccess(message: String)(implicit args: Arguments)                      = info(message)
   def printSkipped(message: String)(implicit args: Arguments)                      = info(message)
   def printPending(message: String)(implicit args: Arguments)                      = info(message)
-  def printFailure(message: String)(implicit args: Arguments)                      = { flushText(force=true); failureLog(output.offset(message)) }
-  def printError(message: String)(implicit args: Arguments)                        = { flushText(force=true); errorLog(output.offset(message)) }
+  def printFailure(message: String)(implicit args: Arguments)                      = { flushText(force=true); failureLog(offset(message)) }
+  def printError(message: String)(implicit args: Arguments)                        = { flushText(force=true); errorLog(offset(message)) }
 
   def printSpecStartTitle(message: String, stats: Stats)(implicit args: Arguments) = printSpecStartName(message, stats)
   def printSpecStartName(message: String, stats: Stats)(implicit args: Arguments)  = { flushText(); info(message) }
   def printStats(message: String)(implicit args: Arguments)                        = { info(message); flushText(force=true) }
-  def status(result: Result)(implicit args: Arguments): String                     = output.status(result)
+  def status(result: Result)(implicit args: Arguments): String                     = result.coloredStatus(args) + " "
 
   def printMessage(message: String)(implicit args: Arguments)                      = printLines(args.textColor(message))
   def printLine(message: String)(implicit args: Arguments)                         = { info(message); flushText(force=true) }
-  def printLines(message: String)(implicit args: Arguments)                        = printLine(output.offset(message))
+
+  def printLines(message: String)(implicit args: Arguments)                        = printLine(offset(message))
+  /** add an offset to the message */
+  private def offset(message: String)(implicit args: Arguments) = message.offset(args.offset)
+
 }
 
 /**
--- src/main/scala/org/specs2/reporter/TextResultOutput.scala
+++ src/main/scala/org/specs2/reporter/TextResultOutput.scala
@@ -10,35 +10,11 @@ import text.Trim._
 /**
  * Implementation of the ResultOutput trait as Text
  */
-class TextResultOutput extends ResultOutput with ConsoleOutput {
+class TextResultOutput extends LineLoggerOutput with ConsoleOutput {
 
-  def printSpecStartName(message: String, stats: Stats)(implicit args: Arguments)  = printLines(args.textColor(message))
-  def printSpecStartTitle(message: String, stats: Stats)(implicit args: Arguments) = printLines(args.textColor(message))
-  def printSeeLink       (message: String, stats: Stats)(implicit args: Arguments) = printLines(status(stats.result)+args.textColor(message))
-  def printSuccess(message: String)(implicit args: Arguments)                      = printLines(args.successColor(message))
-  def printFailure(message: String)(implicit args: Arguments)                      = printLines(args.failureColor(message))
-  def printError(message: String)(implicit args: Arguments)                        = printLines(args.errorColor(message))
-  def printSkipped(message: String)(implicit args: Arguments)                      = printLines(args.skippedColor(message))
-  def printPending(message: String)(implicit args: Arguments)                      = printLines(args.pendingColor(message))
-  def printText(message: String)(implicit args: Arguments)                         = printLines(args.textColor(message))
-  def printStats(message: String)(implicit args: Arguments)                        = printLines(message)
-  def status(result: Result)(implicit args: Arguments): String                     = result.coloredStatus(args) + " "
-  
-  /**
-   * print some text, splitting it on several lines
-   */
-  def printMessage(message: String)(implicit args: Arguments) = printLines(args.textColor(message))
-  
-  def printLines(message: String)(implicit args: Arguments) = printLine(offset(message))
-
-  /**
-   * print one line
-   */
-  def printLine(message: String)(implicit args: Arguments) = offset(message).split("\n", -1).map(println)
-
-  /** add an offset to the message */
-  def offset(message: String)(implicit args: Arguments) =
-    message.offset(args.offset)
+  def infoLog(msg: String)    = println(msg)
+  def failureLog(msg: String) = println(msg)
+  def errorLog(msg: String)   = println(msg)
 
 }
 
