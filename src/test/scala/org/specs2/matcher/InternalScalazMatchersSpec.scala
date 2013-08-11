824ba7935981be13b9d57569015866a7c0bf5c0a
etorreborre <etorreborre@yahoo.com>
2013-08-11 18:07:57 +1000

    fixed a compilation error for the tests

--- src/test/scala/org/specs2/matcher/InternalScalazMatchersSpec.scala
+++ src/test/scala/org/specs2/matcher/InternalScalazMatchersSpec.scala
@@ -6,12 +6,12 @@ import scalaz._
 class InternalScalazMatchersSpec extends Specification with InternalScalazMatchers { def is = s2"""
 
   Validation
-  ${ Success[String, Int](1) must succeedWith(1) }
-  ${ Success[String, Int](1) must beSuccessful }
-  ${ (Success[String, Int](1) must succeedWith(2)) returns("Success(1) is not a Success(2)") }
-  ${ (Success[String, Int](1) must failWith("bad")) returns("Success(1) is not a Failure(bad)") }
-  ${ Failure[String, Int]("bad") must failWith("bad") }
-  ${ (Failure[String, Int]("bad") must failWith("boo")) returns("Failure(bad) is not a Failure(boo)") }
-  ${ (Failure[String, Int]("bad") must succeedWith(2)) returns("Failure(bad) is not a Success(2)") }
-                                                                                                                """
+  ${ Success(1) must succeedWith(1) }
+  ${ Success(1) must beSuccessful }
+  ${ (Success(1) must succeedWith(2)) returns("Success(1) is not a Success(2)") }
+  ${ (Success(1) must failWith("bad")) returns("Success(1) is not a Failure(bad)") }
+  ${ Failure("bad") must failWith("bad") }
+  ${ (Failure("bad") must failWith("boo")) returns("Failure(bad) is not a Failure(boo)") }
+  ${ (Failure("bad") must succeedWith(2)) returns("Failure(bad) is not a Success(2)") }
+                                                                                                """
 }
