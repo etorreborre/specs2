6149cea845bffaeffb7fea0b77837dfdf858a02f
Bruno Bieth <biethb@gmail.com>
2013-09-18 12:03:41 +0200

    added a test for junit traversable matchers

--- src/test/scala/examples/JUnitTest.scala
+++ src/test/scala/examples/JUnitTest.scala
@@ -27,6 +27,16 @@ class JUnitTest extends JUnitMustMatchers {
     "Hello world" must endWithWorld
   }
 
+  @Test
+  def containExactlyBeLike() {
+    Set(Some("a"),Some("b")) must contain(exactly(beLike[Option[ String]] {
+      case Some(a) => a must_== "a"
+    },
+    beLike[Option[String]] {
+      case Some(b) => b must_== "b"
+    }))
+  }
+
   // custom matchers can be created by coercing a function T => (Boolean, String, String) to Matcher[T]
   def endWithWorld: Matcher[String] = (s: String) => (s.endsWith("world"), s+" ends with world", s+" doesn't end with world")
 }
