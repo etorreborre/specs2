067429acbef79a0ac0d168fa82f324f0ddd4e2cf
etorreborre <etorreborre@yahoo.com>
2014-03-12 13:15:05 +1100

    trying a more important number or retries for the future test case

--- tests/src/test/scala/org/specs2/matcher/FutureMatchersSpec.scala
+++ tests/src/test/scala/org/specs2/matcher/FutureMatchersSpec.scala
@@ -15,7 +15,7 @@ class FutureMatchersSpec extends Specification with Groups with NoTimeConversion
 
  with a retries number
  ${ future { Thread.sleep(100); 1 } must be_>(0).await(retries = 2, timeout = 100.millis) }
- ${ (future { Thread.sleep(500); 1 } must be_>(0).await(retries = 2, timeout = 100.millis)) returns "Timeout after 200 milliseconds" }
+ ${ (future { Thread.sleep(300); 1 } must be_>(0).await(retries = 4, timeout = 50.millis)) returns "Timeout after 200 milliseconds" }
 
  A `Future` returning a `Matcher[T]` can be transformed into a `Result`
  ${ future(1 === 1).await }
