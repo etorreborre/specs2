29dd8078b488f464b43e1a43b1b117b0536ae194
Eric Torreborre <etorreborre@yahoo.com>
2014-04-02 10:04:22 +1100

    added back the missing specs2.files object

--- tests/src/test/scala/org/specs2/all.scala
+++ tests/src/test/scala/org/specs2/all.scala
@@ -1,3 +1,7 @@
 package org.specs2
 
+/** run all specifications found in the test directory */
+object files extends runner.FilesRunner
+
+/** run all specifications, alias for the files object */
 object all extends runner.FilesRunner
