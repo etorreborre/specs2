e6ebae48ced308393e3df93a6b1c6db261ffdc1e
Eric Torreborre <etorreborre@yahoo.com>
2014-05-01 23:37:26 +1000

    added back the specs2.files to its proper place. fixes #258

---
+++ core/src/main/scala/org/specs2/files.scala
@@ -0,0 +1,4 @@
+package specs2
+
+/** run all specifications found in the test directory */
+object files extends org.specs2.runner.FilesRunner
--- tests/src/test/scala/org/specs2/all.scala
+++
@@ -1,7 +0,0 @@
-package org.specs2
-
-/** run all specifications found in the test directory */
-object files extends runner.FilesRunner
-
-/** run all specifications, alias for the files object */
-object all extends runner.FilesRunner
