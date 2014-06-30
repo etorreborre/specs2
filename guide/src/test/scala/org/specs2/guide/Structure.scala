4d3b352ccd1734230cd67d6d9bd2c7c332be0172
Eric Torreborre <etorreborre@yahoo.com>
2014-06-30 23:05:20 +1000

    added a page on template specifications

--- guide/src/test/scala/org/specs2/guide/Structure.scala
+++ guide/src/test/scala/org/specs2/guide/Structure.scala
@@ -1,10 +1,9 @@
 package org.specs2
 package guide
 
-object Structure extends UserGuidePage {
-  def is = s2"""
+object Structure extends UserGuidePage { def is = s2"""
 
-### Styles
+## Styles
 
 In a Specification you generally want to include 2 things:
 
@@ -22,24 +21,21 @@ Both ways of writing specifications have advantages and drawbacks:
  - Acceptance specifications are easier to read as a narrative but require navigation between the text and the code. You also need to define a `is` method holding the body of the specification.
  - Unit specifications are easier to navigate but the text tends to be lost in a sea of code
 
-#### Acceptance specification
+### Acceptance specification
 
-An acceptance specification extends `org.specs2.Specification` and defines the `is` method. You can implement this method with an interpolated **`s2`** string: ${
-    snippet {
-      class MySpecification extends org.specs2.Specification {
-        def is = s2"""
+An acceptance specification extends `org.specs2.Specification` and defines the `is` method. You can implement this method with an interpolated **`s2`** string: ${snippet {
+class MySpecification extends org.specs2.Specification { def is = s2"""
 
  this is my specification
    where example 1 must be true           $e1
    where example 2 must be true           $e2
                                           """
 
-        def e1 = 1 must_== 1
+ def e1 = 1 must_== 1
 
-        def e2 = 2 must_== 2
-      }
-    }
-  }
+ def e2 = 2 must_== 2
+}
+}}
 
 The `s2` string contains the text of your specification as well as some references to methods (`e1` and `e2`) defining *`Results`*. When the Specification is executed the `s2` string is analysed and 2 `Examples` are created then executed:
 
@@ -48,22 +44,20 @@ The `s2` string contains the text of your specification as well as some referenc
 
 All the rest, `"this is my specification"`, is parsed as `Text` and is not executed.
 
-#### Unit specification
+### Unit specification
 
-A unit specification extends `org.specs2.mutable.Specification` and uses the `>>` operator to create "blocks" containing `Texts` and `Examples`: ${
-    snippet {
-      class MySpecification extends org.specs2.mutable.Specification {
-        "this is my specification" >> {
-          "where example 1 must be true" >> {
-            1 must_== 1
-          }
-          "where example 2 must be true" >> {
-            2 must_== 2
-          }
-        }
-      }
+A unit specification extends `org.specs2.mutable.Specification` and uses the `>>` operator to create "blocks" containing `Texts` and `Examples`: ${snippet {
+class MySpecification extends org.specs2.mutable.Specification {
+  "this is my specification" >> {
+    "where example 1 must be true" >> {
+      1 must_== 1
+    }
+    "where example 2 must be true" >> {
+       2 must_== 2
     }
   }
+}
+}}
 
 This specification creates one piece of `Text` and 2 `Examples` as before but:
 
@@ -82,44 +76,38 @@ The good news is that for each of the 2 main styles, acceptance and unit, you ca
 
 #### Functional expectations
 
-In an acceptance specification, by default, the `Result` of an `Example` is always given by the last statement of its body. For instance, this example will never fail because the first expectation is "lost": ${
-    snippet {
-      // this will never fail!
-      s2"""
+In an acceptance specification, by default, the `Result` of an `Example` is always given by the last statement of its body. For instance, this example will never fail because the first expectation is "lost":${snippet{
+// this will never fail!
+s2"""
   my example on strings $e1
 """
-      def e1 = {
-        // because this expectation will not be returned,...
-        "hello" must have size (10000)
-        "hello" must startWith("hell")
-      }
-    }
+  def e1 = {
+    // because this expectation will not be returned,...
+    "hello" must have size (10000)
+    "hello" must startWith("hell")
   }
+}}
 
-If you want to get both expectations you will need to use `and` between them: ${
-    snippet {
-      s2"""
+If you want to get both expectations you will need to use `and` between them: ${snippet{
+s2"""
   my example on strings $e1
 """
-      def e1 = ("hello" must have size (10000)) and
-        ("hello" must startWith("hell"))
-    }
-  }
+  def e1 = ("hello" must have size (10000)) and
+           ("hello" must startWith("hell"))
+}}
 
 This is a bit tedious and not very pleasing to read so you can see why this mode encourages one expectation per example only. If you need several expectations per example, you can need to mix-in the `org.specs2.execute.ThrownExpectations` trait to the specification which is the one used for unit specifications by default.
 
-##### Thrown expectations
+#### Thrown expectations
 
-With a unit specification you get "thrown expectations". When an expectation fails, it throws an exception and the rest of the example is not executed: ${
-    snippet {
-      class MySpecification extends org.specs2.mutable.Specification {
-        "This is my example" >> {
-          1 must_== 2 // this fails
-          1 must_== 1 // this is not executed
-        }
-      }
-    }
+With a unit specification you get "thrown expectations". When an expectation fails, it throws an exception and the rest of the example is not executed: ${snippet {
+class MySpecification extends org.specs2.mutable.Specification {
+  "This is my example" >> {
+    1 must_== 2 // this fails
+    1 must_== 1 // this is not executed
   }
+}
+}}
 
 It is also possible to use the "functional" expectation mode with a unit specification by mixing in the `org.specs2.execute.NoThrownExpectations` trait.
 
@@ -145,64 +133,7 @@ It is also possible to use the "functional" expectation mode with a unit specifi
  - add ${"debug statements" ~ DebugStatements}
  - print ${"execution data" ~ PrintExecutionData}
  - extend the specification by creating examples ${"_while it is executing_" ~ CreateOnlineSpecifications}
- - create a trait which will decorate or transform an other specification when mixed-in
+ - create a trait which will ${"decorate or transform other specifications" ~ TemplateSpecification} when mixed-in
 
 """
 }
-/*
-
-
-#### DataTables
-
-[DataTables](org.specs2.guide.Matchers.html#DataTables) are generally used to pack lots of expectations inside one example. A DataTable which is used as a `Result` in the body of an Example will only be displayed when failing. If, on the other hand you want to display the table even when successful, to document your examples, you can omit the example description and inline the DataTable directly in the specification: ${snippet{
-
-  class DataTableSpec extends Specification with Tables { def is =
-
-    "adding integers should just work in scala"  ^ eg {
-      "a"   | "b" | "c" |
-        2    !  2  !  4  |
-        1    !  1  !  2  |>
-        { (a, b, c) =>  a + b must_== c }
-    }
-  }
-}}
-This specification will be rendered as:
-```adding integers should just work in scala
-+  a | b | c |
-   2 | 2 | 4 |
-   1 | 1 | 2 |
-```
-#### Example groups
-
-When you create acceptance specifications, you have to find names to reference your examples, which can sometimes be a bit tedious. You can then get some support from the `${fullName[specification.Grouped]}` trait. This trait provides group traits, named `g1` to `g22` to define groups of examples. Each group trait defines 22 variables named `e1` to `e22`, to define examples bodies. The specification below shows how to use the `Grouped` trait: ${snippet{
-
-  class MySpecification extends Specification with Examples { def is =  s2"""
-  first example in first group                                        ${g1.e1}
-  second example in first group                                       ${g1.e2}
-
-  first example in second group                                       ${g2.e1}
-  second example in second group                                      ${g2.e2}
-  third example in second group, not yet implemented                  ${g2.e3}
-  """
-  }
-
-  trait Examples extends specification.Grouped with matcher.Matchers {
-    // group of examples with no description
-    new g1 {
-      e1 := ok
-      e2 := ok
-    }
-    // group of examples with a description for the group
-    "second group of examples" - new g2 {
-      e1 := ok
-      e2 := ok
-    }
-  }
-}}
-
-Note that, if you use groups, you can use the example names right away, like `g2.e3`, without providing an implementation, the example will be marked as `Pending`.
-
-}
-
-
-*/
---
+++ guide/src/test/scala/org/specs2/guide/TemplateSpecification.scala
@@ -0,0 +1,34 @@
+package org.specs2
+package guide
+
+import specification.BeforeAll
+
+object TemplateSpecification extends UserGuidePage { def is = s2"""
+
+On the ${"Contexts" ~ Contexts} page we saw that there is a way to define an action which will be executed before all examples with the `BeforeAll` trait. It is very likely that that you will actually make your own trait extending `BeforeAll` to reuse this action in more than one specification:${snippet{
+trait DatabaseSetup extends BeforeAll {
+  def beforeAll = println("prepare database")
+}
+
+class DatabaseSpecification1 extends Specification with DatabaseSetup { def is = s2"""
+  // do something with the database
+ """
+}
+}}
+
+How does this work? The `BeforeAll` trait overrides a method called `map` in the `Specification` trait and adds one `Step` before anything else in the specification:
+```
+  override def map(fs: =>Fragments): Fragments =
+    super.map(fs).prepend(fragmentFactory.step(beforeAll))
+```
+
+The `map` method is indeed called everytime the specification returns the list of `Fragment` defining it. You can leverage this and define your own traits:
+
+ - adding some text before a Specification
+ - filtering out some undesirable fragments
+ - reformatting all text
+ - ...
+
+"""
+}
+
