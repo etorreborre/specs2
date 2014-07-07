16a59183bf05975032804d26642b85e742e9f7b5
Eric Torreborre <etorreborre@yahoo.com>
2014-07-07 14:28:54 +1000

    added some missing dsl for tags in mutable specs

---
+++ core/src/main/scala/org/specs2/specification/dsl/mutable/TagsDsl.scala
@@ -0,0 +1,31 @@
+package org.specs2
+package specification
+package dsl
+package mutable
+
+import data.{Tag, NamedTag}
+import core.{Fragments, Fragment}
+
+trait TagsDsl extends FragmentsDsl with MutableFragmentBuilder { outer =>
+  override def tag(tag: NamedTag)      : Fragment = addFragment(super.tag(tag))
+  override def taggedAs(tag: NamedTag) : Fragment = addFragment(super.taggedAs(tag))
+  override def section(tag: NamedTag)  : Fragment = addFragment(super.section(tag))
+  override def asSection(tag: NamedTag): Fragment = addFragment(super.asSection(tag))
+
+  /**
+   * This implicit allows to add tags and sections _after_ the examples
+   */
+  implicit class FragmentTaggedAs(f: =>Fragment) {
+    def tag(tag: NamedTag)     = { outer.tag(names); f }
+    def section(tag: NamedTag) = { outer.tag(tag); f }
+  }
+
+  /**
+   * This implicit allows to add tags and sections _after_ the examples
+   */
+  implicit class FragmentsTaggedAs(fs: =>Fragments) {
+    def tag(tag: NamedTag)     = { outer.tag(names); fs }
+    def section(tag: NamedTag) = { outer.section(tag); fs }
+  }
+
+}
