478ee279e556767beee976761ed669d4f0809c7d
Eric Torreborre <etorreborre@yahoo.com>
2014-09-04 09:19:18 +1000

    fixed the tags dsl with 2 interpretations, one for immutable specs and one for mutable specs

--- core/src/main/scala/org/specs2/specification/dsl/TagsDsl.scala
+++ core/src/main/scala/org/specs2/specification/dsl/TagsDsl.scala
@@ -8,20 +8,16 @@ import create.FragmentsFactory
 
 trait TagsDsl extends FragmentsFactory { outer =>
 
-  def tag(names: String*)    : Fragment   = fragmentFactory.tag(names:_*)
-  def taggedAs(names: String*) : Fragment = fragmentFactory.taggedAs(names:_*)
-  def section(names: String*)  : Fragment = fragmentFactory.section(names:_*)
-  def asSection(names: String*): Fragment = fragmentFactory.asSection(names:_*)
+  def tag(names: String*)    : Fragment   = fragmentFactory.taggedAs(names:_*)
+  def section(names: String*)  : Fragment = fragmentFactory.asSection(names:_*)
 
-  def tag(tag: NamedTag)      : Fragment = fragmentFactory.mark(tag)
-  def taggedAs(tag: NamedTag) : Fragment = fragmentFactory.markAs(tag)
-  def section(tag: NamedTag)  : Fragment = fragmentFactory.markSection(tag)
-  def asSection(tag: NamedTag): Fragment = fragmentFactory.markSectionAs(tag)
+  def tag(tag: NamedTag)      : Fragment = fragmentFactory.markAs(tag)
+  def section(tag: NamedTag)  : Fragment = fragmentFactory.markSectionAs(tag)
 
   /** shortcut to add tag more quickly when rerunning failed tests */
-  private[specs2] def xtag = fragmentFactory.tag("x")
+  private[specs2] def xtag = fragmentFactory.taggedAs("x")
   /** shortcut to add section more quickly when rerunning failed tests */
-  private[specs2] def xsection = fragmentFactory.section("x")
+  private[specs2] def xsection = fragmentFactory.asSection("x")
 
 }
 
--- core/src/main/scala/org/specs2/specification/dsl/mutable/TagsDsl.scala
+++ core/src/main/scala/org/specs2/specification/dsl/mutable/TagsDsl.scala
@@ -8,14 +8,10 @@ import core.{Fragments, Fragment}
 
 trait TagsDsl extends org.specs2.specification.dsl.TagsDsl with MutableFragmentBuilder { outer =>
   override def tag(names: String*)    : Fragment   = addFragment(fragmentFactory.tag(names:_*))
-  override def taggedAs(names: String*) : Fragment = addFragment(fragmentFactory.taggedAs(names:_*))
   override def section(names: String*)  : Fragment = addFragment(fragmentFactory.section(names:_*))
-  override def asSection(names: String*): Fragment = addFragment(fragmentFactory.asSection(names:_*))
 
   override def tag(tag: NamedTag)      : Fragment = addFragment(super.tag(tag))
-  override def taggedAs(tag: NamedTag) : Fragment = addFragment(super.taggedAs(tag))
   override def section(tag: NamedTag)  : Fragment = addFragment(super.section(tag))
-  override def asSection(tag: NamedTag): Fragment = addFragment(super.asSection(tag))
 
   /** shortcut to add tag more quickly when rerunning failed tests */
   private[specs2] override def xtag = addFragment(fragmentFactory.tag("x"))
