96528bdfa511e7f06d2f0bba976c812c4780fb06
Eric Torreborre <etorreborre@yahoo.com>
2014-09-08 19:06:05 +1000

    added back deprecated Step and Action in the acceptance Dsl

--- core/src/main/scala/org/specs2/specification/dsl/ActionDsl.scala
+++ core/src/main/scala/org/specs2/specification/dsl/ActionDsl.scala
@@ -11,4 +11,14 @@ trait ActionDsl extends FragmentsFactory {
   def step(a: =>Any) = fragmentFactory.step(a)
   def step(a: =>Any, global: Boolean = false) = fragmentFactory.step(a).makeGlobal(global)
   def action(a: =>Any) = fragmentFactory.action(a)
+
+  @deprecated(message = "use step instead", since = "3.0")
+  def Step(a: =>Any) = fragmentFactory.step(a)
+
+  @deprecated(message = "use step instead", since = "3.0")
+  def Step(a: =>Any, global: Boolean = false) = fragmentFactory.step(a).makeGlobal(global)
+
+  @deprecated(message = "use action instead", since = "3.0")
+  def Action(a: =>Any) = fragmentFactory.action(a)
+
 }
