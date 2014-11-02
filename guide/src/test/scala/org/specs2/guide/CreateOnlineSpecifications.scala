79e82bcd51adcdbf818e7cf0624a0ed6cf23658e
Eric Torreborre <etorreborre@yahoo.com>
2014-11-03 08:26:33 +1100

    better description for the CreateOnlineSpecification page

--- guide/src/test/scala/org/specs2/guide/CreateOnlineSpecifications.scala
+++ guide/src/test/scala/org/specs2/guide/CreateOnlineSpecifications.scala
@@ -9,9 +9,9 @@ object CreateOnlineSpecifications extends UserGuidePage { def is = s2"""
  Most of the specifications we write are known up front because this is precisely what guides the construction of our systems. However in some systems data comes first and drives what we can do with the system. For example we want to check that:
 
   1. all the Wikipedia pages mentioning the term "BDD" are referencing $specs2
-  2. if there is a $specs2 link on the page, the linked page must provide the name of the author
+  2. if there is a $specs2 link on the page, the linked page must exist
 
-More precisely we want to create one example for `1.` and if it succeeds, create as many examples as there are links in `2.`. This can be done with the `org.specs2.specification.dsl.Online` trait and the `continueWith` method:: ${snippet{
+More precisely we want to create one example for `1.` and if it succeeds, create as many examples as there are links in `2.`. This can be done with the `org.specs2.specification.dsl.Online` trait and the `continueWith` method: ${snippet{
 class WikipediaBddSpec extends Specification with Online { def is = s2"""
   All the pages mentioning the term BDD must contain a reference to specs2 $e1
 """
@@ -24,21 +24,22 @@ class WikipediaBddSpec extends Specification with Online { def is = s2"""
   }
 
   def pagesSpec(pages: Seq[Page]): Fragments = {
-    val specs2Links = pages.flatMap(_.getLinks).filter(_.getName.contains("specs2"))
+    val specs2Links = pages.flatMap(_.getLinks).filter(_.contains("specs2"))
 
     s2"""
-     The specs2 links must all contain a reference to the author of specs2
-     ${specs2Links.repeat(authorExample)}
+     The specs2 links must all be active
+     ${ specs2Links repeat isActive }
     """
   }
 
-  def authorExample(link: HtmlLink) =
+  def isActive(link: HtmlLink) =
     s2"""
   The page at ${link.getName}
-    contains the name torreborre ${ link.getLinkedPage must mention("torreborre") }"""
+    must be active ${ link.getLinkedPage must beActive }"""
 
   // implement this matcher
   def mention(name: String): Matcher[Page] = ???
+  def beActive: Matcher[HtmlLink] = ???
 // 8<----
   // fill in the definitions below
   object Wikipedia {
@@ -50,6 +51,7 @@ class WikipediaBddSpec extends Specification with Online { def is = s2"""
   }
 
   trait HtmlLink {
+    def contains(name: String): Boolean = ???
     def getName: String = ???
     def getLinkedPage: Page = ???
   }
