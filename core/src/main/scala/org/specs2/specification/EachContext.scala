84d41a0cab19cea9c6ad7eba496ac9610297ea1a
Eric Torreborre <etorreborre@yahoo.com>
2014-11-03 08:22:53 +1100

    fixed deprecation messages for contexts traits

--- core/src/main/scala/org/specs2/specification/EachContext.scala
+++ core/src/main/scala/org/specs2/specification/EachContext.scala
@@ -152,14 +152,14 @@ trait BeforeAfterSpec extends SpecificationStructure {
 @deprecated("use the BeforeEach trait instead", "3.0")
 trait BeforeExample extends BeforeEach
 
-@deprecated("use the BeforeEach trait instead", "3.0")
+@deprecated("use the AfterEach trait instead", "3.0")
 trait AfterExample extends AfterEach
 
-@deprecated("use the BeforeEach trait instead", "3.0")
+@deprecated("use the AroundEach trait instead", "3.0")
 trait AroundExample extends AroundEach
 
-@deprecated("use the BeforeEach trait instead", "3.0")
+@deprecated("use the BeforeAfterEach trait instead", "3.0")
 trait BeforeAfterExample extends BeforeAfterEach
 
-@deprecated("use the BeforeEach trait instead", "3.0")
+@deprecated("use the ForEach trait instead", "3.0")
 trait FixtureExample[T] extends ForEach[T]
