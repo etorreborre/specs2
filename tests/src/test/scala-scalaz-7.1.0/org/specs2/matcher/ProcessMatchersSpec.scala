7c990d5846786a3e4ae1d8280b796dd332b6bca9
Eric Torreborre <etorreborre@yahoo.com>
2014-11-09 20:24:19 +1100

    fixed the specs in alternate source directories

--- tests/src/test/scala-scalaz-7.1.0/org/specs2/matcher/ProcessMatchersSpec.scala
+++ tests/src/test/scala-scalaz-7.1.0/org/specs2/matcher/ProcessMatchersSpec.scala
@@ -6,33 +6,40 @@ import scala.concurrent.duration._
 import scalaz.concurrent._
 import scalaz.stream._
 
-class ProcessMatchersSpec extends Specification with ProcessMatchers with ResultMatchers with Retries { def is = s2"""
+class ProcessMatchersSpec extends mutable.Specification with ProcessMatchers with ResultMatchers with Retries {
 
- It is possible to check the execution of processes
-   check the produced values
-   ${ oneElement(1) must returnValues(Seq(1)) }
-   ${ (oneElement(1) must returnValues(IndexedSeq(2))) returns "Vector(1) is not equal to Vector(2)" }
+  "It is possible to check the execution of processes" >> {
+    "check the produced values" >> {
+      { oneElement(1) must returnValues(Seq(1)) }
+      { (oneElement(1) must returnValues(IndexedSeq(2))) returns "Vector(1) is not equal to Vector(2)" }
+    }
 
-   check the last value
-   ${ oneElement(1) must returnLast(1) }
-   ${ (oneElement(1) must returnLast(2)) returns "'1' is not equal to '2'" }
-   ${ (oneElement(1).drop(1) must returnLast(1)) returns "Expected a value, got None" }
 
-   check the last value as an option
-   ${ oneElement(1).drop(1) must returnLastOption(None) }
-   ${ oneElement(1) must returnLastOption(Some(1)) }
-   ${ (oneElement(1) must returnLastOption(Some(2))) returns "'Some(1)' is not equal to 'Some(2)'" }
+    "check the last value" >> {
+      { oneElement(1) must returnLast(1) }
+      { (oneElement(1) must returnLast(2)) returns "'1' is not equal to '2'" }
+      { (oneElement(1).drop(1) must returnLast(1)) returns "Expected a value, got None" }
+    }
 
-   check that the process finishes before a given time
-   ${ oneElement(1) must terminateBefore(10.millis) }
-   ${ (oneElementAfter(1, 100.millis) must terminateBefore(50.millis)) returns "Timeout after 50 milliseconds" } $xtag
+    "check the last value as an option" >> {
+      { oneElement(1).drop(1) must returnLastOption(None) }
+      { oneElement(1) must returnLastOption(Some(1)) }
+      { (oneElement(1) must returnLastOption(Some(2))) returns "'Some(1)' is not equal to 'Some(2)'" }
+    }
 
-   check the produced value with a timeout
-   ${ oneElementAfter(1, 10.millis) must terminateBefore(100.millis).withValues(Seq(1)) }
-   ${ oneElementAfter(1, 10.millis) must returnValues(Seq(1)).before(100.millis) }
+    "check that the process finishes before a given time" >> {
+      { oneElement(1) must terminateBefore(10.millis) }
+      { (oneElementAfter(1, 100.millis) must terminateBefore(50.millis)) returns "Timeout after 50 milliseconds" }
+    }
 
-   ${step(scheduledExecutorService.shutdown)}
-"""
+    "check the produced value with a timeout" >> {
+      { oneElementAfter(1, 10.millis) must terminateBefore(100.millis).withValues(Seq(1)) }
+      { oneElementAfter(1, 10.millis) must returnValues(Seq(1)).before(100.millis) }
+    }
+  }
+
+  step(scheduledExecutorService.shutdown)
+
 
   def oneElement[T](t: =>T) =
     Process.eval(Task.delay(t))
