51382da2584c64dfd90614080bfb9b90052cfa61
Eric Torreborre <etorreborre@yahoo.com>
2014-12-20 13:32:58 +1100

    deactivated the previous scalacheck spec

--- tests/src/test/scala/org/specs2/matcher/ScalaCheckMatchersSpec.scala
+++ tests/src/test/scala/org/specs2/matcher/ScalaCheckMatchersSpec.scala
@@ -18,90 +18,90 @@ class ScalaCheckMatchersSpec extends Spec with ScalaCheckProperties { def is = s
  A ScalaCheck property can be used in the body of an Example
    Here are some examples with
      a result
-     ${ prop { (i:Int) => success } }
+     $${ prop { (i:Int) => success } }
 
      a match result
-     ${ prop { (i:Int) => i must be_>(0) or be_<=(0) } }
+     $${ prop { (i:Int) => i must be_>(0) or be_<=(0) } }
 
      a boolean value
-     ${ prop { (i:Int) => i > 0 || i <= 0 } }
+     $${ prop { (i:Int) => i > 0 || i <= 0 } }
 
      a Prop
-     ${ forAll { (i:Int) => i > 0 || i <= 0 } }
+     $${ forAll { (i:Int) => i > 0 || i <= 0 } }
 
      an implication and a match result
-     ${ prop { (i:Int) => (i > 0) ==> (i must be_>(0)) } }
-     ${ prop { (i:Int, j: Int) => (i > j) ==> (i must be_>(j)) } }
+     $${ prop { (i:Int) => (i > 0) ==> (i must be_>(0)) } }
+     $${ prop { (i:Int, j: Int) => (i > j) ==> (i must be_>(j)) } }
 
      an implication and a boolean value
-     ${ prop { (i:Int) => (i > 0) ==> (i > 0) } }
+     $${ prop { (i:Int) => (i > 0) ==> (i > 0) } }
 
      a unit value with side-effects
-     ${ prop { (i:Int) => { (1 to 5) foreach { n => n must_== n } } } }
+     $${ prop { (i:Int) => { (1 to 5) foreach { n => n must_== n } } } }
 
-     a specific arbitrary instance in the enclosing scope ${
+     a specific arbitrary instance in the enclosing scope $${
        implicit val arbitrary = positiveInts
        prop { (i:Int) => i must be_>(0) }
      }
 
      a specific arbitrary instance
-     ${ positiveInts { (i:Int) => i must be_>(0) } }
+     $${ positiveInts { (i:Int) => i must be_>(0) } }
 
      several specific arbitrary instances
-     ${ (positiveInts, positiveInts) { (i:Int, j: Int) => i+j must be_>(0) } }
+     $${ (positiveInts, positiveInts) { (i:Int, j: Int) => i+j must be_>(0) } }
 
      specific generation parameters
-     ${ prop { (i:Int) => (i > 0) ==> (i > 0) } set (minTestsOk = 50) }
+     $${ prop { (i:Int) => (i > 0) ==> (i > 0) } set (minTestsOk = 50) }
 
-   if it is proved the execution will yield a Success                                                    $prop1
-   if it is a function which is always true, it will yield a Success                                     $prop2
-   if it is a function which is always false, it will yield a Failure                                    $prop3
+   if it is proved the execution will yield a Success                                                    $$prop1
+   if it is a function which is always true, it will yield a Success                                     $$prop2
+   if it is a function which is always false, it will yield a Failure                                    $$prop3
 
    if it is a property throwing an exception
-     it will yield an Error                                                                              $prop4
-     showing the exception type if the message is null                                                   $prop4_1
-     showing the cause                                                                                   $prop4_2
-     showing the stacktrace                                                                              $prop4_3
-   a Property can be used with check                                                                     $prop5
-   a FailureException can be thrown from a Prop                                                          $prop6
-   in the Context of a mutable specification                                                             $prop7
-   with a Pending or Skipped result the Prop is undecided                                                $prop8
-   if the Prop itself is an exception                                                                    $prop9
+     it will yield an Error                                                                              $$prop4
+     showing the exception type if the message is null                                                   $$prop4_1
+     showing the cause                                                                                   $$prop4_2
+     showing the stacktrace                                                                              $$prop4_3
+   a Property can be used with check                                                                     $$prop5
+   a FailureException can be thrown from a Prop                                                          $$prop6
+   in the Context of a mutable specification                                                             $$prop7
+   with a Pending or Skipped result the Prop is undecided                                                $$prop8
+   if the Prop itself is an exception                                                                    $$prop9
 
- It can also be used at the beginning of a specification                                                 $fragment1
+ It can also be used at the beginning of a specification                                                 $$fragment1
 
  A specs2 matcher can be returned by a function to be checked with ScalaCheck
-   if it is a MatchSuccess the execution will yield a Success                                            $matcher1
+   if it is a MatchSuccess the execution will yield a Success                                            $$matcher1
    if the type of the input parameter is not the same as the MatchResult type
-     it should still work                                                                                $matcher2
-     another way is to transform it as a property with .forAll                                           $matcher3
+     it should still work                                                                                $$matcher2
+     another way is to transform it as a property with .forAll                                           $$matcher3
 
- A partial function can also be used in the body of the Example                                          $partial1
- A function with 2 parameters, returning a MatchResult be used in the body of the Example                $partial2
- A function with 3 parameters, returning a MatchResult be used in the body of the Example                $partial3
- A function with 4 parameters, returning a MatchResult be used in the body of the Example                $partial4
- A function with 5 parameters, returning a MatchResult be used in the body of the Example                $partial5
+ A partial function can also be used in the body of the Example                                          $$partial1
+ A function with 2 parameters, returning a MatchResult be used in the body of the Example                $$partial2
+ A function with 3 parameters, returning a MatchResult be used in the body of the Example                $$partial3
+ A function with 4 parameters, returning a MatchResult be used in the body of the Example                $$partial4
+ A function with 5 parameters, returning a MatchResult be used in the body of the Example                $$partial5
 
- Arbitrary instances can be specified for a given property                                               $arb1
- Gen instances can be used to define a property using matchers                                           $gen1
+ Arbitrary instances can be specified for a given property                                               $$arb1
+ Gen instances can be used to define a property using matchers                                           $$gen1
 
  A ScalaCheck property will create a Result
-   with a number of expectations that is equal to the minTestsOk                                         $result1
-   with one expectation per Prop if the OneExpectationPerProp trait is used                              $result2
-   with failure details if any                                                                           $result3
+   with a number of expectations that is equal to the minTestsOk                                         $$result1
+   with one expectation per Prop if the OneExpectationPerProp trait is used                              $$result2
+   with failure details if any                                                                           $$result3
 
  It is possible to change the default parameters used for the test
-   by setting up new implicit parameters locally                                                         ${config().e1}
+   by setting up new implicit parameters locally                                                         $${config().e1}
 
  Properties can be specified with no shrinking
-  ${ propNoShrink { (i:Int) => i > 0 || i <= 0 } }
-  ${ propNoShrink { (i:Int, j: Int) => (i > j) ==> (i must be_>(j)) } }
+  $${ propNoShrink { (i:Int) => i > 0 || i <= 0 } }
+  $${ propNoShrink { (i:Int, j: Int) => (i > j) ==> (i must be_>(j)) } }
 
  It is possible to display
-   the executed tests by setting up display parameters locally                                           ${config().e2}
-   the labels that are set on properties                                                                 ${config().e3}
-   the exceptions that happen on generation                                                              ${config().e4}
-   the collected frequencies                                                                             ${config().e5}
+   the executed tests by setting up display parameters locally                                           $${config().e2}
+   the labels that are set on properties                                                                 $${config().e3}
+   the exceptions that happen on generation                                                              $${config().e4}
+   the collected frequencies                                                                             $${config().e5}
                                                                                                          """
 
   
