fc656c8e3f540d34f6f8868961bb18bb1a7597eb
Eric Torreborre <etorreborre@yahoo.com>
2015-01-21 10:01:36 +1100

    added missing creation methods for scalacheck parameters

--- scalacheck/src/main/scala/org/specs2/scalacheck/ScalaCheckParameters.scala
+++ scalacheck/src/main/scala/org/specs2/scalacheck/ScalaCheckParameters.scala
@@ -2,6 +2,7 @@ package org.specs2
 package scalacheck
 
 import org.scalacheck.util._
+import org.scalacheck.Test
 
 trait ScalaCheckParameters {
   /**
@@ -11,5 +12,27 @@ trait ScalaCheckParameters {
 
   implicit def defaultFreqMapPretty: FreqMap[Set[Any]] => Pretty =
     Pretty.prettyFreqMap
+
+  /** create parameters with verbose = false */
+  def set(minTestsOk: Int                              = defaultParameters.minTestsOk,
+          minSize: Int                                 = defaultParameters.minSize,
+          maxDiscardRatio: Float                       = defaultParameters.maxDiscardRatio,
+          maxSize: Int                                 = defaultParameters.maxSize,
+          workers: Int                                 = defaultParameters.workers,
+          rng: scala.util.Random                       = defaultParameters.rng,
+          callback: Test.TestCallback                  = defaultParameters.testCallback,
+          loader: Option[ClassLoader]                  = defaultParameters.loader): Parameters =
+    new Parameters(minTestsOk, minSize, maxDiscardRatio, maxSize, workers, rng, callback, loader)
+
+  /** create parameters with verbose = true */
+  def display(minTestsOk: Int                              = defaultParameters.minTestsOk,
+              minSize: Int                                 = defaultParameters.minSize,
+              maxDiscardRatio: Float                       = defaultParameters.maxDiscardRatio,
+              maxSize: Int                                 = defaultParameters.maxSize,
+              workers: Int                                 = defaultParameters.workers,
+              rng: scala.util.Random                       = defaultParameters.rng,
+              callback: Test.TestCallback                  = defaultParameters.testCallback,
+              loader: Option[ClassLoader]                  = defaultParameters.loader): Parameters =
+    new Parameters(minTestsOk, minSize, maxDiscardRatio, maxSize, workers, rng, callback, loader).verbose
 }
 
