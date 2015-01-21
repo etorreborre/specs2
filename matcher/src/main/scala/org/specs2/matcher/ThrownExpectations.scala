1485e7c43579bd274a74726901bb01f2fefa429b
Eric Torreborre <etorreborre@yahoo.com>
2015-01-22 09:38:23 +1100

    Revert "added 2 methods to avoid mixing-in MustExpectations to a Scope at compile-time. fixes #321"
    This reverts commit 06ba00c32e70504b3ad68fcc7e2c41bfdbb52e67.

--- matcher/src/main/scala/org/specs2/matcher/ThrownExpectations.scala
+++ matcher/src/main/scala/org/specs2/matcher/ThrownExpectations.scala
@@ -124,16 +124,14 @@ trait NoThrownExpectations extends Expectations {
 /**
  * This trait represents any Scope that is used to enclose expectations which might be thrown
  */
-trait Scope {
-  protected def `*** If you see this message this means that you have mixed in the MustExpectations trait instead of the MustThrownExpectations trait ***`: Expectations = ???
-}
+trait Scope
 
 object Scope {
-  /** typeclass instance to transform a Scope to a Result */
+  /** typeclass to transform a Scope to a Result */
   implicit def scopeAsResult[S <: Scope]: AsResult[S] = new AsResult[S] {
     def asResult(t: =>S): Result = { t; Success() }
   }
-  /** implicit definition to transform a Scope to a Result */
+  /** typeclass to transform a Scope to a Result */
   implicit def scopeToResult(t: =>Scope): Result = AsResult(t)
 }
 
