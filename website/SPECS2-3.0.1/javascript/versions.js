8cc3e255dd9e04047b9ce3cd4a424594f45ff75c
Eric Torreborre <etorreborre@yahoo.com>
2015-03-09 10:28:01 +1100

    fixed the links in the select boxes

--- website/SPECS2-3.0.1/javascript/versions.js
+++ website/SPECS2-3.0.1/javascript/versions.js
@@ -1,8 +1,8 @@
 var guideVersions = [
- {id:"guide/SPECS2-2.4.17", text:"2.4.17"},
-{id:"guide/SPECS2-3.0", text:"3.0"}
+ {id:"../../guide/SPECS2-2.4.17/org.specs2.guide.UserGuide.html", text:"2.4.17"},
+ {id:"../../guide/SPECS2-3.0/org.specs2.guide.UserGuide.html", text:"3.0"}
 ];
 var apiVersions = [
- {id:"api/SPECS2-2.4.17", text:"2.4.17"},
-{id:"api/SPECS2-3.0", text:"3.0"}
+ {id:"../../api/SPECS2-2.4.17/index.html", text:"2.4.17"},
+{id:"../../api/SPECS2-3.0/index.html", text:"3.0"}
 ];
