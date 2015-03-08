3ee0218673abf40b1c0de9d5fbe58089f1faea2f
Eric Torreborre <etorreborre@yahoo.com>
2015-03-09 09:46:41 +1100

    updated site

---
+++ website/SPECS2-3.0.1/javascript/versions.js
@@ -0,0 +1,8 @@
+var guideVersions = [
+ {id:"guide/SPECS2-2.4.17", text:"2.4.17"},
+{id:"guide/SPECS2-3.0", text:"3.0"}
+];
+var apiVersions = [
+ {id:"api/SPECS2-2.4.17", text:"2.4.17"},
+{id:"api/SPECS2-3.0", text:"3.0"}
+];
