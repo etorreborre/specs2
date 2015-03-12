57a6a239ec124f6c03c6f66159dab2f309dfec3e
Eric Torreborre <etorreborre@yahoo.com>
2015-03-13 10:46:03 +1100

    added a scalacheck property for the BiMap

---
+++ tests/src/test/scala/org/specs2/collection/BiMapSpec.scala
@@ -0,0 +1,47 @@
+package org.specs2
+package collection
+
+import BiMap._
+import specification._
+import matcher._
+
+class BiMapSpec extends script.Specification with Groups with ScalaCheck { def is = s2"""
+
+ Bimaps define bijection relationships between values
+
+ + a BiMap can be defined with a sequence of keys and values
+ + it is possible to get the value corresponding to a given key
+ + it is possible to get the key corresponding to a given value
+ + it is possible to know if a value exists
+ + it is possible to know if a key exists
+
+ Properties
+ ==========
+
+ + all keys and values must be in the map
+ """
+
+  "BiMap api" - new group {
+    val bimap = Seq("one" <-> 1, "two" <-> 2, "three" <-> 3)
+
+    eg := bimap.values === Seq(1, 2, 3)
+    eg := bimap.fromKey("one") === Some(1)
+    eg := bimap.fromValue(2) === Some("two")
+    eg := bimap.containsValue(3)
+    eg := bimap.containsKey("three")
+  }
+
+  "properties" - new group {
+    eg := prop { (keys: List[String], values: List[Int]) =>
+      val zipped = keys.distinct.zip(values.distinct)
+      val bimap: BiMap[String, Int] = zipped.map { case (k, v) => k <-> v }
+
+      zipped must contain { kv: (String, Int) => kv match {
+        case (k, v) =>
+          (bimap.fromKey(k) must beSome(v)) and
+          (bimap.fromValue(v) must beSome(k))
+      }}.forall
+    }
+  }
+
+}
