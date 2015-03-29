5bbb976c2a5c61b948334993d2de32f8a1b93476
Eric Torreborre <etorreborre@yahoo.com>
2015-03-29 22:37:55 +1100

    removed some test methods for context on the Hello unit example

--- examples/src/test/scala/examples/HelloWorldUnitSpec.scala
+++ examples/src/test/scala/examples/HelloWorldUnitSpec.scala
@@ -1,11 +1,6 @@
 package examples
 
-import java.util.concurrent.ExecutorService
-
 import org.specs2._
-import org.specs2.specification.core.Env
-
-import scala.concurrent.ExecutionContext
 
 class HelloWorldUnitSpec extends mutable.Specification {
   "The 'Hello world' string" should {
@@ -16,15 +11,6 @@ class HelloWorldUnitSpec extends mutable.Specification {
       "Hello world" must startWith("Hello")
     }
     "end with 'world'" in {
-      "Hello world" must endWith("world")
-    }
-    "end with 'world'" >> { env: Env =>
-      "Hello world" must endWith("world")
-    }
-    "end with 'world'" >> { ec: ExecutorService =>
-      "Hello world" must endWith("world")
-    }
-    "end with 'world'" >> { ec: ExecutionContext =>
       "Hello world" must endWith("world")
     }
   }
