323b002a0228fa687662fb0f53606167fc6a978d
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2015-05-02 16:09:05 -0700

    Add documentation surrounding junit xml output

--- guide/src/test/scala/org/specs2/guide/JUnitXmlOutput.scala
+++ guide/src/test/scala/org/specs2/guide/JUnitXmlOutput.scala
@@ -6,9 +6,28 @@ Many continuous integration servers (like [Jenkins](http://jenkins-ci.org)) acce
 
  `testOnly org.acme.MySpec -- junitxml`
 
-The JUnit XML file corresponding to the specification will be produced by default in the `target/test-reports` directory. You can change this directory by passing the `junit.outdir` argument.
+The JUnit XML file corresponding to the specification will be produced by default in the `target/test-reports` directory. You can change this directory by passing the `junit.outdir` argument like so:
+
+ `testOnly org.acme.MySpec -- junitxml junit.outdir custom_xml_folder`
+
+This will output the xml files in the `custom_xml_folder` in the top level project directory.
 
 ${"Remember" ~/ ConsoleOutput} that using `junitxml` by itself will turn off the console reporting. You need to add `console` to get it back.
+
+Note that `sbt test` does not take parameters so the default behavior is to produce the JUnit XML files for all specifications in the default output directory
+
+In order to change the default output directory of the junit xml files when running the tests with `test`, add the following to your sbt build file:
+
+ `testOptions in Test += Tests.Argument("junitxml", "junit.outdir", "custom_xml_folder")`
+
+Note that this will suppress the console output, which may be what you want if this is a configuration for a build machine. To re-enable console output, use instead:
+
+ ```
+ testOptions in Test ++= Seq(
+    Tests.Argument("junitxml", "junit.outdir", "custom_xml_folder"),
+    Tests.Argument("console")
+ )
+ ```
 
 """
 }
