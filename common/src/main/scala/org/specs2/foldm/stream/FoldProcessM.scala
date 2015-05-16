3305cc6859cb3d43cb02915eaa317fa65b6984f6
Eric Torreborre <etorreborre@yahoo.com>
2015-05-17 09:34:41 +1000

    better implementation (synchronous) of fromSink

--- common/src/main/scala/org/specs2/foldm/stream/FoldProcessM.scala
+++ common/src/main/scala/org/specs2/foldm/stream/FoldProcessM.scala
@@ -4,9 +4,9 @@ package stream
 
 import FoldM._
 
+import scalaz.stream.Process.{Halt, Await, Emit, Step}
 import scalaz.stream._
-import scalaz.stream.async.mutable.Signal
-import scalaz.{Id, ~>}, Id._
+import scalaz.{Id, ~>, \/, \/-, -\/}, Id._
 import scalaz.concurrent.Task
 
 object FoldProcessM {
@@ -27,12 +27,37 @@ object FoldProcessM {
   }
 
   def fromSink[T](sink: Sink[Task, T]) = new FoldM[T, Task, Unit] {
-    type S = Signal[T => Task[Unit]]
-    def start = Task.now(async.toSignal(sink))
-    def fold = (s: S, t: T) => { s.get.run.apply(t).run; s }
-    def end(s: S) = s.close
+    type S = Sink[Task, T] \/ (Sink[Task, T], T => Task[Unit])
+    def start = Task.now(-\/(sink))
+
+    def fold = (s: S, t: T) => {
+      s match {
+        case -\/(pr) =>
+          pr.step match {
+            case Step(Emit(emit), cont) =>
+              val f = emit.head
+              f(t).run
+              \/-((cont.continue, f))
+
+            case Step(Await(req, rcv), cont) =>
+              val f: T => Task[Unit] = rcv(\/-(req.run)).run.take(1).runLastOr((t1: T) => Task.now(())).run
+              f(t).run
+              \/-((cont.continue, f))
+
+            case h @ Halt(c) =>
+              -\/(Process.halt)
+          }
+        case \/-((pr, f)) => f(t).run; s
+
+      }
+    }
+    def end(s: S) = s.fold(_.kill.run, _._1.kill.run)
   }
 
-  def lift[T](f: T => Task[Unit]): SinkTask[T] =
-    fromSink(Process.constant(f))
+  def lift[T](f: T => Task[Unit]): SinkTask[T] = new FoldM[T, Task, Unit] {
+    type S = Unit
+    def start = Task.now(())
+    def fold = (s: S, t: T) => { f(t).run; s }
+    def end(s: S) = Task.now(s)
+  }
 }
