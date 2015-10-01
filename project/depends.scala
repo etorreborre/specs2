5ad7e2ce73d76fda84a5017530ca6a26dfe373ad
Eric Torreborre <etorreborre@yahoo.com>
2015-10-02 08:52:56 +1000

    upgraded scalacheck to 1.12.5

--- project/depends.scala
+++ project/depends.scala
@@ -17,7 +17,7 @@ object depends {
       (if (scalazVersion.startsWith("7.1")) Seq("org.scalaz.stream" %% "scalaz-stream" % "0.7.2a")
        else                                 Seq("org.scalaz.stream" %% "scalaz-stream" % "0.7.2"))
 
-  lazy val scalacheck    = Seq("org.scalacheck" %% "scalacheck"   % "1.12.4")
+  lazy val scalacheck    = Seq("org.scalacheck" %% "scalacheck"   % "1.12.5")
   lazy val mockito       = Seq("org.mockito"    % "mockito-core"  % "1.9.5")
   lazy val junit         = Seq("junit"          % "junit"         % "4.12")
   lazy val hamcrest      = Seq("org.hamcrest"   % "hamcrest-core" % "1.3")
