7f89e0cf5d593815e0c7d5c3f90566b17037f376
Eric Torreborre <etorreborre@yahoo.com>
2015-10-16 17:37:32 +1100

    updated site

---
+++ website/SPECS2-3.6.5/javascript/versions.js
@@ -0,0 +1,36 @@
+var guideVersions = [
+ {id:"../../guide/SPECS2-3.6.5/org.specs2.guide.UserGuide.html", text:"3.6.5"},
+{id:"../../guide/SPECS2-3.6.4/org.specs2.guide.UserGuide.html", text:"3.6.4"},
+{id:"../../guide/SPECS2-3.6.3/org.specs2.guide.UserGuide.html", text:"3.6.3"},
+{id:"../../guide/SPECS2-3.6.2/org.specs2.guide.UserGuide.html", text:"3.6.2"},
+{id:"../../guide/SPECS2-3.6.1/org.specs2.guide.UserGuide.html", text:"3.6.1"},
+{id:"../../guide/SPECS2-3.6/org.specs2.guide.UserGuide.html", text:"3.6"},
+{id:"../../guide/SPECS2-3.5/org.specs2.guide.UserGuide.html", text:"3.5"},
+{id:"../../guide/SPECS2-3.4/org.specs2.guide.UserGuide.html", text:"3.4"},
+{id:"../../guide/SPECS2-3.3.1/org.specs2.guide.UserGuide.html", text:"3.3.1"},
+{id:"../../guide/SPECS2-3.3/org.specs2.guide.UserGuide.html", text:"3.3"},
+{id:"../../guide/SPECS2-3.2/org.specs2.guide.UserGuide.html", text:"3.2"},
+{id:"../../guide/SPECS2-3.1.1/org.specs2.guide.UserGuide.html", text:"3.1.1"},
+{id:"../../guide/SPECS2-3.1/org.specs2.guide.UserGuide.html", text:"3.1"},
+{id:"../../guide/SPECS2-3.0.1/org.specs2.guide.UserGuide.html", text:"3.0.1"},
+{id:"../../guide/SPECS2-3.0/org.specs2.guide.UserGuide.html", text:"3.0"},
+{id:"../../guide/SPECS2-2.4.17/org.specs2.guide.UserGuide.html", text:"2.4.17"}
+];
+ var apiVersions = [
+ {id:"../../api/SPECS2-3.6.5/index.html", text:"3.6.5"},
+{id:"../../api/SPECS2-3.6.4/index.html", text:"3.6.4"},
+{id:"../../api/SPECS2-3.6.3/index.html", text:"3.6.3"},
+{id:"../../api/SPECS2-3.6.2/index.html", text:"3.6.2"},
+{id:"../../api/SPECS2-3.6.1/index.html", text:"3.6.1"},
+{id:"../../api/SPECS2-3.6/index.html", text:"3.6"},
+{id:"../../api/SPECS2-3.5/index.html", text:"3.5"},
+{id:"../../api/SPECS2-3.4/index.html", text:"3.4"},
+{id:"../../api/SPECS2-3.3.1/index.html", text:"3.3.1"},
+{id:"../../api/SPECS2-3.3/index.html", text:"3.3"},
+{id:"../../api/SPECS2-3.2/index.html", text:"3.2"},
+{id:"../../api/SPECS2-3.1.1/index.html", text:"3.1.1"},
+{id:"../../api/SPECS2-3.1/index.html", text:"3.1"},
+{id:"../../api/SPECS2-3.0.1/index.html", text:"3.0.1"},
+{id:"../../api/SPECS2-3.0/index.html", text:"3.0"},
+{id:"../../api/SPECS2-2.4.17/index.html", text:"2.4.17"}
+];
