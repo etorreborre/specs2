d2c2f4173385608d7e383cae1d89416126998627
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2015-11-13 00:50:30 -0700

    Probably fixes broken link mentioned in issue #433

--- guide/src/test/scala/org/specs2/guide/matchers/FutureMatchers.scala
+++ guide/src/test/scala/org/specs2/guide/matchers/FutureMatchers.scala
@@ -38,7 +38,7 @@ All of the above is applicable to `scalaz.concurrent.Future` by using the method
 
 #### Execution
 
-The `await`/`attempt` methods require an implicit `org.specs2.concurrent.ExecutionEnv` (see [here](org.specs2.guide.ExecutionEnvironment.html) for more details). You can pass one in the body of your examples:${snippet{
+The `await`/`attempt` methods require an implicit `org.specs2.concurrent.ExecutionEnv` (see [here](org.specs2.guide.ExecutionEnvironments.html) for more details). You can pass one in the body of your examples:${snippet{
 class MyFutureSpec extends Specification { def is = s2"""
 
  Let's check this scala future ${ implicit ee: EE =>
