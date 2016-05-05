f8ba39b8afcc9b91b2f7b3ad550a5af4308b1fef
Luís Fonseca <luis@shiftforward.eu>
2016-05-05 16:04:32 +0100

    Make EventuallyResults consistent with EventuallyMatchers
    This changes the API so that the eventually returns  again. This will make specs that were
    built under this assumption continue to work as expected.
    The previous issue #462 was kept resolved.

--- common/src/main/scala/org/specs2/execute/EventuallyResults.scala
+++ common/src/main/scala/org/specs2/execute/EventuallyResults.scala
@@ -13,22 +13,26 @@ trait EventuallyResults {
   /**
    * @return a matcher that will retry the nested matcher a given number of times
    */
-  def eventually[T : AsResult](retries: Int, sleep: Duration)(result: =>T): Result = {
-    def retry(retries: Int, sleep: Duration, r: =>T): Result = {
-      lazy val t = r
-      val result = ResultExecution.execute(t)(AsResult(_))
-      if (result.isSuccess || retries <= 1)
-        result
-      else {
-        Thread.sleep(sleep.toMillis)
-        retry(retries - 1, sleep, r)
+  def eventually[T : AsResult](retries: Int, sleep: Duration)(result: =>T): T = {
+    def retry(retries: Int, sleep: Duration, r: =>T): T = {
+      if (retries <= 1) {
+        r
+      } else {
+        lazy val t = r
+        val result = ResultExecution.execute(t)(AsResult(_))
+        if (result.isSuccess)
+          t
+        else {
+          Thread.sleep(sleep.toMillis)
+          retry(retries - 1, sleep, r)
+        }
       }
     }
     retry(retries, sleep, result)
   }
 
   /** @return a result that is retried at least 40 times until it's ok */
-  def eventually[T : AsResult](result: =>T): Result =
+  def eventually[T : AsResult](result: =>T): T =
     eventually(40, 100.millis)(result)
 }
 
