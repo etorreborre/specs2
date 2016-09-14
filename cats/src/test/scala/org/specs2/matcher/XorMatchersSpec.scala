ed7b9306008f856e41fbaf7bd228bd587352fe1a
noama@wix.com <noamal@Noams-MacBook-Pro-3.local>
2016-09-14 17:24:53 +0300

    fixed matcher error message

--- cats/src/test/scala/org/specs2/matcher/XorMatchersSpec.scala
+++ cats/src/test/scala/org/specs2/matcher/XorMatchersSpec.scala
@@ -14,7 +14,7 @@ class XorMatchersSpec extends Spec with ResultMatchers with XorMatchers { def is
   ${ Right(1) must beXorRight(===(1)) }
   ${ Right(Seq(1)) must beXorRight(===(Seq(1))) }
   ${ Right(1) must beXorRight.like { case i => i must be_>(0) } }
-  ${ (Right(1) must beXorRight.like { case i => i must be_<(0) }) returns "'Right(1)' is Right but 1 is not less than 0" }
+  ${ (Right(1) must beXorRight.like { case i => i must be_<(0) }) returns "Right(1) is Right but 1 is not less than 0" }
 
   beXorLeft checks if an element is Left(_)                                                                              
   ${ Left(1) must beXorLeft(1) }
