be52cdce0a0a1bc31a9f04410cf2efb411be4078
Eric Torreborre <etorreborre@yahoo.com>
2016-11-17 08:45:53 +0100

    fixed the description of hamcrest results

--- mock/src/main/scala/org/specs2/mock/HamcrestMatcherAdapter.scala
+++ mock/src/main/scala/org/specs2/mock/HamcrestMatcherAdapter.scala
@@ -22,7 +22,7 @@ case class HamcrestMatcherAdapter[T](m: Matcher[T]) extends BaseMatcher[T] {
     val i = if (item != null && item.isInstanceOf[Function0[_]]) item.asInstanceOf[Function0[_]].apply().asInstanceOf[A] else item
     try {
       matcher.apply(Expectable(i)) match {
-        case f: MatchFailure[_] => message = f.koMessage; false
+        case f: MatchFailure[_] => message = f.okMessage; false
         case _ => true
       }
     // a class cast exception can happen if we tried: vet.treat(dog); there must be one(vet).treat(bird) (see issue #222)
