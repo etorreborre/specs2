3230b2f88ae26cdce8d9878e7bb166c328ad0bbe
Eric Torreborre <etorreborre@yahoo.com>
2017-08-14 12:29:35 +0200

    set more threadsnb on travis

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,1 +1,1 @@
-sbt -jvm-opts .travis.jvmopts -sbt-version 0.13.15 -scala-version $TRAVIS_SCALA_VERSION "$1/testOnly -- xonly timefactor 3 neverstore exclude travis"
+sbt -jvm-opts .travis.jvmopts -sbt-version 0.13.15 -scala-version $TRAVIS_SCALA_VERSION "$1/testOnly -- xonly timefactor 3 neverstore exclude travis threadsnb 100"
