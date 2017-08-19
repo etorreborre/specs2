c7c68fdaacdf2231d28f44b3334d61e05ca9487c
Eric Torreborre <etorreborre@yahoo.com>
2017-08-19 22:39:05 +0200

    don't run classes in parallel in travis to avoid node.js connection issues

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,1 +1,1 @@
-sbt -jvm-opts .travis.jvmopts -sbt-version 0.13.15 -scala-version $TRAVIS_SCALA_VERSION "$1/testOnly -- timefactor 3 neverstore exclude travis"
+sbt -jvm-opts .travis.jvmopts -sbt-version 0.13.15 -scala-version $TRAVIS_SCALA_VERSION ";set parallelExecution in ThisBuild := false; $1/testOnly -- timefactor 3 neverstore exclude travis"
