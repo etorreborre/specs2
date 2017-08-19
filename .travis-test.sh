c39e20b0b0f1e4ca6e15e26cd622f0ae8e9c2805
Eric Torreborre <etorreborre@yahoo.com>
2017-08-19 18:47:08 +0200

    use the default number of threads on travis

--- .travis-test.sh
+++ .travis-test.sh
@@ -1,1 +1,1 @@
-sbt -jvm-opts .travis.jvmopts -sbt-version 0.13.15 -scala-version $TRAVIS_SCALA_VERSION "$1/testOnly -- timefactor 3 neverstore exclude travis specs2threadsnb 10 threadsnb 10"
+sbt -jvm-opts .travis.jvmopts -sbt-version 0.13.15 -scala-version $TRAVIS_SCALA_VERSION "$1/testOnly -- timefactor 3 neverstore exclude travis"
