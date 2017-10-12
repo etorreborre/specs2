4aad4c287db989c1922a0029b0e28aec27c614d5
Eric Torreborre <etorreborre@yahoo.com>
2017-10-12 21:53:52 +0200

    updated the website for 4.0.0

---
+++ website/SPECS2-4.0.0/javascript/versions.js
@@ -0,0 +1,8 @@
+var guideVersions = [
+ {id:"../../guide/SPECS2-3.9.4/org.specs2.guide.UserGuide.html", text:"3.9.4"},
+{id:"../../guide/SPECS2-4.0.0/org.specs2.guide.UserGuide.html", text:"4.0.0"}
+];
+ var apiVersions = [
+ {id:"../../api/SPECS2-3.9.4/index.html", text:"3.9.4"},
+{id:"../../api/SPECS2-4.0.0/index.html", text:"4.0.0"}
+];
