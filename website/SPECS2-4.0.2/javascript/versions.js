b4d801389b88aece1cf1c95957e8bb4891fa5483
Eric Torreborre <etorreborre@yahoo.com>
2017-12-01 15:44:44 +0100

    updated site

---
+++ website/SPECS2-4.0.2/javascript/versions.js
@@ -0,0 +1,10 @@
+var guideVersions = [
+ {id:"../../guide/SPECS2-3.9.4/org.specs2.guide.UserGuide.html", text:"3.9.4"},
+{id:"../../guide/SPECS2-4.0.0/org.specs2.guide.UserGuide.html", text:"4.0.0"},
+{id:"../../guide/SPECS2-4.0.1/org.specs2.guide.UserGuide.html", text:"4.0.1"}
+];
+ var apiVersions = [
+ {id:"../../api/SPECS2-3.9.4/index.html", text:"3.9.4"},
+{id:"../../api/SPECS2-4.0.0/index.html", text:"4.0.0"},
+{id:"../../api/SPECS2-4.0.1/index.html", text:"4.0.1"}
+];
