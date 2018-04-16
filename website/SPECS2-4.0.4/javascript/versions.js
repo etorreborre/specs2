09a3f57e2e751a5d5cff19a663adbcb9e8525966
Eric Torreborre <etorreborre@yahoo.com>
2018-04-16 17:22:39 +0200

    updated site

---
+++ website/SPECS2-4.0.4/javascript/versions.js
@@ -0,0 +1,16 @@
+var guideVersions = [
+ {id:"../../guide/SPECS2-3.9.4/org.specs2.guide.UserGuide.html", text:"3.9.4"},
+{id:"../../guide/SPECS2-4.0.0/org.specs2.guide.UserGuide.html", text:"4.0.0"},
+{id:"../../guide/SPECS2-4.0.1/org.specs2.guide.UserGuide.html", text:"4.0.1"},
+{id:"../../guide/SPECS2-4.0.2/org.specs2.guide.UserGuide.html", text:"4.0.2"},
+{id:"../../guide/SPECS2-4.0.3/org.specs2.guide.UserGuide.html", text:"4.0.3"},
+{id:"../../guide/SPECS2-4.0.4/org.specs2.guide.UserGuide.html", text:"4.0.4"}
+];
+ var apiVersions = [
+ {id:"../../api/SPECS2-3.9.4/index.html", text:"3.9.4"},
+{id:"../../api/SPECS2-4.0.0/index.html", text:"4.0.0"},
+{id:"../../api/SPECS2-4.0.1/index.html", text:"4.0.1"},
+{id:"../../api/SPECS2-4.0.2/index.html", text:"4.0.2"},
+{id:"../../api/SPECS2-4.0.3/index.html", text:"4.0.3"},
+{id:"../../api/SPECS2-4.0.4/index.html", text:"4.0.4"}
+];
