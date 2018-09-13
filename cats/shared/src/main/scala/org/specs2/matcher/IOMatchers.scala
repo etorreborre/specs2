5f466a090a42fd95e4e13c75c9a62c96a8a56657
Diego E. Alonso-Blas <diego.e.a@47deg.com>
2018-09-13 01:18:11 +0100

    Refactor IOMatchers to add Generic
    We split the IOMatchers trait to make it independent from IO.
    - We extract a generic `RuntimedMatchers` trait, which is generic on
      the effect type `F[_]`, and has most methods from `IOMatchers`.
    - The generic `RuntimedMatchers` defines two methods, that act as
      placeholders for `IO.timeout` and `IO.unsafeRunSync`.
    - To the `RunTimedMatchers` trait, we add an inner `TimedMatcher`
      class, that has most of the logic from the `IOMatcher` class.
      This class has methods, akin to `checkIOWithDuration` and
      `checkIO`, but with different names.
    
    Having moved most of the logic to `RuntimedMatchers`, the `IOMatcher`
    becomes just a specific instance of `RunTimedMatchers[IO]`, and the
    two methods just call the IO methods.
    
    - We keep an `IOMatcher` inner class inside `IOMatchers`, for
      compatibility. This class keeps the two `check` methods above.

--- cats/shared/src/main/scala/org/specs2/matcher/IOMatchers.scala
+++ cats/shared/src/main/scala/org/specs2/matcher/IOMatchers.scala
@@ -7,61 +7,82 @@ import org.specs2.matcher.describe.Diffable
 import scala.concurrent.TimeoutException
 import scala.concurrent.duration.FiniteDuration
 import org.specs2.text.NotNullStrings._
-import scala.concurrent.ExecutionContext.Implicits.global
 
-trait IOMatchers {
-  def returnOk[T]: IOMatcher[T] =
+trait RunTimedMatchers[F[_]] {
+
+  protected def runWithTimeout[A](fa: F[A], timeout: FiniteDuration): A
+  protected def runAwait[A](fa: F[A]) : A
+
+  def returnOk[T]: TimedMatcher[T] =
     attemptRun(ValueCheck.alwaysOk, None)
 
-  def returnValue[T](check: ValueCheck[T]): IOMatcher[T] =
+  def returnValue[T](check: ValueCheck[T]): TimedMatcher[T] =
     attemptRun(check, None)
 
-  def returnBefore[T](duration: FiniteDuration): IOMatcher[T] =
+  def returnBefore[T](duration: FiniteDuration): TimedMatcher[T] =
     attemptRun(ValueCheck.alwaysOk, Some(duration))
 
-  private[specs2] def attemptRun[T](check: ValueCheck[T], duration: Option[FiniteDuration]): IOMatcher[T] =
-    IOMatcher(check, duration)
+  protected[specs2] def attemptRun[T](check: ValueCheck[T], duration: Option[FiniteDuration]): TimedMatcher[T] =
+    new TimedMatcher(check, duration)
 
-  case class IOMatcher[T](check: ValueCheck[T], duration: Option[FiniteDuration]) extends Matcher[IO[T]] {
-    def apply[S <: IO[T]](e: Expectable[S]) = duration.fold(checkIO(e))(checkIOWithDuration(e,_))
+  class TimedMatcher[T](check: ValueCheck[T], duration: Option[FiniteDuration]) extends Matcher[F[T]] {
 
-    def checkIOWithDuration[S <: IO[T]](e: Expectable[S], d: FiniteDuration): MatchResult[S] = {
+    def apply[S <: F[T]](e: Expectable[S]) = duration.fold(checkAwait(e))(checkWithDuration(e,_))
+
+    def checkWithDuration[S <: F[T]](e: Expectable[S], d: FiniteDuration): MatchResult[S] =
       try {
-        checkResult(e)(e.value.timeout(d).unsafeRunSync())
+        checkResult(e)(runWithTimeout(e.value, d))
       } catch {
         case x: TimeoutException => timeoutResult(e,d)
         case x: Exception => errorResult(e)(x)
       }
-    }
 
-    def checkIO[S <: IO[T]](e: Expectable[S]) = {
+    def checkAwait[S <: F[T]](e: Expectable[S]) =
       try {
-        checkResult(e)(e.value.unsafeRunSync())
+        checkResult(e)(runAwait(e.value))
       } catch {
         case x: Exception => errorResult(e)(x)
       }
-    }
 
-    def before(d: FiniteDuration): IOMatcher[T] =
-      copy(duration = Some(d))
+    def before(d: FiniteDuration): TimedMatcher[T] =
+      new TimedMatcher(check, Some(d))
 
-    def withValue(check: ValueCheck[T]): IOMatcher[T] =
-      copy(check = check)
+    def withValue(check: ValueCheck[T]): TimedMatcher[T] =
+      new TimedMatcher(check, duration)
 
-    def withValue(t: T)(implicit di: Diffable[T]): IOMatcher[T] =
+    def withValue(t: T)(implicit di: Diffable[T]): TimedMatcher[T] =
       withValue(valueIsTypedValueCheck(t))
 
-    private def timeoutResult[S <: IO[T]](e: Expectable[S], d: FiniteDuration): MatchResult[S] = {
+    private def timeoutResult[S <: F[T]](e: Expectable[S], d: FiniteDuration): MatchResult[S] = {
       val message = s"Timeout after ${d.toMillis} milliseconds"
       result(false, message, message, e)
     }
 
-    private def errorResult[S <: IO[T]](e: Expectable[S])(t: Throwable): MatchResult[S] = {
+    private def errorResult[S <: F[T]](e: Expectable[S])(t: Throwable): MatchResult[S] = {
       val message = "an exception was thrown "+t.getMessage.notNull+" "+t.getClass.getName
       result(false, message, message, e)
     }
 
-    private def checkResult[S <: IO[T]](e: Expectable[S])(t: T): MatchResult[S] =
+    private def checkResult[S <: F[T]](e: Expectable[S])(t: T): MatchResult[S] =
       result(check.check(t), e)
   }
+}
+
+trait IOMatchers extends RunTimedMatchers[IO] {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  protected def runWithTimeout[A](fa: IO[A], d: FiniteDuration): A = fa.timeout(d).unsafeRunSync
+  protected def runAwait[A](fa: IO[A]) : A = fa.unsafeRunSync
+
+  protected[specs2] override def attemptRun[T](check: ValueCheck[T], duration: Option[FiniteDuration]): IOMatcher[T] =
+    IOMatcher(check, duration)
+
+  case class IOMatcher[T](check: ValueCheck[T], duration: Option[FiniteDuration])
+      extends TimedMatcher[T](check, duration) {
+    def checkIOWithDuration[S <: IO[T]](e: Expectable[S], d: FiniteDuration): MatchResult[S] =
+      checkWithDuration(e, d)
+    def checkIO[S <: IO[T]](e: Expectable[S]) = checkAwait(e)
+  }
+
 }
