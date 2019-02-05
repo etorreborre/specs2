7f31b4d59571b13f156962983ee1576556a92f10
Brian P. Holt <bholt@dwolla.com>
2019-02-04 19:47:58 -0600

    break out IOMatchers to only expose working methods on each platform

---
+++ cats/jvm/src/test/scala/org/specs2/matcher/IOMatchersJvmSpec.scala
@@ -0,0 +1,22 @@
+package org.specs2
+package matcher
+
+import cats.effect._
+import cats.implicits._
+
+import scala.concurrent.duration._
+
+class IOMatchersJvmSpec extends mutable.Specification with IOMatchers with ResultMatchers {
+
+  "It is possible to check the execution of tasks" >> {
+    "check that the task finishes before a given time" >> {
+      { IO(1) must returnBefore(10.millis) }
+      { (IO.sleep(50.millis).as(1) must returnBefore(10.millis)) returns "Timeout after 10 milliseconds" }
+    }
+
+    "check the return value with a timeout" >> {
+      { IO.sleep(5.millis).as(1) must returnBefore(10.millis).withValue(1) }
+      { IO.sleep(5.millis).as(1) must returnValue(1).before(10.millis) }
+    }
+  }
+}
--- cats/shared/src/test/scala/org/specs2/matcher/IOMatchersSpec.scala
+++ cats/shared/src/test/scala/org/specs2/matcher/IOMatchersSpec.scala
@@ -2,25 +2,12 @@ package org.specs2
 package matcher
 
 import cats.effect._
-import cats.implicits._
-
-import scala.concurrent.duration._
 
 class IOMatchersSpec extends mutable.Specification with IOMatchers with ResultMatchers {
   "It is possible to check the execution of tasks" >> {
     "check the return value" >> {
       { IO(1) must returnValue(1) }
       { (IO(1) must returnValue(2)) returns "1 != 2" }
-    }
-
-    "check that the task finishes before a given time" >> {
-      { IO(1) must returnBefore(10.millis) }
-      { (IO.sleep(50.millis).as(1) must returnBefore(10.millis)) returns "Timeout after 10 milliseconds" }
-    }
-
-    "check the return value with a timeout" >> {
-      { IO.sleep(5.millis).as(1) must returnBefore(10.millis).withValue(1) }
-      { IO.sleep(5.millis).as(1) must returnValue(1).before(10.millis) }
     }
 
     "an IO[MatchResult[_]] is implicitly a Result" >> {
