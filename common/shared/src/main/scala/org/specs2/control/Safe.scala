f4c4ec611178ec33c8116e58fe2bffb9709d03f8
etorreborre <eric.torreborre@symbiont.io>
2019-12-01 13:14:21 +0100

    moved some classes to their own file: Operation, Safe, Finalizer

---
+++ common/shared/src/main/scala/org/specs2/control/Safe.scala
@@ -0,0 +1,33 @@
+package org.specs2
+package control
+
+/**
+ * Typeclass for "safe" actions which are actions which can
+ * be attempted or finalized
+ */
+trait Safe[F[_]] {
+  def finalizeWith[A](fa: F[A], f: Finalizer): F[A]
+  def attempt[A](fa: F[A]): F[Throwable Either A]
+}
+
+object Safe {
+  def apply[F[_]](implicit f: Safe[F]): Safe[F] =
+    f
+}
+
+/**
+ * Delayed action
+ */
+case class Finalizer(run: () => Unit) {
+  def attempt: Option[Throwable] =
+    try { run(); None }
+    catch { case t: Throwable => Some(t) }
+}
+
+object Finalizer {
+  def runFinalizers(finalizers: Vector[Finalizer]): Unit =
+    finalizers.foreach(_.attempt)
+
+  def create(action: =>Unit): Finalizer =
+    Finalizer(() => action)
+}
