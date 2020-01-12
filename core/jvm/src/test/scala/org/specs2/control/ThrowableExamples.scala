0fe4e3e87237ee214c5b27ba94bbfb48bc2ad056
etorreborre <eric.torreborre@symbiont.io>
2020-01-12 17:58:12 +0100

    migrated the Throwablex spec

--- core/jvm/src/test/scala/org/specs2/control/ThrowableExamples.scala
+++ core/jvm/src/test/scala/org/specs2/control/ThrowableExamples.scala
@@ -1,9 +1,10 @@
 package org.specs2
 package control
 
-trait ThrowablexContext {
+// This object is put in a separate file since there are some
+// expectations related to the exact line numbers of thrown exceptions
+object ThrowableExamples {
   lazy val cause = new IllegalArgumentException("cause")
   lazy val e = new Exception("message", cause)
   lazy val trace = e.getStackTrace()(0)
 }
-object ThrowablexContext extends ThrowablexContext
--- core/jvm/src/test/scala/org/specs2/control/ThrowablexSpec.scala
+++ core/jvm/src/test/scala/org/specs2/control/ThrowablexSpec.scala
@@ -1,49 +1,45 @@
 package org.specs2
 package control
 
-import specification._
+import ThrowableExamples._
 
-class ThrowablexSpec extends script.Specification with Throwablex with Grouped { def is = s2"""
+class ThrowablexSpec extends Specification with Throwablex { def is = s2"""
 
 The Throwablex trait provides extensions to regular throwables:
 
- # It provides methods to access nested exceptions
-   + chainedExceptions returns a list of nested exceptions
-   + getFullStackTrace returns a list of all nested stackTraceElements
-   + messageAndCause returns the exception message and its cause if any
+It provides methods to access chained exceptions
+ chainedExceptions returns a list of chained exceptions $chained1
+ getFullStackTrace returns a list of all chained stackTraceElements $chained2
+ messageAndCause returns the exception message and its cause if any $chained3
 
- # It has location methods
-   + to get the name of the file and the line from an exception
-   + to get the class name and line number of an exception
-   + to get the class name, file name and line number of an exception
-   + to get the path of an exception
+It has location methods
+ to get the name of the file and the line from an exception $location1
+ to get the class name and line number of an exception $location2
+ to get the class name, file name and line number of an exception $location3
+ to get the path of an exception $location4
 
- # It allows to filter stacktraces
-   + to filter all the lines matching a given pattern
-   + to filter all the lines not matching a given pattern
+It allows to filter stacktraces
+ to filter all the lines matching a given pattern $filter1
+ to filter all the lines not matching a given pattern $filter2
 
- # It provides utility functions for stacktrace elements
-   + apply returns the nth element
-   + headOption returns the first element as an option
-                                                                                                                 """
+It provides utility functions for stacktrace elements
+ apply returns the nth element $stack1
+ headOption returns the first element as an option $stack2
 
-  "chained" - new group with ThrowablexContext {
-    eg := e.chainedExceptions      === List(e.getCause)
-    eg := e.getFullStackTrace.size === e.getStackTrace.length + e.getCause.getStackTrace.length
-    eg := e.messageAndCause        === "message. Cause: cause"
-  }
-  "location" - new group with ThrowablexContext {
-    eg := e.location                === "ThrowablexContext.scala:6"
-    eg := e.classLocation           === "org.specs2.control.ThrowablexContext:6"
-    eg := e.fullLocation            === "org.specs2.control.ThrowablexContext (ThrowablexContext.scala:6)"
-    eg := TraceLocation(trace).path === "org/specs2/control/ThrowablexContext.scala"
-  }
-  "filter" - new group with ThrowablexContext {
-    eg := e.filter("org.specs2.specification.core").getStackTrace.toList.map(_.toString) must containMatch("org.specs2.specification.core")
-    eg := e.filterNot("org.specs2.control").getStackTrace.toList.map(_.toString) must not (containMatch("org.specs2.control"))
-  }
-  "stack" - new group with ThrowablexContext {
-    eg := e(2).toString aka e.getStackTrace.mkString("\n") must beMatching(".*org.specs2.control.ThrowablexSpec\\$.*")
-    eg := e.headOption.map(_.toString).toIterable must containMatch("ThrowablexContext")
-  }
+"""
+
+  def chained1 = e.chainedExceptions      === List(e.getCause)
+  def chained2 = e.getFullStackTrace.size === e.getStackTrace.length + e.getCause.getStackTrace.length
+  def chained3 = e.messageAndCause        === "message. Cause: cause"
+
+  def location1 = e.location                === "ThrowableExamples.scala:8"
+  def location2 = e.classLocation           === "org.specs2.control.ThrowableExamples:8"
+  def location3 = e.fullLocation            === "org.specs2.control.ThrowableExamples (ThrowableExamples.scala:8)"
+  def location4 = TraceLocation(trace).path === "org/specs2/control/ThrowableExamples.scala"
+
+  def filter1 = e.filter("org.specs2.specification.core").getStackTrace.toList.map(_.toString) must containMatch("org.specs2.specification.core")
+  def filter2 = e.filterNot("org.specs2.control").getStackTrace.toList.map(_.toString) must not (containMatch("org.specs2.control"))
+
+  def stack1 = e(1).toString aka e.getStackTrace.mkString("\n") must beMatching(".*org.specs2.control.ThrowableExamples\\$.e.*")
+  def stack2 = e.headOption.map(_.toString).toIterable must containMatch("ThrowableExamples")
 }
