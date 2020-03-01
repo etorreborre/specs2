e5a6a3746a6c48bb0bc432018a592da128f71ca9
etorreborre <eric.torreborre@symbiont.io>
2020-03-01 15:17:44 +0100

    feature: more complete implementation of Classes for ScalaJS

--- common/js-native/src/main/scala/org/specs2/reflect/Classes.scala
+++ common/js-native/src/main/scala/org/specs2/reflect/Classes.scala
@@ -11,38 +11,40 @@ trait Classes extends ClassOperations {
   type EnableReflectiveInstantiation =
     org.portablescala.reflect.annotation.EnableReflectiveInstantiation
 
-  def newInstance(name: String, loader: ClassLoader): Any =
+  def newInstance(name: String): Any =
     Reflect
-      .lookupInstantiatableClass(name, loader)
+      .lookupInstantiatableClass(name)
       .getOrElse(throw new ClassNotFoundException(name))
       .newInstance
 
-  def loadModule(name: String, loader: ClassLoader): Any =
+  def loadModule(name: String): Any =
     Reflect
-      .lookupLoadableModuleClass(name, loader)
+      .lookupLoadableModuleClass(name)
       .getOrElse(throw new ClassNotFoundException(name))
       .loadModule
 
   def createInstance[T <: AnyRef](className: String)(implicit m: ClassTag[T]): Operation[T] =
-    throw new Exception("Classes.createInstance: no js implementation")
+    if (className.endsWith("$"))
+      protect[OperationStack, T](loadModule(className).asInstanceOf[T])
+    else
+      protect[OperationStack, T](newInstance(className).asInstanceOf[T])
 
   def createInstance[T <: AnyRef](className: String, loader: ClassLoader, defaultInstances: =>List[AnyRef] = Nil)(implicit m: ClassTag[T]): Operation[T] =
-    if (className.endsWith("$"))
-      protect[OperationStack, T](loadModule(className, loader).asInstanceOf[T])
-    else
-      protect[OperationStack, T](newInstance(className, loader).asInstanceOf[T])
+    createInstance[T](className)
 
-  def createInstanceFromClass[T <: AnyRef](klass: Class[T], defaultInstances: =>List[AnyRef])(implicit m: ClassTag[T]): Operation[T] = {
-    Use(m)
-    throw new Exception("Classes.createInstanceFromClass: no js implementation")
-  }
+  def createInstanceFromClass[T <: AnyRef](klass: Class[T], defaultInstances: =>List[AnyRef])(implicit m: ClassTag[T]): Operation[T] =
+    createInstance[T](klass.getName)(m)
 
   def createInstanceFromClass[T <: AnyRef](klass: Class[T], loader: ClassLoader, defaultInstances: =>List[AnyRef] = Nil)(implicit m: ClassTag[T]): Operation[T] =
-    createInstance(klass.getName, loader, defaultInstances)
+    createInstance(klass.getName, loader, defaultInstances)(m)
 
   /** try to create an instance but return an exception if this is not possible */
   def createInstanceEither[T <: AnyRef](className: String, loader: ClassLoader, defaultInstances: =>List[AnyRef] = Nil)(implicit m: ClassTag[T]): Operation[Throwable Either T] =
-    throw new Exception("Classes.createInstanceEither: no js implementation")
+    try {
+      createInstance[T](className).map(Right(_))
+    } catch {
+      case e: Throwable => Operations.ok(Left(e))
+    }
 
   def loadClassEither[T <: AnyRef](className: String, loader: ClassLoader): Operation[Throwable Either Class[T]] =
     throw new Exception("Classes.loadClassEither: no js implementation")
@@ -51,7 +53,9 @@ trait Classes extends ClassOperations {
     throw new Exception("Classes.loadClass: no js implementation")
 
   def existsClass(className: String, loader: ClassLoader): Operation[Boolean] =
-    throw new Exception("Classes.existsClass: no js implementation")
+    protect[OperationStack, Boolean](
+      Reflect.lookupInstantiatableClass(className).nonEmpty ||
+      Reflect.lookupLoadableModuleClass(className).nonEmpty)
 
 }
 
