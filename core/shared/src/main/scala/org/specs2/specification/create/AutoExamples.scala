d78f70582ef794aadbef0c72c336b2a3c8152b9c
etorreborre <eric.torreborre@symbiont.io>
2020-04-12 13:25:01 +0200

    dotty feature: migrated the auto-examples macro

--- core/shared/src/main/scala/org/specs2/specification/create/AutoExamples.scala
+++ core/shared/src/main/scala/org/specs2/specification/create/AutoExamples.scala
@@ -3,23 +3,35 @@ package specification
 package create
 
 import execute.AsResult
-import reflect.MacroContext._
-import reflect.Macros
 import text.Trim._
-import org.specs2.specification.core.{Description, Fragment, Fragments}
+import core.{Description, Fragment, Fragments}
+import scala.quoted._
 
 /**
  * This trait allows to create examples where the description is the code itself
  * and the code returns an AsResult value
  */
 trait AutoExamples extends FragmentsFactory {
+  inline def eg[T](code: =>T)(using asResult: AsResult[T]): Fragments =
+    ${ AutoExamples.create[T]('code, 'asResult, 'postProcessAutoExample) }
 
-  implicit def eg[T : AsResult](code: =>T): Fragments = macro AutoExamples.create[T]
+  /** this function is introduced just to allow the mutable specification to register the newly created fragments */
+  def postProcessAutoExample(fs: Fragments): Fragments =
+    fs
+}
+
+object AutoExamples extends AutoExamples {
+
+  def create[T](code: Expr[T], asResult: Expr[AsResult[T]], postProcess: Expr[Fragments => Fragments])(using qctx: QuoteContext)(using t: Type[T]): Expr[Fragments] = {
+    val expression = Expr(code.show)
+    Expr.betaReduce('{(ex: String, c: $t, as: AsResult[$t], post: Fragments => Fragments) =>
+      post(createExample[$t](ex, c, as))})(expression, code, asResult, postProcess)
+  }
 
   def createExample[T](expression: String, code: =>T, asResult: AsResult[T]): Fragments =
-    Fragments(makeExample(expression, code, asResult))
+    Fragments(AutoExamples.makeExample(expression, code, asResult))
 
-  protected def makeExample[T](expression: String, code: =>T, asResult: AsResult[T]): Fragment =
+  def makeExample[T](expression: String, code: =>T, asResult: AsResult[T]): Fragment =
     fragmentFactory.example(Description.code(trimExpression(expression)), code)(asResult)
 
   private[specs2] def trimExpression(call: String) = {
@@ -32,15 +44,3 @@ trait AutoExamples extends FragmentsFactory {
   }
 
 }
-
-object AutoExamples extends AutoExamples {
-
-  def create[T](c: Context)(code: c.Expr[T])(asResult: c.Expr[AsResult[T]]): c.Expr[Fragments] = {
-    import c.{universe => u}; import u._
-    import Macros._
-    val result = c.Expr(methodCall(c)("createExample", stringExprMacroPos(c)(code), code.tree.duplicate, asResult.tree))
-    c.Expr(atPos(c.prefix.tree.pos)(result.tree))
-  }
-
-}
-
--- core/shared/src/main/scala/org/specs2/specification/dsl/mutable/AutoExamples.scala
+++ core/shared/src/main/scala/org/specs2/specification/dsl/mutable/AutoExamples.scala
@@ -4,14 +4,16 @@ package dsl
 package mutable
 
 import execute.AsResult
-import org.specs2.specification.core.{Fragments}
+import core.{Fragments}
+import create.{FragmentsFactory}
 
 /**
  * Auto-example creation for mutable specifications
  */
 trait AutoExamples extends create.AutoExamples with FragmentBuilder with FragmentsFactory {
-  override def createExample[T](expression: String, code: =>T, asResult: AsResult[T]): Fragments = {
-    addFragment(makeExample(expression, code, asResult))
+
+  override def postProcessAutoExample(fs: Fragments): Fragments = {
+    addFragments(fs)
     addFragment(fragmentFactory.break)
     Fragments.empty
   }
