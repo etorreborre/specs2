d6b4b9e031a2e693e563fe480b9fc6f1bde73424
Seth Tisue <seth@tisue.net>
2020-05-28 23:04:13 -0700

    make sbt version in scripts match project/build.properties

--- .travis-test-only.sh
+++ .travis-test-only.sh
@@ -1,2 +1,2 @@
-sbt -jvm-opts .travis.jvmopts -sbt-version 1.2.8 -scala-version $TRAVIS_SCALA_VERSION ";set parallelExecution in ThisBuild := false; $1/testOnly $2 -- xonly timefactor 3 neverstore exclude travis"
+sbt -jvm-opts .travis.jvmopts -sbt-version 1.3.10 -scala-version $TRAVIS_SCALA_VERSION ";set parallelExecution in ThisBuild := false; $1/testOnly $2 -- xonly timefactor 3 neverstore exclude travis"
 
