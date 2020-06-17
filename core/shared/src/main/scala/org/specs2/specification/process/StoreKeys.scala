f8937df490b4aad975101a75a955bd4b907dcd33
etorreborre <eric.torreborre@symbiont.io>
2020-06-17 08:54:17 +0200

    style: removed some unused imports

--- core/shared/src/main/scala/org/specs2/specification/process/StoreKeys.scala
+++ core/shared/src/main/scala/org/specs2/specification/process/StoreKeys.scala
@@ -7,8 +7,6 @@ import org.specs2.io.Key
 import time.SimpleTimer
 import scala.util.Try
 import org.specs2.fp.syntax._
-import org.specs2.fp.syntax._
-import org.specs2.fp.syntax._
 
 object StoreKeys {
   def resolve[A](key: Key[A]): String =
