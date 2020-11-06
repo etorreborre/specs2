3aa72296ab0afef476c9ab1e9a505c2f6e0e4d29
Scala Steward <me@scala-steward.org>
2020-11-06 14:24:22 +0100

    Update scalacheck to 1.15.1

--- project/depends.scala
+++ project/depends.scala
@@ -56,7 +56,7 @@ object depends {
   lazy val tagsoup = "org.ccil.cowan.tagsoup" % "tagsoup" % "1.2.1"
 
   lazy val scalacheck = Def.setting {
-    "org.scalacheck" %%% "scalacheck" % "1.14.3"
+    "org.scalacheck" %%% "scalacheck" % "1.15.1"
   }
 
   def paradise(scalaVersion: String) =
