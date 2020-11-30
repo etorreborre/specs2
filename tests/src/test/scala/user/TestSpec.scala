62993b48deab74b45657278a69da96a635ab2c46
etorreborre <eric.torreborre@symbiont.io>
2020-11-30 10:20:08 +0100

    WIP new blocks definitions

--- tests/src/test/scala/user/TestSpec.scala
+++ tests/src/test/scala/user/TestSpec.scala
@@ -16,3 +16,41 @@ class TestSpec extends Specification with ScalaCheck { def is = s2"""
 class TestMutableSpec extends org.specs2.mutable.Specification:
 
   "e1" >> ok
+
+trait Blocks:
+  trait ToBlock[S, R]:
+    def toBlock(d: String, s: S): R
+
+  given ToBlock[F, F]:
+    def toBlock(s: String, f: F): F =  f
+
+  given ToBlock[FS, FS]:
+    def toBlock(s: String, fs: FS): FS = fs
+
+  given [R : AsE] as ToBlock[R, F]:
+    def toBlock(s: String, r: R): F =
+      new F {}
+
+  extension [S,R] (d: String):
+    def >>>(s: S)(using t: ToBlock[S, R]): R =
+     summon[ToBlock[S, R]].toBlock(d, s)
+
+trait S extends Blocks
+
+object Test:
+  new S:
+    "try this" >>> {
+      "in that" >>> new R {}
+    }
+
+
+trait F
+trait FS
+trait E
+trait R
+
+trait AsE[R]:
+ def toE(r: =>R): E
+
+given AsE[R]:
+ def toE(r: =>R) = new E {}
