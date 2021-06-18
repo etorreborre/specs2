3ca1c779e93920871774f4b4f4af573ab1a4455a
etorreborre <eric.torreborre@symbiont.io>
2021-06-18 11:54:01 +0200

    scala3 project: fixed the directory references with VERSION

--- guide/src/test/scala/org/specs2/guide/Specs2Variables.scala
+++ guide/src/test/scala/org/specs2/guide/Specs2Variables.scala
@@ -8,10 +8,10 @@ object Specs2Variables extends Specs2Variables
 trait Specs2Variables:
 
   lazy val VERSION        = BuildInfo.version
-  lazy val GUIDE_DIR      = s"https://etorreborre.github.io/specs2/guide/SPECS2-$VERSION"
-  lazy val SITE_DIR       = s"https://etorreborre.github.io/specs2/website/SPECS2-$VERSION"
-  lazy val API_DIR        = s"https://etorreborre.github.io/specs2/api/SPECS2-$VERSION"
-  lazy val PROJECT_DIR    = s"https://github.com/etorreborre/specs2/tree/SPECS2-$VERSION"
+  lazy val GUIDE_DIR      = s"https://etorreborre.github.io/specs2/guide/$VERSION"
+  lazy val SITE_DIR       = s"https://etorreborre.github.io/specs2/website/$VERSION"
+  lazy val API_DIR        = s"https://etorreborre.github.io/specs2/api/$VERSION"
+  lazy val PROJECT_DIR    = s"https://github.com/etorreborre/specs2/tree/$VERSION"
   lazy val EXAMPLES_DIR   = s"$PROJECT_DIR/examples/jvm/src/test/scala/examples"
 
   lazy val specs2Variables: Map[String, String] =
