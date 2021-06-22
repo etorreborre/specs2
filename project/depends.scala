eee7bea1f81333d9c3561614fcd4e85b53a8383e
etorreborre <eric.torreborre@symbiont.io>
2021-06-22 09:00:55 +0200

    reverted the xml dependency to 1.3.0

--- project/depends.scala
+++ project/depends.scala
@@ -44,7 +44,7 @@ object depends {
       scalaParser.value
   }
 
-  def scalaXml = "org.scala-lang.modules" %% "scala-xml" % "2.0.0"
+  def scalaXml = "org.scala-lang.modules" %% "scala-xml" % "1.3.0"
 
   lazy val mockito  = "org.mockito"  % "mockito-core"  % "3.11.2"
   lazy val junit    = "junit"        % "junit"         % "4.13.2"
