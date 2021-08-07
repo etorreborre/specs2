13a77e9dde9c277feb1671afd46149b934fe110c
etorreborre <eric.torreborre@symbiont.io>
2021-08-07 16:23:25 +0200

    feature: generalize PendingUntilFixed to Executions and add a prefix method

--- core/jvm/src/test/scala/org/specs2/execute/PendingUntilFixedSpec.scala
+++ core/jvm/src/test/scala/org/specs2/execute/PendingUntilFixedSpec.scala
@@ -2,7 +2,7 @@ package org.specs2
 package execute
 
 import mutable.Specification
-import org.specs2.specification.core.{Env, Fragment, OwnEnv}
+import org.specs2.specification.core.{Env, Execution, Fragment, OwnEnv}
 import org.specs2.specification.process.DefaultExecutor
 import org.specs2.control.ExecuteActions._
 
@@ -13,25 +13,35 @@ class PendingUntilFixedSpec(val env: Env) extends Specification with OwnEnv {
   "it must change to failed when the example succeeds" in e3
   "with a specific message" in e4
   "An AssertionError must be interpreted as non-fixed" in e5
+  "An Execution can be marked as pending until fixed" in e6
 
   def e1 = {
     val ex = "ex" ! { 1 must_== 2 }.pendingUntilFixed
     execute(ex) must_== Pending("Pending until fixed")
   }
+
   def e2 = {
     val ex = "ex" ! { 1 must_== 2 }.pendingUntilFixed("ISSUE-123")
     execute(ex) must_== Pending("ISSUE-123. Pending until fixed")
   }
+
   def e3 = {
     val ex = "ex" ! { 1 must_== 1 }.pendingUntilFixed
     execute(ex) must_== Failure("Fixed now, you should remove the 'pendingUntilFixed' marker")
   }
+
   def e4 = {
     val ex = "ex" ! { 1 must_== 1 }.pendingUntilFixed("ISSUE-123")
     execute(ex) must_== Failure("ISSUE-123. Fixed now, you should remove the 'pendingUntilFixed' marker")
   }
+
   def e5 = {
     val ex = "ex" ! { assert(false); 1 must_== 2 }.pendingUntilFixed
+    execute(ex) must_== Pending("Pending until fixed")
+  }
+
+  def e6 = {
+    val ex = "ex" ! pendingUntilFixed { Execution.result(1 must_== 2) }
     execute(ex) must_== Pending("Pending until fixed")
   }
 
---
+++ core/shared/src/main/scala/org/specs2/execute/PendingUntilFixed.scala
@@ -0,0 +1,47 @@
+package org.specs2
+package execute
+
+import text.Quote._
+import org.specs2.specification.core._
+
+/**
+ * This function allows to mark the body of an example as pending until it is fixed.
+ *
+ * If the result becomes a success then it is reported as a Failure so that the user thinks of
+ * removing the marker
+ */
+trait PendingUntilFixed { outer =>
+
+  /** @return Pending unless the result of the execution is a success */
+  def pendingUntilFixed[T : AsExecution](t: =>T): Execution =
+    pendingUntilFixed("")(t)
+
+  /** @return Pending unless the result of the execution is a success */
+  def pendingUntilFixed[T : AsExecution](m: String = "")(t: =>T): Execution =
+    AsExecution[T].execute(t).mapResult {
+    case s @ Success(_,_) => Failure(m.prefix(". ", "Fixed now, you should remove the 'pendingUntilFixed' marker"))
+    case other            => Pending(m.prefix(". ", "Pending until fixed"))
+  }
+
+  // postfix methods
+  implicit def toPendingUntilFixed[T : AsExecution](t: =>T): PendingUntilFixed[T] =
+    new PendingUntilFixed(t)
+
+  class PendingUntilFixed[T : AsExecution](t: =>T) {
+    def pendingUntilFixed: Execution =
+      outer.pendingUntilFixed("")(t)
+
+    def pendingUntilFixed(m: String = ""): Execution =
+      outer.pendingUntilFixed(m)(t)
+  }
+
+}
+
+/**
+ * use this trait to remove the pending until fixed implicit conversion
+ */
+trait NoPendingUntilFixed extends PendingUntilFixed {
+  override def toPendingUntilFixed[T : AsExecution](t: =>T) = super.toPendingUntilFixed(t)
+}
+
+object PendingUntilFixed extends PendingUntilFixed
