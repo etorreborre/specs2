8b40eb010f311afa48bfd727bf9b2c0254ef2cc0
Marko Elezovic <marko.elezovic@oradian.com>
2021-09-01 14:00:14 +0200

    Add .pendingUntilFixed + BeforeAll spec
    Seems to be a regression since 4.12.4

---
+++ core/jvm/src/test/scala/org/specs2/execute/PendingUntilFixedBeforeAllSpec.scala
@@ -0,0 +1,18 @@
+package org.specs2
+package execute
+
+import org.specs2.mutable.Specification
+import org.specs2.specification.BeforeAll
+
+class PendingUntilFixedBeforeAllSpec extends Specification with BeforeAll {
+  private[this] var setupFinished = false
+
+  override def beforeAll(): Unit = {
+    setupFinished = true // will not execute as of 4.12.4
+  }
+
+  "Pending until fixed can be used with BeforeAll" >> {
+    setupFinished ==== true
+    (1 ==== 1).pendingUntilFixed("Will explode")
+  }
+}
