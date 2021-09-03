4bd49e555d22b4e160ca85a640c9223d6fb0a639
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-09-03 22:48:42 +0200

    Update scalafmt-core to 3.0.2 (#985)
    * Update scalafmt-core to 3.0.2
    
    * Reformat with scalafmt 3.0.2

--- core/.jvm/src/test/scala/org/specs2/mutable/MutableSpec.scala
+++ core/.jvm/src/test/scala/org/specs2/mutable/MutableSpec.scala
@@ -7,7 +7,7 @@ class MutableSpec(val env: Env) extends Specification with OwnExecutionEnv:
 
   "s2 strings must create examples".p
   s2"""${this.is.examplesList must haveSize(2)}"""
-  
+
   "an execution can be used in a block even if an exception is thrown" >> pendingUntilFixed("it's ok") {
     throw new Exception("boom")
     (1 === 1).pendingUntilFixed("to force the conversion of the block to an Execution")
