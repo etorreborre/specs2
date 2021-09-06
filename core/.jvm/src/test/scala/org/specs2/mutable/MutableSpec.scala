93ceb9a23162aa357b1548e4933249ba4e0be8c1
etorreborre <eric.torreborre@symbiont.io>
2021-09-06 18:51:45 +0200

    test: added some tests cases related to #981 and implicits

--- core/.jvm/src/test/scala/org/specs2/mutable/MutableSpec.scala
+++ core/.jvm/src/test/scala/org/specs2/mutable/MutableSpec.scala
@@ -2,6 +2,8 @@ package org.specs2
 package mutable
 
 import org.specs2.specification.core.{Env, OwnExecutionEnv}
+import org.specs2.execute.{AsResult, Result}
+import org.specs2.specification.AroundEach
 
 class MutableSpec(val env: Env) extends Specification with OwnExecutionEnv:
 
@@ -12,3 +14,43 @@ class MutableSpec(val env: Env) extends Specification with OwnExecutionEnv:
     throw new Exception("boom")
     (1 === 1).pendingUntilFixed("to force the conversion of the block to an Execution")
   }
+
+
+// https://github.com/etorreborre/specs2/pull/981#issuecomment-911400717
+// this used to throw a ClassCastException possibly due to a scala bug with implicits
+class NestedSpec extends Specification {
+  "This is a test for #981" >> {
+    "Nested" >> {
+      1 === 1
+    }
+  }
+}
+
+// #984 this again looks like a weird behaviour with implicits
+// Prior to the fix which shuffled implicits around the var
+// was being captured by the >> { ... } closure and never evaluated again
+class ImplicitClosureMutableSpec extends Specification with AroundEach:
+  private var value: Int = 0
+
+  override def around[R: AsResult](r: =>R): Result =
+    value = 1
+    AsResult(r)
+
+  "Around should use the same value" >> {
+    value === 1
+  }
+
+class ImplicitClosureSpec extends org.specs2.Specification with AroundEach:
+  def is = s2"""
+
+  Around should use the same value $checkValue
+  """
+
+  private var value: Int = 0
+
+  override def around[R: AsResult](r: => R): Result =
+    value = 1
+    AsResult(r)
+
+  def checkValue =
+    value === 1
