d51e3a0da8919f5697d21351288455606101d944
etorreborre <eric.torreborre@symbiont.io>
2021-09-10 01:04:11 +0200

    test: reworked the ActionMatchers in order to return a Future[Result] after a match

--- core/shared/src/main/scala/org/specs2/matcher/ActionMatchers.scala
+++ core/shared/src/main/scala/org/specs2/matcher/ActionMatchers.scala
@@ -5,52 +5,61 @@ import control.*
 import execute.*
 import text.Regexes.*
 import concurrent.*
+import scala.concurrent.*
 
 /** Matchers for Action values
   */
 trait ActionMatchers extends ValueChecks:
 
-  private val ee: ExecutionEnv =
+  given executionEnv: ExecutionEnv =
     ExecutionEnv.fromGlobalExecutionContext
 
-  def beOk[T]: Matcher[Action[T]] =
-    Matcher { (action: Action[T]) =>
-      AsResult(action.runAction(ee).fold(t => Error(t), _ => Success()))
-    }
+  given ExecutionContext =
+    executionEnv.executionContext
 
-  def beOk[T, R: AsResult](f: T => R): Matcher[Action[T]] =
-    Matcher { (action: Action[T]) =>
-      AsResult(action.runAction(ee).fold(t => Error(t), t => AsResult(f(t))))
-    }
+  def beOk[T]: FutureMatcher[Action[T]] =
+    beOk[T, T](identity)
 
-  def beOk[T](check: ValueCheck[T]): Matcher[Action[T]] =
-    Matcher { (action: Action[T]) =>
-      AsResult(action.runAction(ee).fold(t => Error(t), check.check))
-    }
+  def beOk[T, R](f: T => R): FutureMatcher[Action[T]] =
+    FutureMatcher((_:Action[T]).map(f).runFuture(executionEnv))
 
-  def beOkWithValue[T](t: T): Matcher[Action[T]] =
+  def beOk[T](check: ValueCheck[T]): FutureMatcher[Action[T]] =
+    FutureMatcher((_:Action[T]).map(check.check).runFuture(executionEnv))
+
+  def beOkWithValue[T](t: T): FutureMatcher[Action[T]] =
     beOk(new BeEqualTo(t))
 
-  def beKo[T]: Matcher[Action[T]] =
-    Matcher { (action: Action[T]) =>
-      action
-        .runAction(ee)
-        .fold(
-          e => Success(),
-          ok => Failure("a failure was expected")
-        )
+  def beKo[T]: FutureMatcher[Action[T]] =
+    FutureMatcher { (action: Action[T]) =>
+      action.runFuture(executionEnv).map(_ => Failure("a failure was expected")).recover(_ => Success("ok"))
     }
 
-  def beKo[T](message: String): Matcher[Action[T]] =
-    Matcher { (action: Action[T]) =>
-      action
-        .runAction(ee)
-        .fold(
-          throwable =>
-            if throwable.getMessage `matchesSafely` message then Success()
-            else Failure(s"the action failed with message ${throwable.getMessage}. Expected: $message"),
-          ok => Failure(s"a failure with message $message was expected")
-        )
+  def beKo[T](message: String): FutureMatcher[Action[T]] =
+    FutureMatcher { (action: Action[T]) =>
+      action.runFuture(executionEnv).map(_ => Failure(s"a failure with message $message was expected")).
+        recover {
+          case t if t.getMessage `matchesSafely` message => Success("ok")
+          case t => Failure(s"the action failed with message ${t.getMessage}. Expected: $message"),
+      }
     }
+
+  extension [T](action: Action[T])
+    infix def must(m: FutureMatcher[Action[T]]): Future[Result] =
+      m(action)
 
 object ActionMatchers extends ActionMatchers
+
+trait FutureMatcher[-T]:
+
+  /** apply this matcher to a value
+    * @return
+    *   a Future Result describing the outcome of the match
+    */
+  def apply[S <: T](s: S): Future[Result]
+
+object FutureMatcher:
+  def apply[T, R](f: T => Future[R])(using ee: ExecutionEnv): FutureMatcher[T] =
+    new FutureMatcher[T]:
+      def apply[S <: T](s: S): Future[Result] =
+        given ExecutionContext = ee.executionContext
+        f(s).map(_ => Success("ok")).recover(t => Error(t))
