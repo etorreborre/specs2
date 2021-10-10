9bf6bbf4af3618a534c80b19717600e3745f8139
etorreborre <etorreborre@users.noreply.github.com>
2021-10-10 23:13:23 +0000

    Deploying to gh-pages from @ etorreborre/specs2@048cd9be741b5f4bfd93e2a06a98320c925fed81 🚀

---
+++ website/SPECS2-5.0.0-RC-14/javascript/versions.js
@@ -0,0 +1,10 @@
+var guideVersions
+ = [
+ {id:"../../guide/SPECS2-4.12.0/org.specs2.guide.UserGuide.html", text:"4.12.0"},
+{id:"../../guide/SPECS2-5.0.0-RC-14/org.specs2.guide.UserGuide.html", text:"5.0.0-RC-14"}
+];
+ var apiVersions
+ = [
+ {id:"../../api/SPECS2-4.12.0/index.html", text:"4.12.0"},
+{id:"../../api/SPECS2-5.0.0-RC-14/api/index.html", text:"5.0.0-RC-14"}
+];
