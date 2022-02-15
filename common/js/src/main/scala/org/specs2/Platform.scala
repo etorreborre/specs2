4a2919457c64c7813be4cac34dc10b05b584d7c4
Arman Bilge <armanbilge@gmail.com>
2022-02-15 19:53:58 +0000

    No getSuperclass on native

---
+++ common/js/src/main/scala/org/specs2/Platform.scala
@@ -0,0 +1,7 @@
+package org.specs2
+
+private[specs2] object Platform {
+  final val isJVM = false
+  final val isJS = true
+  final val isNative = false
+}
---
+++ common/jvm/src/main/scala/org/specs2/Platform.scala
@@ -0,0 +1,7 @@
+package org.specs2
+
+private[specs2] object Platform {
+  final val isJVM = true
+  final val isJS = false
+  final val isNative = false
+}
