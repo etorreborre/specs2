9daf4b7ce4a87e4e9330014794090c62caa9077b
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-09-09 16:33:54 +0200

    Update scalafmt-core to 3.5.9 (#1098)
    * Update scalafmt-core to 3.5.9
    
    * Reformat with scalafmt 3.5.9
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.5.9' to .git-blame-ignore-revs

--- project/mima.scala
+++ project/mima.scala
@@ -4,7 +4,11 @@ import com.typesafe.tools.mima.core.ProblemFilters._
 object Mima {
 
   lazy val excluded = Seq(
-    ProblemFilters.exclude[ReversedMissingMethodProblem]("org.specs2.specification.dsl.mutable.ReferenceDsl.org$specs2$specification$dsl$mutable$ReferenceDsl$$super$~"),
-    ProblemFilters.exclude[ReversedMissingMethodProblem]("org.specs2.specification.dsl.mutable.ReferenceDsl.org$specs2$specification$dsl$mutable$ReferenceDsl$$super$~/")
+    ProblemFilters.exclude[ReversedMissingMethodProblem](
+      "org.specs2.specification.dsl.mutable.ReferenceDsl.org$specs2$specification$dsl$mutable$ReferenceDsl$$super$~"
+    ),
+    ProblemFilters.exclude[ReversedMissingMethodProblem](
+      "org.specs2.specification.dsl.mutable.ReferenceDsl.org$specs2$specification$dsl$mutable$ReferenceDsl$$super$~/"
     )
+  )
 }
