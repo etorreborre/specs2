f51032ccee3e5c6af724b1896badd8554b9963fe
etorreborre <etorreborre@yahoo.com>
2023-11-04 14:48:26 +0100

    feature: add a MockitoThrowExpectations object
    to be used with specifications where failures should be thrown as exceptions
    fixes #1200

--- mock/shared/src/main/scala/org/specs2/mock/Mockito.scala
+++ mock/shared/src/main/scala/org/specs2/mock/Mockito.scala
@@ -3,14 +3,15 @@ package mock
 
 import mockito._
 import matcher.Expectations
+import matcher.ThrownExpectations
 
 /**
  * This trait can be used to access Mockito functionalities.
  */
 trait Mockito extends MocksCreation
   with CalledMatchers
-  with MockitoStubs 
-  with CapturedArgument 
+  with MockitoStubs
+  with CapturedArgument
   with MockitoMatchers
   with ArgThat
   with Expectations
@@ -18,3 +19,7 @@ trait Mockito extends MocksCreation
 
 object Mockito extends Mockito
 
+// This object provides mockito expectations which will throw an exception in case of a failure.
+// It should be used with either org.specs2.mutable.Specification or org.specs2.SpecificationWithJUnit
+// which are specifications throwing exceptions when there are failures
+object MockitoThrownExpectations extends Mockito with ThrownExpectations
