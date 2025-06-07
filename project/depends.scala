314b40f8b8979baae9de5f5ed73fe2128874be2c
Scala Steward <scala_steward@virtuslab.com>
2025-06-07 14:39:30 +0000

    Update junit-platform-engine to 1.13.1

--- project/depends.scala
+++ project/depends.scala
@@ -23,7 +23,7 @@ object depends {
 
   // used in specs2-junit
   val junitVintage = "org.junit.vintage" % "junit-vintage-engine" % "5.13.0"
-  val junit = libraryDependencies ++= Seq(junitVintage, "org.junit.platform" % "junit-platform-engine" % "1.13.0")
+  val junit = libraryDependencies ++= Seq(junitVintage, "org.junit.platform" % "junit-platform-engine" % "1.13.1")
   val junitTest = libraryDependencies += junitVintage % Test
 
   // used in specs2-markdown for the markdown parser
