860e3c221db92acd3b6655fb36c7a545bed6e2a4
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-10-06 10:05:42 +0200

    Update junit-platform-engine to 6.0.0 (#1430)
    Co-authored-by: Eric Torreborre <etorreborre@users.noreply.github.com>

--- project/depends.scala
+++ project/depends.scala
@@ -23,7 +23,7 @@ object depends {
 
   // used in specs2-junit
   val junitVintage = "org.junit.vintage" % "junit-vintage-engine" % "6.0.0"
-  val junit = libraryDependencies ++= Seq(junitVintage, "org.junit.platform" % "junit-platform-engine" % "1.14.0")
+  val junit = libraryDependencies ++= Seq(junitVintage, "org.junit.platform" % "junit-platform-engine" % "6.0.0")
   val junitTest = libraryDependencies += junitVintage % Test
 
   // used in specs2-markdown for the markdown parser
